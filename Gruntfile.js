3a61a223e16207e1d55e55df91223e1d46de052f
Michael Bailly <mbailly@linagora.com>
2014-01-02 13:08:03 +0100

    Add automation tasks manager (grunt) integration

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'backend/**/*.js', 'frontend/js/**/*.js', 'test/**/**/*.js'],
+      options: {
+        jshintrc: '.jshintrc',
+        ignores: []
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+    mochacli: {
+      options: {
+        require: ['chai', 'mockery'],
+        reporter: 'spec'
+      },
+      all: ['test/unit-backend/**/*.js']
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-shell');
+
+  grunt.registerTask('test-backend', 'run tests', ['test-backend-prepare', 'mochacli']);
+  
+  grunt.registerTask('test-backend-prepare', 'prepare tests environment', function() {
+    var done = this.async();
+    var child = require('child_process').spawn('sh', ['./scripts/prepare-backend-tests-environment.sh']);
+    child.stdout.on('data', function(chunk) {
+      grunt.log.write(chunk);
+    });
+    child.stderr.on('data', function(chunk) {
+      grunt.log.error(chunk);
+    });
+    child.on('close',function(code) {
+      done(code ? false : true);
+    });
+  });
+
+  grunt.registerTask('gjslint', 'run the closure linter', function() {
+    var done = this.async();
+    var child = require('child_process').spawn('python', ['./scripts/gjslint.py', '--disable', '0110', '--nojsdoc', '-r', 'test', '-r', 'backend']);
+    child.stdout.on('data', function(chunk) {
+      grunt.log.write(chunk);
+    });
+    child.stderr.on('data', function(chunk) {
+      grunt.log.error(chunk);
+    });
+    child.on('close',function(code) {
+      done(code ? false : true);
+    });
+  });
+
+
+  grunt.registerTask('test', ['jshint', 'gjslint', 'test-backend']);
+
+  grunt.registerTask('default', ['test']);
+
+};
---
+++ scripts/prepare-backend-tests-environment.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ "$1" = "help" ] || [ "$1" = "-h" ] ; then
+  echo "Usage: $0 [port]"
+  exit
+fi
+
+CONFIG_FILE="test/unit-backend/default.test.json"
+
+if [ ! -f "$CONFIG_FILE" ] ; then
+  echo "Error: no test file $CONFIG_FILE"
+  exit 1
+fi
+
+WEBSERVER_PORT=
+WSSERVER_PORT=
+if [ "$1" ] ; then
+  WEBSERVER_PORT="$1"
+fi
+if [ "$WEBSERVER_PORT" = "" ] ; then
+  WEBSERVER_PORT=4567
+fi
+
+if [ "$2" ] ; then
+  WSSERVER_PORT="$2"
+fi
+if [ "$WSSERVER_PORT" = "" ] ; then
+  WSSERVER_PORT=4567
+fi
+
+CONFIG_FILENAME=$(basename "$CONFIG_FILE")
+CONFIG_TARGET_FILE="config/$CONFIG_FILENAME"
+
+cat "$CONFIG_FILE" | sed "s/__WEBSERVER_PORT___/$WEBSERVER_PORT/g" \
+                  |  sed "s/__WSSERVER_PORT___/$WSSERVER_PORT/g"   \
+                  > "$CONFIG_TARGET_FILE"
+RET=$?
+if [ $RET -ne 0 ] ; then
+  echo "Error: can't copy $CONFIG_FILE to $CONFIG_TARGET_FILE"
+fi
+
+export NODE_ENV="test"
+
+# mocha --colors --reporter spec test/unit-backend/**/*.js test/unit-backend/
+
+# rm -f "$CONFIG_TARGET_FILE"
