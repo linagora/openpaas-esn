84903349e3128053f222aadcd7b360cc7aed62ea
Michael Bailly <mbailly@linagora.com>
2014-01-02 13:16:04 +0100

    New Core module

---
+++ backend/core/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('./env');
+var fs = require('fs');
+exports = module.exports = {};
+
+fs.readdirSync(__dirname).forEach(function(filename) {
+  var stat = fs.statSync(__dirname + '/' + filename);
+  if (!stat.isDirectory()) { return; }
+  function load() { return require('./' + filename); }
+  exports.__defineGetter__(filename, load);
+});
