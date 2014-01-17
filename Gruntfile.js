c169ae364ba209bde887dee946d92183a6777984
Michael Bailly <mbailly@linagora.com>
2014-01-17 15:23:36 +0100

    OR-6 Add grunt tasks entries for frontend tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,7 +61,27 @@ module.exports = function(grunt) {
   });
 
 
-  grunt.registerTask('test', ['jshint', 'gjslint', 'test-backend']);
+  grunt.registerTask('test-frontend', 'run the FrontEnd tests', function() {
+    var done = this.async();
+    
+    var child = require('child_process').spawn('karma', ['start', '--browsers', 'PhantomJS', './test/config/karma.conf.js']);
+    
+    child.stdout.on('data', function(chunk) { grunt.log.write(chunk); });
+    child.stderr.on('data', function(chunk) { grunt.log.error(chunk); });
+    child.on('close',function(code) { done(code ? false : true); });
+  });
+  
+  grunt.registerTask('test-frontend-all', 'run the FrontEnd tests on all possible browsers', function() {
+    var done = this.async();
+    
+    var child = require('child_process').spawn('karma', ['start', '--browsers', 'PhantomJS,Firefox,Chrome', './test/config/karma.conf.js']);
+    
+    child.stdout.on('data', function(chunk) { grunt.log.write(chunk); });
+    child.stderr.on('data', function(chunk) { grunt.log.error(chunk); });
+    child.on('close',function(code) { done(code ? false : true); });
+  });
+  
+  grunt.registerTask('test', ['jshint', 'gjslint', 'test-backend', 'test-frontend']);
   grunt.registerTask('default', ['test']);
 
 };
