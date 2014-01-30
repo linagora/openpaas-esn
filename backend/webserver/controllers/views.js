259d8afc3f8495335110b10804a001dcbbf0506d
Christophe Hamerling <christophe.hamerling@gmail.com>
2014-01-30 21:17:34 +0100

    OR-26 remove / route and make views controller independent from express app

--- backend/webserver/controllers/views.js
+++ backend/webserver/controllers/views.js
@@ -3,8 +3,5 @@
 function views(req, res, next) {
   res.render(req.params[0]);
 }
-
-exports = module.exports = function(application) {
-  application.get('/views/*', views);
-};
+module.exports.views = views;
 
--- backend/webserver/routes.js
+++ backend/webserver/routes.js
@@ -2,15 +2,13 @@
 
 exports = module.exports = function(application) {
 
-  require('./controllers/views')(application);
+  var views = require('./controllers/views');
+  application.get('/views/*', views.views);
+
   require('./middleware/setup-routes')(application);
 
   var documentstore = require('./controllers/document-store');
   application.put('/api/document-store/connection', documentstore.store);
   application.get('/api/document-store/connection/:hostname/:port/:dbname', documentstore.test);
-
-  application.get('/', function(req, res) {
-    res.render('index');
-  });
 };
 
