e7f54519d94973f95c8fd5743df1705f74038cb0
Christophe Hamerling <christophe.hamerling@gmail.com>
2014-01-31 17:56:49 +0100

    OR-36 Passport + local file test with express and supertest

--- backend/webserver/passport.js
+++ backend/webserver/passport.js
@@ -9,20 +9,11 @@ var BasicStrategy = require('passport-http').BasicStrategy;
 var config = require('../core').config('default');
 
 var strategies = {
-  file : function() {
-    return new BasicStrategy(require('../core/auth/file'));
-  }
+  file: new BasicStrategy(require('../core/auth/file'))
 };
 
 var auth = 'file';
 if (config.auth && config.auth.strategy) {
   auth = config.auth.strategy;
 }
-passport.use('file', strategies[auth]);
-
-exports.isAuthenticated = function(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login');
-};
+passport.use(strategies[auth]);
