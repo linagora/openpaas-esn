46594327437fba71d34836dd0741eba12ed43cf4
Michael Bailly <mbailly@linagora.com>
2014-02-06 15:26:49 +0100

    OR-47 core module tries to initialize mongoose connection on load

--- backend/core/db/index.js
+++ backend/core/db/index.js
@@ -1,13 +1,5 @@
-var logger = require('../../core').logger;
-
-
 var dbModule = {
   mongo: require('./mongo')
 };
-
-// try to initialize Mongo
-if (!dbModule.mongo.init()) {
-  logger.warn('The MongoDB datastore could not be initialized');
-}
 
 module.exports = dbModule;
