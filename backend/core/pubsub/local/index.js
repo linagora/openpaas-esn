e4492ff7372f82c3ef2a25c41d9ceb710a670457
dready92 <mickael.bailly@free.fr>
2014-02-10 23:50:56 +0100

    OR-62 local pubsub module

---
+++ backend/core/pubsub/local/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var Pubsub = require('../pubsub');
+var EventEmitter = require('events').EventEmitter;
+var emitter = new EventEmitter();
+
+var removeListener = emitter.removeListener;
+
+emitter.removeListener = function(event, handler) {
+  var count = this.listeners(event).length;
+  var countAfterRemove = 0;
+  while (count !== countAfterRemove) {
+    count = countAfterRemove;
+    removeListener.call(this, event, handler);
+    countAfterRemove = this.listeners(event).length;
+  }
+};
+
+var pubsub = new Pubsub(emitter);
+module.exports = pubsub;
