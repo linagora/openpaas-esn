098ac89b5a17e20341d621ec4698d390c43b8402
Christophe Hamerling <christophe.hamerling@gmail.com>
2014-02-10 17:28:47 +0100

    OR-51 Each type of configuration got its own module

--- fixtures/index.js
+++ fixtures/index.js
@@ -7,56 +7,22 @@
 var fs = require('fs');
 var path = require('path');
 
-var loadDirectory = function(name, done) {
-  console.log('* Loading directory ', name);
-  fs.readdirSync(name).forEach(function (filename) {
-    var f = name + '/' + filename;
-    var stat = fs.statSync(f);
-    if (stat.isDirectory()) {
-      loadDirectory(f, function() {
-        console.log(' ** ' + f + ' has been loaded');
-      });
-    } else {
-      loadFile(f, function(err) {
-        if (err) {
-          console.log('  [ERROR] ' + f + ' has not been loaded (' + err.message + ')');
-        } else {
-          console.log('  [OK] ' + f + ' has been loaded');
-        }
-      });
-    }
-  });
-  if (done) {
-    done();
-  }
+var exit = function(status) {
+  process.exit(status);
 };
 
-var loadFile = function(name, done) {
-  console.log(' -> Loading file ', name);
-  var data;
-  try {
-    data = JSON.parse(fs.readFileSync(name));
-  } catch(err) {
-    if (done) {
-      return done(err);
-    }
-  }
-  var item = name.slice(name.lastIndexOf('/') + 1, name.lastIndexOf('.'));
-  try {
-    var e = require('../backend/core/esn-config')(item);
-    e.store(data, function(err) {
-      console.log(err);
-      if (done) {
-        return done(err);
+require('./config')(function(err) {
+  if (err) {
+    // aborting, we may not be able to load other fixtures if the database has not been configured
+    console.log('[ERROR] Can not load config fixtures, aborting...');
+    exit(1);
+  } else {
+    require('./esn-config')(function(err) {
+      if (err) {
+        console.log('[ERROR] ', err.message);
+      } else {
+        exit();
       }
     });
-  } catch(err) {
-    console.log(err);
-    return done(err);
   }
-};
-
-loadDirectory(__dirname, function() {
-  console.log('Done');
-  process.exit();
 });
---
+++ test/fixtures/test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('The fixtures module', function() {
+
+  describe('The config module', function() {
+
+    it('should copy the files in the config folder', function(done) {
+      done();
+    });
+
+
+
+  });
+
+
+});
