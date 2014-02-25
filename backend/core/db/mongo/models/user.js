d8670c4f050f7815ecc631f23bd894c8b2a63cbf
Christophe Hamerling <christophe.hamerling@gmail.com>
2014-02-25 16:16:10 +0100

    OR-115 Add password to user + password check method

--- backend/core/db/mongo/models/user.js
+++ backend/core/db/mongo/models/user.js
@@ -2,6 +2,7 @@
 
 var emailAddresses = require('email-addresses');
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 var Schema = mongoose.Schema;
 
 
@@ -29,11 +30,49 @@ var UserSchema = new Schema({
   emails: {type: [String], required: true, unique: true, validate: validateEmails},
   firstname: {type: String},
   lastname: {type: String},
+  password: {type: String},
   timestamps: {
     creation: {type: Date, default: Date.now}
   },
   schemaVersion: {type: Number, default: 1}
 });
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+  var SALT_FACTOR = 5;
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods = {
+  comparePassword: function(candidatePassword, cb) {
+    if (!candidatePassword) {
+      return cb(new Error('Can not compare with null password'));
+    }
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, isMatch);
+    });
+  }
+};
 
 UserSchema.statics = {
 
