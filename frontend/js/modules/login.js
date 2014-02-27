f210447d9eeba30bdd691f4eb515bdf43c1e774f
slemaistre <slemaistre@linagora.com>
2014-02-27 19:11:24 +0100

    OR-117 add a login module integrated with include tag of jade

--- frontend/js/modules/login.js
+++ frontend/js/modules/login.js
@@ -1,3 +1,21 @@
 'use strict';
 
+angular.module('esn.login', ['restangular'])
+  .controller('login', function($scope, $window) {
+    $scope.login = function(username, password) {
+      if(username === password) {
+        $window.location.href='/logged';
+      }
+    };
 
+  })
+  .factory('loginAPI', ['Restangular', function(Restangular) {
+
+    function get() {
+      return true;
+    }
+
+    return {
+      get: get
+    };
+  }]);
