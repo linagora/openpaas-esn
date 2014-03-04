507c6f31f9076568c0c9692134fe842a764f0b0f
Christophe Hamerling <christophe.hamerling@gmail.com>
2014-03-04 14:33:37 +0100

    OR-133 Add serialize tests

--- test/unit-backend/webserver/passport-test.js
+++ test/unit-backend/webserver/passport-test.js
@@ -45,4 +45,60 @@ describe('The passport configuration module', function() {
     });
 
   });
+
+  /*
+   passport.serializeUser(function(user, done) {
+   if (user && user.emails && user.emails.length && user.emails[0]) {
+   var email = user.emails[0].value || user.emails[0];
+   return done(null, email);
+   }
+   return done(new Error('Unable to serialize a session without email'));
+   });
+   passport.deserializeUser(function(username, done) {
+   User.loadFromEmail(username, function(err, user) {
+   done(err, user);
+   });
+   });
+   */
+
+  describe('The serialize fn', function() {
+    it('should serialize ESN user', function(done) {
+      require(this.testEnv.basePath + '/backend/webserver/passport');
+      var passport = require('passport');
+      var mail = 'foo@bar.com';
+      var user = {
+        emails: [mail]
+      };
+      passport.serializeUser(user, function(err, serialized) {
+        expect(err).to.not.exist;
+        expect(serialized).to.equal(mail);
+        done();
+      });
+    });
+
+    it('should serialize a passport user', function(done) {
+      require(this.testEnv.basePath + '/backend/webserver/passport');
+      var passport = require('passport');
+      var mail = 'foo@bar.com';
+      var user = {
+        emails: [{type: 'home', value: mail}]
+      };
+      passport.serializeUser(user, function(err, serialized) {
+        expect(err).to.not.exist;
+        expect(serialized).to.equal(mail);
+        done();
+      });
+    });
+
+    it('should fail when no email is set', function(done) {
+      require(this.testEnv.basePath + '/backend/webserver/passport');
+      var passport = require('passport');
+      var user = {};
+      passport.serializeUser(user, function(err, serialized) {
+        expect(err).to.exist;
+        expect(serialized).to.not.exist;
+        done();
+      });
+    });
+  });
 });
