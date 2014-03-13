115dbf208dfd373714bfd5df2dfd01cae5a230f3
Christophe Hamerling <christophe.hamerling@gmail.com>
2014-03-13 13:50:44 +0100

    OR-146 Add loginErrorService and display infos in login form when needed

--- frontend/js/modules/login.js
+++ frontend/js/modules/login.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('esn.login', ['restangular'])
-  .controller('login', function($scope, $location, $window, loginAPI) {
-    $scope.credentials = {username: '', password: '', rememberme: false};
+  .controller('login', function($scope, $location, $window, loginAPI, loginErrorService) {
+    $scope.loginIn = false;
+    $scope.credentials = loginErrorService.getCredentials() || {username: '', password: '', rememberme: false};
+    $scope.error = loginErrorService.getError();
+
     $scope.loginButton = {
       label: 'Sign In',
       notRunning: 'Sign In',
@@ -22,21 +25,22 @@ angular.module('esn.login', ['restangular'])
 
       loginAPI.login($scope.credentials).then(
         function(data) {
+          $scope.loginIn = true;
           $scope.loginTask.running = false;
           $window.location.href = '/';
-          return data;
         },
         function(err) {
           $scope.loginButton.label = $scope.loginButton.notRunning;
           $scope.loginTask.running = false;
+          $scope.error = err.data;
+          loginErrorService.set($scope.credentials, err.data);
           $location.path('/login');
-          $scope.error = {
-            status: 'error',
-            details: err
-          };
-          return $scope.error;
         }
       );
+    };
+
+    $scope.showError = function() {
+      return loginErrorService.getError() && $location.path() !== '/' && !$scope.loginTask.running && !$scope.loginIn;
     };
   })
   .factory('loginAPI', ['Restangular', function(Restangular) {
@@ -51,4 +55,32 @@ angular.module('esn.login', ['restangular'])
       login: login
     };
 
-  }]);
+  }])
+  .service('loginErrorService', function($rootScope, $location) {
+    this.data = {};
+
+    var self = this;
+    $rootScope.$on('$routeChangeSuccess', function() {
+      if ($location.path() === '/') {
+        self.data = {};
+      }
+    });
+
+    this.set = function(credentials, error) {
+      this.data.credentials = credentials;
+      this.data.error = error;
+    };
+
+    this.getData = function() {
+      return this.data;
+    };
+
+    this.getCredentials = function() {
+      return this.data.credentials;
+    };
+
+    this.getError = function() {
+      return this.data.error;
+    };
+  });
+
