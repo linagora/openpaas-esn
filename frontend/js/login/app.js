9a1c9ccfe8e24474db12782aa37ac1033c88cc5f
slemaistre <slemaistre@linagora.com>
2014-03-14 11:40:49 +0100

    OR-145 remove useless files and duplicate login template

--- frontend/js/login/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// REMOVE IT
-
-angular.module('loginApp', ['esn.login', 'restangular', 'ngRoute'])
-  .config(function($routeProvider, RestangularProvider) {
-    RestangularProvider.setBaseUrl('/api');
-  });
-
