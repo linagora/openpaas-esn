c33d1953561bf178c5dae1a4c01c9ccd78c1bc94
Graham Crosmarie <gcrosmarie@linagora.com>
2014-04-08 15:25:24 +0200

    OR-209 correct code style violation

--- test/config/karma.conf.js
+++ test/config/karma.conf.js
@@ -57,7 +57,7 @@ module.exports = function(config) {
       // setting this option will create only a single module that contains templates
       // from all the files, so you can load them all with module('templates')
       jadeRenderConfig: {
-        __: function(str){
+        __: function(str) {
           return str;
         }
       },
