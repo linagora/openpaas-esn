ed60db59abc91efb01beef51e6aaaf39dc61d6aa
Christophe Hamerling <christophe.hamerling@gmail.com>
2014-04-25 16:53:25 +0200

    OR-248 Add the GET /api/domains/:uuid route

--- backend/webserver/routes.js
+++ backend/webserver/routes.js
@@ -10,6 +10,7 @@ exports = module.exports = function(application) {
   var domains = require('./controllers/domains');
   application.get('/api/companies', companies.search);
   application.get('/api/domains/:uuid/members', domains.getMembers);
+  application.get('/api/domains/:uuid', authorize.requiresAPILogin, domains.load, domains.getDomain);
   application.post('/api/domains', domains.createDomain);
   application.post('/api/domains/:uuid/invitations', authorize.requiresAPILogin, domains.load, authorize.requiresDomainManager, domains.sendInvitations);
   application.get('/api/domains/:uuid/manager', authorize.requiresAPILogin, domains.load, authorize.requiresDomainManager, domains.getDomain);
