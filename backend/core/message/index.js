b0cdaf138f52fc1fcbc5c58918138fb6d71c1ea1
Stephen LE MAISTRE <slemaistre@linagora.com>
2014-04-28 11:11:35 +0200

    OR-244 add the message core module

---
+++ backend/core/message/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var logger = require(__dirname + '/../../core').logger,
+    mongoose = require('mongoose'),
+    Whatsup = mongoose.model('Whatsup');
+
+module.exports.save = function(message, callback) {
+  var whatsup = new Whatsup(message);
+  whatsup.save(function(err, response) {
+    if (!err) {
+      logger.info('Added new message in database:', { _id: response._id.toString() });
+    } else {
+      logger.warn('Error while trying to add a new message in database:', err.message);
+    }
+    callback(err, response);
+  });
+}
