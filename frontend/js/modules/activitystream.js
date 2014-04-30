ce153a1460a954a72cc263d2618fb0f7c8046c42
Michael Bailly <mbailly@linagora.com>
2014-04-30 14:22:16 +0200

    OR-254 new activitystreamFilter service
    This service remembers the ids of the objects already seen.

--- frontend/js/modules/activitystream.js
+++ frontend/js/modules/activitystream.js
@@ -8,4 +8,39 @@ angular.module('esn.activitystream', ['restangular'])
   return {
     get: get
   };
-}]);
+}])
+.factory('activitystreamFilter', function() {
+
+  return function() {
+
+    var sent = {}, removed = {};
+
+    function addToSentList(id) {
+      sent[id] = true;
+    }
+
+    function addToRemovedList(id) {
+      removed[id] = true;
+    }
+
+    function filter(item) {
+      if (sent[item.object._id] || removed[item.object._id]) {
+        return false;
+      }
+      if (item.verb === 'remove') {
+        addToRemovedList(item.object._id);
+        return false;
+      }
+      addToSentList(item.object._id);
+      return true;
+    }
+
+    return {
+      filter: filter,
+      addToSentList: addToSentList,
+      addToRemovedList: addToRemovedList
+    };
+  };
+
+
+});
--- test/unit-frontend/modules/activitystream.js
+++ test/unit-frontend/modules/activitystream.js
@@ -34,4 +34,80 @@ describe('The esn.activitystream Angular module', function() {
     });
   });
 
+  describe('activitystreamFilter service', function() {
+
+    beforeEach(function() {
+      angular.mock.module('esn.activitystream');
+    });
+    beforeEach(inject(function(activitystreamFilter) {
+      this.filter = activitystreamFilter;
+    }));
+
+    it('should be a function', function() {
+      expect(this.filter).to.be.a.function;
+    });
+
+    it('should return an object with filter, addToSentList and addToRemovedList methods', function() {
+      var f = this.filter();
+      expect(f).to.respondTo('filter');
+      expect(f).to.respondTo('addToSentList');
+      expect(f).to.respondTo('addToRemovedList');
+    });
+
+    describe('filter method', function() {
+      it('should respond true for a new timeline entry', function() {
+        var f = this.filter();
+        var tle = {
+          verb: 'post',
+          object: {
+            _id: 'ID1'
+          }
+        };
+        expect(f.filter(tle)).to.be.true;
+      });
+
+      it('should respond false for a new timeline entry having the verb remove', function() {
+        var f = this.filter();
+        var tle = {
+          verb: 'remove',
+          object: {
+            _id: 'ID1'
+          }
+        };
+        expect(f.filter(tle)).to.be.false;
+      });
+
+      it('should respond false for a new timeline entry it already knows', function() {
+        var f = this.filter();
+        var tle = {
+          verb: 'post',
+          object: {
+            _id: 'ID1'
+          }
+        };
+        f.filter(tle);
+        expect(f.filter(tle)).to.be.false;
+      });
+
+      it('should respond false for a new timeline entry it already knows and that was "remove"d', function() {
+        var f = this.filter();
+        var tle = {
+          verb: 'post',
+          object: {
+            _id: 'ID1'
+          }
+        };
+        var tle2 = {
+          verb: 'remove',
+          object: {
+            _id: 'ID1'
+          }
+        };
+        f.filter(tle2);
+        expect(f.filter(tle)).to.be.false;
+      });
+    });
+
+  });
+
 });
