5c8f2da43d68d081624fc2339eaee55f5bdc9058
Michael Bailly <mbailly@linagora.com>
2014-05-01 23:42:58 +0200

    OR-241 New checkDoc mongo helper feature for tests
    This awesome method takes a collection name, a search criteria (that can be a string,
    matched on _id, or anything else, passed as is to findOne(), a check criteria that can
    be a function or an object to deep equal, and a done callback

--- test/helpers.js
+++ test/helpers.js
@@ -2,6 +2,7 @@
 
 var mongoose = require('mongoose'),
     async = require('async'),
+    expect = require('chai').expect,
     MongoClient = require('mongodb').MongoClient;
 
 module.exports = function(mixin, testEnv) {
@@ -29,6 +30,51 @@ module.exports = function(mixin, testEnv) {
         async.forEach(collections, function(collection, done) {
           mongoose.connection.db.dropCollection(collection.collectionName, done);
         }, callback);
+      });
+    },
+    /*
+    *check a mongodb document
+    * @param collection string - the mongodb collection to get the doc
+    * @param id string|object - the doc _id (string) or the find criteria (object)
+    * @param check function|object - the function that checks the doc (function). This function should return something in case of error
+    *                                or the doc to check against (object)
+    * @param done function - the callback. No arguments on success, error on error
+    */
+    checkDoc: function(collection, id, check, done) {
+      MongoClient.connect(testEnv.mongoUrl, function(err, db) {
+
+        function close(err) {
+          db.close(function() {
+            done(err);
+          });
+        }
+
+        if (err) {
+          return done(err);
+        }
+
+        if (typeof id === 'string') {
+          id = {_id: id};
+        }
+
+        db.collection(collection).findOne(id, function(err, doc) {
+          if (err) {
+            return close(err);
+          }
+          expect(doc).to.exist;
+
+          if (typeof check === 'function') {
+            var checkErr = check(doc);
+            if (checkErr) {
+              return close(checkErr);
+            }
+          } else {
+            doc = JSON.parse(JSON.stringify(doc));
+            expect(doc).to.deep.equal(check);
+          }
+
+          close();
+        });
       });
     }
   };
