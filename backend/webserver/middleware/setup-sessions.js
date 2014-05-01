24c079e9ba57a07e8e3325670a00b3cf1c24a132
Michael Bailly <mbailly@linagora.com>
2014-05-01 21:36:41 +0200

    OR-241 express session middleware is reinitialized on any new mongodb connection

--- backend/webserver/middleware/setup-sessions.js
+++ backend/webserver/middleware/setup-sessions.js
@@ -2,18 +2,27 @@
 
 var express = require('express'),
     MongoStore = require('connect-mongo')(express),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    core = require('../../core'),
+    mongo = core.db.mongo,
+    topic = core.pubsub.local.topic('mongodb:connectionAvailable'),
+    logger = core.logger;
 
 function setupSession(session) {
   var setSession = function() {
+    logger.debug('mongo is connected, setting up mongo session store');
     session.setMiddleware(express.session({
+      cookie: { maxAge: 6000000 },
       store: new MongoStore({
         auto_reconnect: true,
         mongoose_connection: mongoose.connections[0]
       })
     }));
   };
-  mongoose.connection.once('connected', setSession);
+  if (mongo.isConnected()) {
+    setSession();
+  }
+  topic.subscribe(setSession);
 }
 
 module.exports = setupSession;
---
+++ test/unit-backend/webserver/middleware/setup-session.js
@@ -0,0 +1,122 @@
+'use strict';
+
+var expect = require('chai').expect;
+var mockery = require('mockery');
+
+describe('the setup-session middleware', function() {
+  it('should be a function', function() {
+    var setupSession = require(this.testEnv.basePath + '/backend/webserver/middleware/setup-sessions');
+    expect(setupSession).to.be.a.function;
+  });
+
+  it('should set the middleware if the datastore is connected', function(done) {
+    var mongooseMock = require(this.testEnv.fixtures + '/mongoose').mongoose();
+    mongooseMock.connections = [true];
+
+    var coreMock = {
+      db: {
+        mongo: {
+          isConnected: function() { return true; }
+        }
+      },
+      logger: { debug: function() {} },
+      pubsub: {
+        local: {
+          topic: function() { return { subscribe: function() {} }; }
+        }
+      }
+    };
+
+    var connectMongoMock = function() { return function() { this.on = function() {}; }; };
+
+    var session = {
+      setMiddleware: function() {
+        done();
+      }
+    };
+
+    mockery.registerMock('mongoose', mongooseMock);
+    mockery.registerMock('../../core', coreMock);
+    mockery.registerMock('connect-mongo', connectMongoMock);
+
+    var setupSession = require(this.testEnv.basePath + '/backend/webserver/middleware/setup-sessions');
+    setupSession(session);
+
+  });
+
+  it('should register a subscription mongodb:connectionAvailable', function() {
+    var mongooseMock = require(this.testEnv.fixtures + '/mongoose').mongoose();
+    mongooseMock.connections = [true];
+
+    var subscriptions = {};
+
+    var coreMock = {
+      db: {
+        mongo: {
+          isConnected: function() { return false; }
+        }
+      },
+      logger: { debug: function() {} },
+      pubsub: {
+        local: {
+          topic: function(topic) { return { subscribe: function(handler) { subscriptions[topic] = handler; } }; }
+        }
+      }
+    };
+
+    var connectMongoMock = function() { return function() { this.on = function() {}; }; };
+
+    var session = {
+      setMiddleware: function() {
+      }
+    };
+
+    mockery.registerMock('mongoose', mongooseMock);
+    mockery.registerMock('../../core', coreMock);
+    mockery.registerMock('connect-mongo', connectMongoMock);
+
+    var setupSession = require(this.testEnv.basePath + '/backend/webserver/middleware/setup-sessions');
+    setupSession(session);
+    expect(subscriptions).to.have.property('mongodb:connectionAvailable');
+    expect(subscriptions['mongodb:connectionAvailable']).to.be.a.function;
+  });
+
+  it('should set the session middleware when mongodb:connectionAvailable is published', function(done) {
+    var mongooseMock = require(this.testEnv.fixtures + '/mongoose').mongoose();
+    mongooseMock.connections = [true];
+
+    var subscriptions = {};
+
+    var coreMock = {
+      db: {
+        mongo: {
+          isConnected: function() { return false; }
+        }
+      },
+      logger: { debug: function() {} },
+      pubsub: {
+        local: {
+          topic: function(topic) { return { subscribe: function(handler) { subscriptions[topic] = handler; } }; }
+        }
+      }
+    };
+
+    var connectMongoMock = function() { return function() { this.on = function() {}; }; };
+
+    var session = {
+      setMiddleware: function() {
+      }
+    };
+
+    mockery.registerMock('mongoose', mongooseMock);
+    mockery.registerMock('../../core', coreMock);
+    mockery.registerMock('connect-mongo', connectMongoMock);
+
+    var setupSession = require(this.testEnv.basePath + '/backend/webserver/middleware/setup-sessions');
+    setupSession(session);
+    session.setMiddleware = function() {done();};
+    subscriptions['mongodb:connectionAvailable']();
+
+  });
+
+});
