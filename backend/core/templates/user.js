cd370ae14d9122196c44b88e408121cba6a19319
Michael Bailly <mbailly@linagora.com>
2014-05-01 23:46:26 +0200

    OR-241 fix templates injection, wait available mongo connection to try them

--- backend/core/templates/user.js
+++ backend/core/templates/user.js
@@ -3,8 +3,11 @@
 function store(callback) {
     var esnConf = require('../esn-config');
     var user = esnConf('user', 'templates');
-
-    user.store(require('./data/user-template.js'), callback);
+    user.get(function(err, doc) {
+      if (!doc) {
+        user.store(require('./data/user-template.js'), callback);
+      }
+    });
   }
 
 module.exports.store = store;
--- test/unit-backend/core/db/config-pubsub.js
+++ test/unit-backend/core/db/config-pubsub.js
@@ -1,45 +1,28 @@
 'use strict';
 
 var expect = require('chai').expect;
-var mockery = require('mockery');
 
 describe('The local pubsub for MongoDB configuration', function() {
-  var pubsub = null;
 
-  before(function() {
+  beforeEach(function() {
     this.testEnv.writeDBConfigFile();
-    pubsub = require(this.testEnv.basePath + '/backend/core').pubsub.local;
   });
 
-  after(function() {
+  afterEach(function() {
     this.testEnv.removeDBConfigFile();
   });
 
   it('should fire a publish when mongodb configuration is available', function(done) {
-
-    var mongodb = {
-      hostname: 'localhost',
-      port: 27017,
-      dbname: 'hiveety-test'
-    };
-
-    var topic = pubsub.topic('mongodb:configurationAvailable');
-    var configuredMock =
-      function() {
-        topic.publish(mongodb);
-        return true;
-      };
-
-    mockery.registerMock('../configured', configuredMock);
-
+    var mongodbConfiguration = { connectionString: 'mongodb://localhost:23456/tests' };
     var core = require(this.testEnv.basePath + '/backend/core');
-    var templates = core.templates;
+    var pubsub = core.pubsub.local;
+    var topic = pubsub.topic('mongodb:configurationAvailable');
 
     topic.subscribe(function(config) {
-      expect(config).to.deep.equal(mongodb);
+      expect(config).to.deep.equal(mongodbConfiguration);
       done();
     });
 
-    templates.inject(function() {});
+    core.configured();
   });
 });
