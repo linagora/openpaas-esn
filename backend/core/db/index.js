a6be492f44e67c7d93dc928c6a12c392ba69aef6
Michael Bailly <mbailly@linagora.com>
2014-05-04 21:14:52 +0200

    OR-241 remove 'mongodb:configurationAvailable' defererd

--- backend/core/db/index.js
+++ backend/core/db/index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var Q = require('q');
 var pubsub = require('..').pubsub.local;
 var configured = require('../configured');
 var core = require('..');
@@ -8,18 +7,5 @@ var core = require('..');
 var dbModule = {
   mongo: require('./mongo')
 };
-
-var mongoConfigDeferred = Q.defer();
-
-if (configured()) {
-  mongoConfigDeferred.resolve(core.config('db'));
-}
-else {
-  pubsub.topic('mongodb:configurationAvailable').subscribe(function(config) {
-    mongoConfigDeferred.resolve(config);
-  });
-}
-
-dbModule.mongoAvailable = mongoConfigDeferred.promise;
 
 module.exports = dbModule;
