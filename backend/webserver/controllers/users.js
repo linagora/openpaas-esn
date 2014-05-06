9ce1a74188610757d0562c7802cc5a4656599ac8
Christophe Hamerling <christophe.hamerling@gmail.com>
2014-05-06 11:24:01 +0200

    OR-219 Set default value to empty string if undefined

--- backend/webserver/controllers/users.js
+++ backend/webserver/controllers/users.js
@@ -155,7 +155,7 @@ function updateProfile(req, res) {
     return res.json(400, {error: {code: 400, message: 'Bad Request', details: 'Unknown parameter ' + parameter}});
   }
 
-  userModule.updateProfile(req.user, parameter, req.body.value, function(err) {
+  userModule.updateProfile(req.user, parameter, req.body.value || '', function(err) {
     if (err) {
       return res.json(500, {error: 500, message: 'Server Error', details: err.message});
     }
