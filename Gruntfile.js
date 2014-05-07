4cc93f0c707ab44f28511ea06ce55e621a0ff842
Christophe Hamerling <christophe.hamerling@gmail.com>
2014-05-07 14:28:15 +0200

    OR-282 Add stdout and stderr to options

--- Gruntfile.js
+++ Gruntfile.js
@@ -117,6 +117,12 @@ module.exports = function(grunt) {
       all: {
         options: {
           log: true,
+          stdout: function(data) {
+            grunt.log.write(data);
+          },
+          stderr: function(data) {
+            grunt.log.error(data);
+          },
           args: grunt.option('test') ? {test: grunt.option('test')} : {},
           process: function(res){
             if (res.fail){
@@ -133,6 +139,12 @@ module.exports = function(grunt) {
       midway_backend: {
         options: {
           log: true,
+          stdout: function(data) {
+            grunt.log.write(data);
+          },
+          stderr: function(data) {
+            grunt.log.error(data);
+          },
           args: grunt.option('test') ? {test: grunt.option('test')} : {},
           process: function(res){
             if (res.fail){
@@ -151,6 +163,12 @@ module.exports = function(grunt) {
         options: {
           log: true,
           args: grunt.option('test') ? {test: grunt.option('test')} : {},
+          stdout: function(data) {
+            grunt.log.write(data);
+          },
+          stderr: function(data) {
+            grunt.log.error(data);
+          },
           process: function(res){
             if (res.fail){
               grunt.config.set('esn.tests.success',false);
@@ -167,6 +185,12 @@ module.exports = function(grunt) {
       frontend: {
         options: {
           log: true,
+          stdout: function(data) {
+            grunt.log.write(data);
+          },
+          stderr: function(data) {
+            grunt.log.error(data);
+          },
           process: function(res){
             if (res.fail){
               grunt.config.set('esn.tests.success',false);
