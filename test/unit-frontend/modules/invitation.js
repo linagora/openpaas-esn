77ab0e0379ffb8e294bdc6a67e5bf40ea1c9cd5d
Thomas M <thomas.morsellino@linagora.com>
2014-05-12 11:24:19 +0200

    OR-226 correct tests after merge failure

--- test/unit-frontend/modules/invitation.js
+++ test/unit-frontend/modules/invitation.js
@@ -24,8 +24,8 @@ describe('The Invitation Angular module', function() {
         };
       }));
 
-      it('should send a request to /invitation/:id', function() {
-        this.$httpBackend.expectGET('/invitation/' + this.invitationId).respond(this.response);
+      it('should send a request to /invitations/:id', function() {
+        this.$httpBackend.expectGET('/invitations/' + this.invitationId).respond(this.response);
         this.invitationAPI.get(this.invitationId);
         this.$httpBackend.flush();
       });
@@ -51,8 +51,8 @@ describe('The Invitation Angular module', function() {
         this.response = {};
       }));
 
-      it('should send a request to /invitation', function() {
-        this.$httpBackend.expectPOST('/invitation').respond(this.response);
+      it('should send a request to /invitations', function() {
+        this.$httpBackend.expectPOST('/invitations').respond(this.response);
         this.invitationAPI.create(this.data);
         this.$httpBackend.flush();
       });
@@ -82,8 +82,8 @@ describe('The Invitation Angular module', function() {
         this.response = {};
       }));
 
-      it('should send a JSON PUT to /invitation/:uuid', function() {
-        this.$httpBackend.expectPUT('/invitation/' + this.uuid, this.data).respond(this.response);
+      it('should send a JSON PUT to /invitations/:uuid', function() {
+        this.$httpBackend.expectPUT('/invitations/' + this.uuid, this.data).respond(this.response);
         this.invitationAPI.finalize(this.uuid, this.data);
         this.$httpBackend.flush();
       });
