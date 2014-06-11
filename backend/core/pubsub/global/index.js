ef642f41f1865734d11c84be4be9e2a1912b135d
Christophe Hamerling <christophe.hamerling@gmail.com>
2014-06-11 11:00:32 +0200

    OR-345 Catch default error on redis connect

--- backend/core/pubsub/global/index.js
+++ backend/core/pubsub/global/index.js
@@ -4,10 +4,14 @@ var Pubsub = require('../pubsub');
 var redisPubsub = new Pubsub();
 var localPubsub = require('../').local;
 var AwesomeNodeRedisPubsub = require('awesome-node-redis-pubsub');
+var logger = require('../../logger');
 
 module.exports = redisPubsub;
 
 localPubsub.topic('redis:configurationAvailable').subscribe(function(config) {
+  config.onRedisError = function(err) {
+    logger.error('Got an error on redis pubsub : ' + err);
+  };
   var client = new AwesomeNodeRedisPubsub(config);
   redisPubsub.setClient(client);
 });
