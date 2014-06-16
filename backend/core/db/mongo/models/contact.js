485bf531639e1712c2caf9f7c6b26d9cb1ea2558
Michael Bailly <mbailly@linagora.com>
2014-06-16 09:56:52 +0200

    OR-309 New Contact and Addressbook models

---
+++ backend/core/db/mongo/models/contact.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var trim = require('trim');
+
+
+var ContactSchema = new mongoose.Schema({
+  owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+  addressbooks: {type: [mongoose.Schema.Types.ObjectId], ref: 'AddressBook'},
+  emails: {type: [String], required: true, unique: true},
+  given_name: {type: String},
+  timestamps: {
+    creation: {type: Date, default: Date.now}
+  }
+});
+
+
+ContactSchema.pre('save', function(next) {
+  var contact = this;
+
+  contact.emails = contact.emails.map(function(email) {
+    return trim(email).toLowerCase();
+  });
+
+  return next();
+});
+
+
+module.exports = mongoose.model('Contact', ContactSchema);
