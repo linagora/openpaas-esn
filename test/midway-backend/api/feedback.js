b755abec08ec4617d04ddfe8732a6018318a2e12
Romain STG_PIGNOLET <rpignolet@linagora.com>
2014-07-17 16:45:38 +0200

    OR-416 Add feedback midway backend tests

---
+++ test/midway-backend/api/feedback.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var request = require('supertest'),
+  expect = require('chai').expect;
+
+describe('The feedback API', function() {
+  var app;
+  var user;
+  var password = 'secret';
+
+  beforeEach(function(done) {
+    var self = this;
+    this.testEnv.initCore(function() {
+      app = require(self.testEnv.basePath + '/backend/webserver/application');
+
+      self.helpers.api.applyDomainDeployment('linagora_IT', function(err, models) {
+        if (err) { return done(err); }
+        user = models.users[0];
+        done();
+      });
+    });
+  });
+
+  afterEach(function(done) {
+    var mongoose = require('mongoose');
+    mongoose.connection.db.dropDatabase();
+    mongoose.disconnect(done);
+  });
+
+  it('should not be able to send a feedback without being authenticated', function(done) {
+    request(app)
+      .post('/api/feedback/')
+      .expect(401)
+      .end(done);
+  });
+
+  it('should be able to send a feedback when logged but should receive 400 without request body', function(done) {
+    this.helpers.api.loginAsUser(app, user.emails[0], password, function(err, requestAsMember) {
+      if (err) { return done(err); }
+      var req = requestAsMember(request(app).post('/api/feedback/'));
+      req.expect(400).end(done);
+    });
+  });
+
+  it('should be able to send a feedback when logged but should receive 400 with request body empty', function(done) {
+    this.helpers.api.loginAsUser(app, user.emails[0], password, function(err, requestAsMember) {
+      if (err) { return done(err); }
+      var req = requestAsMember(request(app).post('/api/feedback/'));
+      req.send({});
+      req.expect(400).end(done);
+    });
+  });
+
+  it('should be able to send a feedback when logged but should receive 400 with request body which contain empty string', function(done) {
+    this.helpers.api.loginAsUser(app, user.emails[0], password, function(err, requestAsMember) {
+      if (err) { return done(err); }
+      var req = requestAsMember(request(app).post('/api/feedback/'));
+      req.send({content: ''});
+      req.expect(400).end(done);
+    });
+  });
+
+  it('should be able to send a feedback when logged', function(done) {
+    var feedback = {
+      content: 'A feedback content'
+    };
+
+    this.helpers.api.loginAsUser(app, user.emails[0], password, function(err, requestAsMember) {
+      if (err) { return done(err); }
+      var req = requestAsMember(request(app).post('/api/feedback/'));
+      req.send(feedback);
+      req.expect(201).end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res.body).to.be.not.null;
+        expect(res.body.content).to.deep.equal(feedback.content);
+        expect(res.body.author).to.deep.equal(user._id.toString());
+        done();
+      });
+    });
+  });
+
+});
