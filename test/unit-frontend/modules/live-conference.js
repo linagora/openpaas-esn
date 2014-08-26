920db1ff3fc6bdb19a640baae6b3e0ca098ccd59
Stephen LE MAISTRE <slemaistre@linagora.com>
2014-08-26 17:14:32 +0200

    OR-445 fix live-conference.js of test-frontend

--- test/unit-frontend/modules/live-conference.js
+++ test/unit-frontend/modules/live-conference.js
@@ -8,12 +8,186 @@ describe('The live-conference Angular module', function() {
   beforeEach(angular.mock.module('esn.conference'));
   beforeEach(angular.mock.module('esn.live-conference'));
 
+  describe('easyRTCService service', function() {
+    var service, $q, tokendefer, $rootScope, $log, tokenAPI, session, webrtcFactory;
+
+    beforeEach(function() {
+      tokenAPI = {};
+      $log = {
+        debug: function() {}
+      };
+      session = {
+        user: {
+          _id: 2,
+          emails: ['test@openpaas.io']
+        },
+        domain: {
+          _id: 123
+        }
+      };
+
+      webrtcFactory = {
+        get: function() {
+          return {
+            setRoomOccupantListener: function() {},
+            setRoomEntryListener: function() {},
+            setDisconnectListener: function() {},
+            joinRoom: function() {},
+            easyApp: function() {},
+            hangupAll: function() {},
+            setOnCall: function() {},
+            setOnHangup: function() {},
+            setSocketOptions: function() {}
+          };
+        }
+      };
+
+      module(function($provide) {
+        $provide.value('$log', $log);
+        $provide.value('tokenAPI', tokenAPI);
+        $provide.value('session', session);
+        $provide.value('webrtcFactory', webrtcFactory);
+      });
+    });
+
+    it('$scope.performCall should hangupAll', function(done) {
+      webrtcFactory = {
+        get: function () {
+          return {
+            hangupAll: function () {
+              done();
+            }
+          };
+        }
+      };
+
+      module(function($provide) {
+        $provide.value('webrtcFactory', webrtcFactory);
+      });
+
+      inject(function($injector) {
+        service = $injector.get('easyRTCService');
+      });
+
+      service.performCall('YOLO');
+    });
+
+    it('$scope.performCall should call the given user id', function(done) {
+      var user_id = 123;
+
+      webrtcFactory = {
+        get: function () {
+          return {
+            hangupAll: function () {},
+            call: function(id) {
+              expect(id).to.equal(user_id);
+              done();
+            }
+          };
+
+        }
+      };
+
+      module(function($provide) {
+        $provide.value('webrtcFactory', webrtcFactory);
+      });
+
+      inject(function($injector) {
+        service = $injector.get('easyRTCService');
+      });
+
+      service.performCall(user_id);
+    });
+
+    it('$scope.connect should create the easyRTC app if token is retrieved', function(done) {
+
+      tokenAPI.getNewToken = function() {
+        tokendefer = $q.defer();
+        return tokendefer.promise;
+      };
+
+      webrtcFactory = {
+        get: function () {
+          return {
+            setRoomOccupantListener: function() {},
+            setRoomEntryListener: function() {},
+            setDisconnectListener: function() {},
+            joinRoom: function() {},
+            setSocketOptions: function() {},
+            easyApp: function() {
+              done();
+            }
+          };
+        }
+      };
+
+      module(function($provide) {
+        $provide.value('tokenAPI', tokenAPI);
+        $provide.value('webrtcFactory', webrtcFactory);
+      });
+
+      inject(function($injector, _$q_, _$rootScope_) {
+        service = $injector.get('easyRTCService');
+        $q = _$q_;
+        $rootScope = _$rootScope_;
+      });
+
+      service.connect({ _id: 123 }, {}, []);
+
+      tokendefer.resolve({token: '123'});
+      $rootScope.$digest();
+    });
+
+
+
+    it('$scope.connect should not create the easyRTC app if token is not retrieved', function(done) {
+      $log.error = function() {
+        done();
+      };
+
+      webrtcFactory = {
+        get: function () {
+          return {
+            setRoomOccupantListener: function() {},
+            setRoomEntryListener: function() {},
+            setDisconnectListener: function() {},
+            joinRoom: function() {},
+            setSocketOptions: function() {},
+            easyApp: function() {
+              done(new Error());
+            }
+          };
+        }
+      };
+
+      tokenAPI.getNewToken = function() {
+        tokendefer = $q.defer();
+        return tokendefer.promise;
+      };
+
+      module(function($provide) {
+        $provide.value('$log', $log);
+        $provide.value('tokenAPI', tokenAPI);
+        $provide.value('webrtcFactory', webrtcFactory);
+      });
+
+      inject(function($injector, _$q_, _$rootScope_) {
+        service = $injector.get('easyRTCService');
+        $q = _$q_;
+        $rootScope = _$rootScope_;
+      });
+
+      service.connect({ _id: 123 }, {}, []);
+      tokendefer.reject({data: 'ERROR'});
+      $rootScope.$digest();
+    });
+  });
+
   describe('liveConferenceController controller', function() {
-    beforeEach(angular.mock.inject(function($rootScope, $log, conferenceAPI, domainAPI, tokenAPI, webrtcFactory, $controller, $q) {
+    beforeEach(angular.mock.inject(function($rootScope, $log, conferenceAPI, domainAPI, tokenAPI, $controller, $q) {
       this.conferenceAPI = conferenceAPI;
       this.tokenAPI = tokenAPI;
       this.domainAPI = domainAPI;
-      this.webrtcFactory = webrtcFactory;
       this.$rootScope = $rootScope;
       this.scope = $rootScope.$new();
       this.$log = $log;
@@ -31,22 +205,6 @@ describe('The live-conference Angular module', function() {
         }
       };
 
-      this.webrtcFactory = {
-        get: function() {
-          return {
-            setRoomOccupantListener: function() {},
-            setRoomEntryListener: function() {},
-            setDisconnectListener: function() {},
-            joinRoom: function() {},
-            easyApp: function() {},
-            hangupAll: function() {},
-            setOnCall: function() {},
-            setOnHangup: function() {},
-            setSocketOptions: function() {}
-          };
-        }
-      };
-
       $controller('liveConferenceController', {
         $scope: this.scope,
         $log: this.$log,
@@ -54,7 +212,6 @@ describe('The live-conference Angular module', function() {
         conferenceAPI: this.conferenceAPI,
         domainAPI: this.domainAPI,
         tokenAPI: this.tokenAPI,
-        webrtcFactory: this.webrtcFactory,
         conference: this.conference
       });
     }));
@@ -64,57 +221,6 @@ describe('The live-conference Angular module', function() {
         done();
       };
       this.scope.invite({_id: 123});
-    });
-
-    it('$scope.performCall should hangupAll', function(done) {
-      this.scope.easyrtc.hangupAll = function() {
-        done();
-      };
-      this.scope.performCall('YOLO');
-    });
-
-    it('$scope.performCall should call the given user id', function(done) {
-      var user_id = 123;
-      this.scope.easyrtc.call = function(id) {
-        expect(id).to.equal(user_id);
-        done();
-      };
-      this.scope.easyrtc.hangupAll = function() {};
-      this.scope.performCall(user_id);
-    });
-
-    it('$scope.connect should create the easyRTC app if token is retrieved', function(done) {
-      var d = this.$q.defer();
-      d.resolve({token: '123'});
-
-      this.tokenAPI.getNewToken = function() {
-        return d.promise;
-      };
-
-      this.scope.easyrtc.easyApp = function() {
-        done();
-      };
-      this.scope.connect();
-      this.$rootScope.$digest();
-    });
-
-    it('$scope.connect should not create the easyRTC app if token is not retrieved', function(done) {
-      var d = this.$q.defer();
-      d.reject({data: 'ERROR'});
-
-      this.tokenAPI.getNewToken = function() {
-        return d.promise;
-      };
-
-      this.$log.error = function() {
-        done();
-      };
-
-      this.scope.easyrtc.easyApp = function() {
-        done(new Error());
-      };
-      this.scope.connect();
-      this.$rootScope.$digest();
     });
   });
 
