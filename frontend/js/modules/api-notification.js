87dabe8e0169987897e9f04764c8c4af9ae4df2d
Romain STG_PIGNOLET <rpignolet@linagora.com>
2014-09-01 19:27:02 +0200

    OR-513 Add the directive for the api notification

---
+++ frontend/js/modules/api-notification.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('esn.api-notification', ['esn.notification'])
+  .directive('apiNotification', ['$log', 'session', 'notificationFactory', 'livenotification',
+    function($log, session, notificationFactory, livenotification) {
+      return {
+        restrict: 'E',
+        link: function(scope) {
+
+          function liveNotificationHandler(msg) {
+            function displayNotification() {
+              return msg.target.some(function(item) {
+                return item === session.user._id;
+              });// && msg.parent;
+            }
+
+            if (displayNotification()) {
+              $log.debug('New notification of namespace /notifications with data', msg);
+
+              var text = msg.author + ' ' + msg.action + ' a ' + msg.object;
+              if (msg.link) {
+                text = 'Check it out on <a target="_blank" href=\"' + msg.link + '\">link</a>';
+              }
+              notificationFactory.strongInfo(msg.title || 'New notification', text);
+            }
+          }
+
+          var sio = livenotification('/notifications').on('notification', liveNotificationHandler);
+
+          scope.$on('$destroy', function() {
+            sio.removeListener('notification', liveNotificationHandler);
+          });
+        }
+      };
+    }]);
