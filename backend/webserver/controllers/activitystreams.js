e92d7a9dd5efd3b0810baeda8257c6608820da7c
Romain PIGNOLET <rpignolet@linagora.com>
2014-09-08 15:41:16 +0200

    OR-534 Widget update
    Widget is correctly update when the user click on the activity stream
    update notifier

--- backend/webserver/controllers/activitystreams.js
+++ backend/webserver/controllers/activitystreams.js
@@ -80,10 +80,11 @@ function get(req, res) {
     /*
      * Check if the tracker can be update :
      *  * req.user is mandatory
-     *  * req.query.before/after must not be set because the tracker must be update only when a user GET all the timelines entries
+     *  * req.query.before must not be set because the tracker must be update only when a user GET all the timelines entries
+     *  or if the req.query.after is defined
      *  * if result[0] is defined then there is at least 1 timeline entry in the activity stream
      */
-    if (req.user && !req.query.before && !req.query.after && result[0]) {
+    if (req.user && !req.query.before && result[0]) {
       tracker.updateLastTimelineEntryRead(req.user._id, activity_stream._id, result[0]._id, function(err) {});
     }
   });
