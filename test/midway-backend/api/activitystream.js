01b77b7192ea525ef7c35ffa9db94bcee22baae4
Romain PIGNOLET <rpignolet@linagora.com>
2014-09-12 11:56:08 +0200

    OR-534 Fix tests
    * Move a test from unit-storage to midway-backend because the test need
    the pubsub to be init.
    * Fix some tests due to core changes

--- test/midway-backend/api/activitystream.js
+++ test/midway-backend/api/activitystream.js
@@ -215,7 +215,9 @@ describe('The activitystreams routes', function() {
 
     describe('Tracker tests', function() {
       var domain;
+      var community;
       var user;
+      var user2;
       var activitystreamId;
 
       beforeEach(function(done) {
@@ -226,12 +228,14 @@ describe('The activitystreams routes', function() {
 
           domain = models.domain;
           user = models.users[0];
+          user2 = models.users[1];
 
           self.helpers.api.createCommunity('Node', user, domain, function(err, saved) {
             if (err) {
               return done(err);
             }
             activitystreamId = saved.activity_stream.uuid;
+            community = saved;
             done();
           });
         });
@@ -289,6 +293,39 @@ describe('The activitystreams routes', function() {
                   expect(res.body).to.exist;
                   expect(res.body._id).to.deep.equal(activitystreamId);
                   expect(res.body.unread_count).to.deep.equal(3);
+                  done();
+                });
+              });
+            });
+          });
+        });
+      });
+
+      it('should return 0 unread TimelineEntry for new user in community', function(done) {
+        var self = this;
+
+        var communityCore = require(this.testEnv.basePath + '/backend/core/community');
+
+        // Login
+        this.helpers.api.loginAsUser(webserver.application, user.emails[0], password, function(err, loggedInAsUser) {
+          // Add three Timeline Entry
+          self.helpers.api.applyMultipleTimelineEntries(activitystreamId, 3, 'post', function(err, models) {
+            if (err) { done(err); }
+
+            // Add the second user to the community
+            communityCore.join(community, user2, user2, function(err, updated) {
+              if (err) { done(err); }
+
+              self.helpers.api.loginAsUser(webserver.application, user2.emails[0], password, function(err, loggedInAsUser2) {
+                // Get the number of unread Timeline Entries for the second user
+                var req = loggedInAsUser2(request(webserver.application).get(
+                    '/api/activitystreams/' + activitystreamId + '/unreadcount'));
+                req.expect(200);
+                req.end(function(err, res) {
+                  expect(err).to.not.exist;
+                  expect(res.body).to.exist;
+                  expect(res.body._id).to.deep.equal(activitystreamId);
+                  expect(res.body.unread_count).to.deep.equal(0);
                   done();
                 });
               });
