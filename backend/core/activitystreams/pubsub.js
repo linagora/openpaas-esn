324562b189535ca92f58d63065f52f1c6ae010bc
Romain PIGNOLET <rpignolet@linagora.com>
2014-09-12 11:45:18 +0200

    OR-534 When a new user join a community, unread message is 0
    When a user join a community, query the activity stream in order to
    create the timeline entries tracker. If there is no timeline entry the
    tracker is not create and if there are one or more timeline entries then
    the tracker is create and set to the last timeline entry.

--- backend/core/activitystreams/pubsub.js
+++ backend/core/activitystreams/pubsub.js
@@ -3,6 +3,8 @@
 var pubsub = require('../pubsub').local;
 var logger = require('../logger');
 var activitystream = require('./index');
+var tracker = require('./tracker');
+var Community = require('mongoose').model('Community');
 var initialized = false;
 
 function createActivity(data, callback) {
@@ -24,12 +26,61 @@ function createActivity(data, callback) {
 }
 module.exports.createActivity = createActivity;
 
+function updateTimelineEntriesTracker(data, callback) {
+  if (!data) {
+    logger.warn('Can not create timeline entries tracker from null data');
+    return;
+  }
+
+  callback = callback || function(err, saved) {
+    if (err) {
+      logger.warn('Error while adding timeline entry tracker : ', + err.message);
+    } else {
+      if (saved) {
+        logger.debug('Timeline entries tracker has been created / updated into database : ' + saved._id);
+      }
+    }
+  };
+
+  Community.findOne({_id: data.community}, function(err, community) {
+    if (err) {
+      return callback(err);
+    }
+
+    var options = {
+      target: {
+        objectType: 'activitystream',
+        _id: community.activity_stream.uuid
+      },
+      limit: 1
+    };
+    activitystream.query(options, function(err, results) {
+      if (err) {
+        return callback(err);
+      }
+      if (! results || results.length === 0) {
+        return callback(null, null);
+      }
+
+      tracker.updateLastTimelineEntryRead(data.target, community.activity_stream.uuid, results[0]._id, function(err, saved) {
+        if (err) {
+          return callback(err);
+        }
+        return callback(null, saved);
+      });
+    });
+  });
+}
+
+module.exports.updateTimelineEntriesTracker = updateTimelineEntriesTracker;
+
 function init() {
   if (initialized) {
     logger.warn('Activity Stream Pubsub is already initialized');
     return;
   }
   pubsub.topic('message:activity').subscribe(createActivity);
+  pubsub.topic('community:join').subscribe(updateTimelineEntriesTracker);
   initialized = true;
 }
 module.exports.init = init;
