1ab12920cf92449e3f22481d3441aedb82d67213
Christophe Hamerling <christophe.hamerling@gmail.com>
2014-09-29 10:24:35 +0200

    OR-634 Map Directives

--- frontend/js/modules/maps.js
+++ frontend/js/modules/maps.js
@@ -23,5 +23,25 @@ angular.module('esn.maps', ['ngGeolocation'])
       reverse: reverse,
       getCurrentPosition: getCurrentPosition
     };
-  }]
-);
+  }])
+  .directive('mapGetDisplayCurrentPosition', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: '/views/modules/maps/getDisplayCurrentPosition.html'
+    }
+  })
+  .directive('displayPosition', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: {
+        position: '='
+      },
+      templateUrl: '/views/modules/maps/displayPosition.html',
+      controller: function(scope) {
+        console.log('Position to display', scope.position);
+      }
+    }
+  })
+;
