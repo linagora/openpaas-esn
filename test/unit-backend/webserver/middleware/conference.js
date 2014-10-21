edbac0778b8006c812fa2e76e300b464669fa3e0
Graham Crosmarie <gcrosmarie@linagora.com>
2014-10-21 09:13:21 +0200

    OR-443 add tests for canAddAttendee middleware

--- test/unit-backend/webserver/middleware/conference.js
+++ test/unit-backend/webserver/middleware/conference.js
@@ -186,4 +186,151 @@ describe('The conference middleware', function() {
     };
     middleware(req, {}, done);
   });
+
+  describe('canAddAttendee function', function() {
+    it('should send back HTTP 400 when user is not set in request', function(done) {
+      mockery.registerMock('../../core/conference', {});
+      var middleware = require(this.testEnv.basePath + '/backend/webserver/middleware/conference').canAddAttendee;
+      var req = {
+        conference: {}
+      };
+      var res = {
+        json: function(code) {
+          expect(code).to.equal(400);
+          done();
+        }
+      };
+      middleware(req, res);
+    });
+
+    it('should send back HTTP 400 when conference is not set in request', function(done) {
+      mockery.registerMock('../../core/conference', {});
+      var middleware = require(this.testEnv.basePath + '/backend/webserver/middleware/conference').canAddAttendee;
+      var req = {
+        user: {}
+      };
+      var res = {
+        json: function(code) {
+          expect(code).to.equal(400);
+          done();
+        }
+      };
+      middleware(req, res);
+    });
+
+    it('should send back HTTP 500 when conference#userIsConferenceCreator fails', function(done) {
+      mockery.registerMock('../../core/conference', {
+        userIsConferenceCreator: function(conference, user, callback) {
+          return callback(new Error());
+        }
+      });
+
+      var middleware = require(this.testEnv.basePath + '/backend/webserver/middleware/conference').canAddAttendee;
+      var req = {
+        user: {},
+        conference: {}
+      };
+      var res = {
+        json: function(code) {
+          expect(code).to.equal(500);
+          done();
+        }
+      };
+      middleware(req, res);
+    });
+
+    it('should call next when conference#userIsConferenceCreator returns true', function(done) {
+      mockery.registerMock('../../core/conference', {
+        userIsConferenceCreator: function(conference, user, callback) {
+          return callback(null, true);
+        }
+      });
+
+      var middleware = require(this.testEnv.basePath + '/backend/webserver/middleware/conference').canAddAttendee;
+      var req = {
+        user: {},
+        conference: {}
+      };
+      var res = {
+        json: function() {
+          done(new Error());
+        }
+      };
+      middleware(req, res, done);
+    });
+
+    it('should send back HTTP 403 when conference#userIsConferenceCreator returns false ' +
+      'and userIsConferenceAttendee return false', function(done) {
+      mockery.registerMock('../../core/conference', {
+        userIsConferenceCreator: function(conference, user, callback) {
+          return callback(null, false);
+        },
+        userIsConferenceAttendee: function(conference, user, callback) {
+          return callback(null, false);
+        }
+      });
+
+      var middleware = require(this.testEnv.basePath + '/backend/webserver/middleware/conference').canAddAttendee;
+      var req = {
+        user: {},
+        conference: {}
+      };
+      var res = {
+        json: function(code) {
+          expect(code).to.equal(403);
+          done();
+        }
+      };
+      middleware(req, res);
+    });
+
+    it('should send back HTTP 500 when conference#userIsConferenceCreator returns false ' +
+      'and userIsConferenceAttendee fails', function(done) {
+      mockery.registerMock('../../core/conference', {
+        userIsConferenceCreator: function(conference, user, callback) {
+          return callback(null, false);
+        },
+        userIsConferenceAttendee: function(conference, user, callback) {
+          return callback(new Error());
+        }
+      });
+
+      var middleware = require(this.testEnv.basePath + '/backend/webserver/middleware/conference').canAddAttendee;
+      var req = {
+        user: {},
+        conference: {}
+      };
+      var res = {
+        json: function(code) {
+          expect(code).to.equal(500);
+          done();
+        }
+      };
+      middleware(req, res);
+    });
+
+    it('should call next when conference#userIsConferenceCreator returns false ' +
+      'and userIsConferenceAttendee return true', function(done) {
+      mockery.registerMock('../../core/conference', {
+        userIsConferenceCreator: function(conference, user, callback) {
+          return callback(null, false);
+        },
+        userIsConferenceAttendee: function(conference, user, callback) {
+          return callback(null, true);
+        }
+      });
+
+      var middleware = require(this.testEnv.basePath + '/backend/webserver/middleware/conference').canAddAttendee;
+      var req = {
+        user: {},
+        conference: {}
+      };
+      var res = {
+        json: function(code) {
+          done(new Error());
+        }
+      };
+      middleware(req, res, done);
+    });
+  });
 });
