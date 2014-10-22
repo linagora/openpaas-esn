8cfa217b0b2f56b0d8dabd06ee52775746094b41
Stephen LE MAISTRE <slemaistre@linagora.com>
2014-10-22 16:03:22 +0200

    OR-687 the usernotification pubsub handles community:membership:invite and community:membership:request

--- backend/core/notification/pubsub.js
+++ backend/core/notification/pubsub.js
@@ -7,6 +7,29 @@ var async = require('async'),
     usernotification = require('./usernotification'),
     initialized = false;
 
+function createUserNotification(data, callback) {
+  if (!data) {
+    logger.warn('Can not create usernotification from null data');
+    return;
+  }
+  usernotification.addUserNotification(data, callback);
+}
+
+function onSuccessPublishIntoGlobal(callback) {
+  return function(err, result) {
+    if (err) {
+      logger.warn('Error while adding a usernotification : ', + err.message);
+      callback(err);
+    } else {
+      if (result) {
+        logger.debug('A new usernotification has been saved : ' + result._id);
+        globalpubsub.topic('usernotification:created').publish(result);
+      }
+      return callback(null);
+    }
+  };
+}
+
 function augmentToCommunityJoin(data, callback) {
   var notification = {
     subject: {objectType: 'user', id: data.author},
@@ -21,32 +44,62 @@ function augmentToCommunityJoin(data, callback) {
   return callback(null, notification);
 }
 
-function createUserNotification(data, callback) {
-  if (!data) {
-    logger.warn('Can not create usernotification from null data');
-    return;
-  }
-  usernotification.addUserNotification(data, callback);
-}
-
 function communityJoinHandler(data, callback) {
   async.waterfall([
     augmentToCommunityJoin.bind(null, data),
     createUserNotification
-  ], function(err, result) {
-    if (err) {
-      logger.warn('Error while adding a usernotification : ', + err.message);
-      callback(err);
-    } else {
-      if (result) {
-        logger.debug('A new usernotification has been saved : ' + result._id);
-        globalpubsub.topic('usernotification:created').publish(result);
-      }
-      return callback(null);
-    }
-  });
+  ],
+    onSuccessPublishIntoGlobal(callback));
 }
 module.exports.communityJoinHandler = communityJoinHandler;
+
+function augmentToMembershipInvite(data, callback) {
+  var notification = {
+    subject: {objectType: 'user', id: data.author},
+    verb: {label: 'ESN_MEMBERSHIP_INVITE', text: 'has invited you in'},
+    complement: {objectType: 'community', id: data.community},
+    context: null,
+    description: null,
+    icon: {objectType: 'icon', id: 'fa-users'},
+    category: 'community:membership:invite',
+    interactive: true,
+    target: [{objectType: 'user', id: data.target}]
+  };
+  return callback(null, notification);
+}
+
+function membershipInviteHandler(data, callback) {
+  async.waterfall([
+      augmentToMembershipInvite.bind(null, data),
+      createUserNotification
+    ],
+    onSuccessPublishIntoGlobal(callback));
+}
+module.exports.membershipInviteHandler = membershipInviteHandler;
+
+function augmentToMembershipRequest(data, callback) {
+  var notification = {
+    subject: {objectType: 'user', id: data.author},
+    verb: {label: 'ESN_MEMBERSHIP_REQUEST', text: 'requested membership on'},
+    complement: {objectType: 'community', id: data.community},
+    context: null,
+    description: null,
+    icon: {objectType: 'icon', id: 'fa-users'},
+    category: 'community:membership:request',
+    interactive: true,
+    target: [{objectType: 'community', id: data.community}]
+  };
+  return callback(null, notification);
+}
+
+function membershipRequestHandler(data, callback) {
+  async.waterfall([
+      augmentToMembershipRequest.bind(null, data),
+      createUserNotification
+    ],
+    onSuccessPublishIntoGlobal(callback));
+}
+module.exports.membershipRequestHandler = membershipRequestHandler;
 
 function init() {
   if (initialized) {
@@ -54,6 +107,8 @@ function init() {
     return;
   }
   localpubsub.topic('community:join').subscribe(communityJoinHandler);
+  localpubsub.topic('community:membership:invite').subscribe(membershipInviteHandler);
+  localpubsub.topic('community:membership:request').subscribe(membershipRequestHandler);
   initialized = true;
 }
 module.exports.init = init;
--- test/unit-backend/core/notification/pubsub.js
+++ test/unit-backend/core/notification/pubsub.js
@@ -43,12 +43,98 @@ describe('The notification pubsub module', function() {
         }
         expect(datastub).to.deep.equal({
           subject: {objectType: 'user', id: '123'},
-          verb: {label: 'ESN_COMMUNITY_JOIN', text: 'has join'},
+          verb: {label: 'ESN_COMMUNITY_JOIN', text: 'has joined'},
           complement: {objectType: 'community', id: '789'},
           context: null,
           description: null,
           icon: {objectType: 'icon', id: 'fa-users'},
           category: 'community:join',
+          target: [{objectType: 'community', id: '789'}]
+        });
+        expect(globalstub.topics['usernotification:created'].data[0]).to.equal('saved');
+        done();
+      });
+    });
+
+  });
+
+  describe('membershipInviteHandler method', function() {
+
+    it('should subscribe to community:join, save a augmented usernotification then forward it into global usernotification:created', function(done) {
+      var globalstub = {};
+      var datastub = {};
+      var data = {
+        author: '123',
+        target: '456',
+        community: '789'
+      };
+      var usernotificationMocked = {
+        addUserNotification: function(data, callback) {
+          datastub = data;
+          callback(null, 'saved');
+        }
+      };
+
+      this.helpers.mock.pubsub('../pubsub', {}, globalstub);
+      mockery.registerMock('./usernotification', usernotificationMocked);
+
+      var module = require(this.testEnv.basePath + '/backend/core/notification/pubsub');
+      module.membershipInviteHandler(data, function(err) {
+        if (err) {
+          return done(err);
+        }
+        expect(datastub).to.deep.equal({
+          subject: {objectType: 'user', id: '123'},
+          verb: {label: 'ESN_MEMBERSHIP_INVITE', text: 'has invited you in'},
+          complement: {objectType: 'community', id: '789'},
+          context: null,
+          description: null,
+          icon: {objectType: 'icon', id: 'fa-users'},
+          category: 'community:membership:invite',
+          interactive: true,
+          target: [{objectType: 'user', id: '456'}]
+        });
+        expect(globalstub.topics['usernotification:created'].data[0]).to.equal('saved');
+        done();
+      });
+    });
+
+  });
+
+  describe('membershipRequestHandler method', function() {
+
+    it('should subscribe to community:join, save a augmented usernotification then forward it into global usernotification:created', function(done) {
+      var globalstub = {};
+      var datastub = {};
+      var data = {
+        author: '123',
+        target: '456',
+        community: '789'
+      };
+      var usernotificationMocked = {
+        addUserNotification: function(data, callback) {
+          datastub = data;
+          callback(null, 'saved');
+        }
+      };
+
+      this.helpers.mock.pubsub('../pubsub', {}, globalstub);
+      mockery.registerMock('./usernotification', usernotificationMocked);
+
+      var module = require(this.testEnv.basePath + '/backend/core/notification/pubsub');
+      module.membershipRequestHandler(data, function(err) {
+        if (err) {
+          return done(err);
+        }
+        expect(datastub).to.deep.equal({
+          subject: {objectType: 'user', id: '123'},
+          verb: {label: 'ESN_MEMBERSHIP_REQUEST', text: 'requested membership on'},
+          complement: {objectType: 'community', id: '789'},
+          context: null,
+          description: null,
+          icon: {objectType: 'icon', id: 'fa-users'},
+          category: 'community:membership:request',
+          interactive: true,
           target: [{objectType: 'community', id: '789'}]
         });
         expect(globalstub.topics['usernotification:created'].data[0]).to.equal('saved');
