5c9266380cdf95d566ce3b132c7ee16404f0251b
Graham Crosmarie <gcrosmarie@linagora.com>
2014-10-28 11:55:32 +0100

    OR-730 add tests for communityInjectionsWidget directive

--- test/unit-frontend/modules/community.js
+++ test/unit-frontend/modules/community.js
@@ -2510,4 +2510,95 @@ describe('The Community Angular module', function() {
       });
     });
   });
+
+  describe('the communityInjectionsWidget directive', function() {
+
+    beforeEach(angular.mock.inject(function($rootScope, $compile) {
+      this.$rootScope = $rootScope;
+      this.$compile = $compile;
+      this.scope = $rootScope.$new();
+      this.scope.community = {
+        _id: '123'
+      };
+      this.anchorId = 'testAnchor';
+      this.html = '<div community-injections-widget=' + this.anchorId + '/>';
+    }));
+
+    it('should hide the element if no anchor is defined', function() {
+      var noAnchorHtml = '<div community-injections-widget />';
+      var element = this.$compile(noAnchorHtml)(this.scope);
+      this.scope.$digest();
+      expect(element[0].getAttribute('style')).to.contain('display: none');
+    });
+
+    it('should hide the element if community has no injections', function() {
+      var element = this.$compile(this.html)(this.scope);
+      this.scope.$digest();
+      expect(element[0].getAttribute('style')).to.contain('display: none');
+    });
+
+    it('should hide the element if community has no injections for this anchor', function() {
+      this.scope.community.injections = [
+        {
+          key: 'otherAnchor',
+          values: [
+            { directive: 'directive3', attributes: [{ name: 'att4', value: 'value4' }] }
+          ]
+        }
+      ];
+      var element = this.$compile(this.html)(this.scope);
+      this.scope.$digest();
+      expect(element[0].getAttribute('style')).to.contain('display: none');
+    });
+
+    it('should build directive form injections and show the element', function() {
+      this.scope.community.injections = [
+        {
+          key: this.anchorId,
+          values: [
+            { directive: 'directive1', attributes: [{ name: 'att1', value: 'value1' }] }
+          ]
+        },
+        {
+          key: this.anchorId,
+          values: [
+            { directive: 'directive2', attributes: [{ name: 'att2', value: 'value2' }, { name: 'att3', value: 'value3' }] }
+          ]
+        },
+        {
+          key: 'otherAnchor',
+          values: [
+            { directive: 'directive3', attributes: [{ name: 'att4', value: 'value4' }] }
+          ]
+        }
+      ];
+      var element = this.$compile(this.html)(this.scope);
+      this.scope.$digest();
+      expect(element[0].getAttribute('style')).to.not.contain('display: none');
+
+      var generatedHtml = element.children();
+      expect(generatedHtml.length).to.equal(4);
+
+      var directive1 = generatedHtml[0];
+      expect(directive1.nodeName.toLowerCase()).to.equal('directive1');
+      expect(directive1.getAttribute('att1')).to.equal('value1');
+
+      var firstSpace = generatedHtml[1];
+      expect(firstSpace.nodeName.toLowerCase()).to.equal('div');
+      expect(firstSpace.getAttribute('class')).to.equal('vertiz-space');
+
+      var directive2 = generatedHtml[2];
+      expect(directive2.nodeName.toLowerCase()).to.equal('directive2');
+      expect(directive2.getAttribute('att2')).to.equal('value2');
+      expect(directive2.getAttribute('att3')).to.equal('value3');
+
+      var secondSpace = generatedHtml[3];
+      expect(secondSpace.nodeName.toLowerCase()).to.equal('div');
+      expect(secondSpace.getAttribute('class')).to.equal('vertiz-space');
+
+      expect(element.html()).to.not.contain('directive3');
+      expect(element.html()).to.not.contain('att4');
+      expect(element.html()).to.not.contain('value4');
+    });
+  });
 });
