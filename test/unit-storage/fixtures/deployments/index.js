593c6e40de7129a989c3370560f90e2dd00a8fa4
Michael Bailly <mbailly@linagora.com>
2014-10-29 11:41:15 +0100

    Revert "OR-737 Add the core for searching users not in specific community"
    This reverts commit a3091e0fc7a6a47f26340780d689368e97559f21.

--- test/unit-storage/fixtures/deployments/index.js
+++ test/unit-storage/fixtures/deployments/index.js
@@ -37,43 +37,6 @@ module.exports.linagora_test_domain = function() {
 
 };
 
-module.exports.linagora_test_domain2 = function() {
-
-  return {
-    domain: {
-      name: 'TestDomain2',
-      company_name: 'Linagora2'
-    },
-    users: [
-      {
-        password: 'secret',
-        firstname: 'a2 ',
-        lastname: 'user12',
-        emails: ['user12@lng.net']
-      },
-      {
-        password: 'secret',
-        firstname: 'b2',
-        lastname: 'user22',
-        emails: ['user22@lng.net']
-      },
-      {
-        password: 'secret',
-        firstname: 'c2',
-        lastname: 'user32',
-        emails: ['user32@lng.net']
-      },
-      {
-        password: 'secret',
-        firstname: 'd2',
-        lastname: 'user42',
-        emails: ['user42@linagora.com']
-      }
-    ]
-  };
-
-};
-
 module.exports.linagora_test_cases = function() {
 
   return {
