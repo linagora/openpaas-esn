88573ace828c178bac6689bc0eccdd954539af48
Christophe Hamerling <christophe.hamerling@gmail.com>
2014-11-04 23:35:04 +0100

    OR-634 Add maps templates in karma

--- test/config/karma.conf.js
+++ test/config/karma.conf.js
@@ -43,7 +43,8 @@ module.exports = function(config) {
       'frontend/views/modules/message/**/*.jade',
       'frontend/views/modules/community/**/*.jade',
       'frontend/views/modules/profile/**/*.jade',
-      'frontend/views/modules/user-notification/**/*.jade'
+      'frontend/views/modules/user-notification/**/*.jade',
+      'frontend/views/modules/maps/**/*.jade'
     ],
 
     frameworks: ['mocha'],
