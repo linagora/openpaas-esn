5310bb7a4041ed886b030afb6f2d330de564adc9
Graham Crosmarie <gcrosmarie@linagora.com>
2014-11-12 14:48:30 +0100

    OR-756 wsserver now always publishes created user notification to their unique target user

--- backend/wsserver/notification/usernotifications.js
+++ backend/wsserver/notification/usernotifications.js
@@ -2,26 +2,18 @@
 
 var pubsub = require('../../core/pubsub').global,
     logger = require('../../core/logger'),
-    community = require('../../core/community'),
-    helper = require('../helper/socketio'),
-    async = require('async');
+    helper = require('../helper/socketio');
 
 var initialized = false;
 
-var NAMESPACE = '/usernotifications';
+var NAMESPACE = '/usernotification';
 
 var TOPIC_CREATED = 'usernotification:created';
 var TOPIC_UPDATED = 'usernotification:updated';
 
-var NOTIFICATION_EVENT_CREATED = 'created';
-var NOTIFICATION_EVENT_UPDATED = 'updated';
+var NOTIFICATION_EVENT_CREATED = 'usernotification:created';
+var NOTIFICATION_EVENT_UPDATED = 'usernotification:updated';
 
-var NOTIFICATION_CATEGORIES_FOR_ALL = [
-  'external'
-];
-var NOTIFICATION_CATEGORIES_FOR_MANAGERS = [
-  'community:membership:request'
-];
 
 function notify(io, user, event, usernotification) {
   var clientSockets = helper.getUserSocketsFromNamespace(user, io.of(NAMESPACE).sockets);
@@ -34,59 +26,8 @@ function notify(io, user, event, usernotification) {
   });
 }
 
-function isForAll(usernotification) {
-  return NOTIFICATION_CATEGORIES_FOR_ALL.indexOf(usernotification.category) >= 0;
-}
-
-function isForManagers(usernotification) {
-  return NOTIFICATION_CATEGORIES_FOR_MANAGERS.indexOf(usernotification.category) >= 0;
-}
-
-function addUsersToCache(err, users, finds, callback) {
-  if (err) {
-    return callback(err);
-  }
-  finds.forEach(function(member) {
-    users[member.user] = true;
-  });
-  return callback();
-}
-
 function handler(io, event, usernotification) {
-  if (usernotification.parentId) {
-    return;
-  }
-  var users = {};
-
-  function filterByUserAndCommunity(target, callback) {
-    if (target.objectType === 'user') {
-      users[target.id] = true;
-      callback();
-    } else if (target.objectType === 'community') {
-      if (isForAll(usernotification)) {
-        community.getMembers(target.id, null, function(err, members) {
-          addUsersToCache(err, users, members, callback);
-        });
-      } else if (isForManagers(usernotification)) {
-        community.getManagers(target.id, null, function(err, managers) {
-          addUsersToCache(err, users, managers, callback);
-        });
-      }
-    }
-  }
-
-  function onEndOfSeries(err) {
-    if (err) {
-      return;
-    }
-    for (var user in users) {
-      if (users.hasOwnProperty(user)) {
-        notify(io, user, event, usernotification);
-      }
-    }
-  }
-
-  async.eachSeries(usernotification.target, filterByUserAndCommunity, onEndOfSeries);
+  notify(io, usernotification.target, event, usernotification);
 }
 
 function init(io) {
@@ -101,6 +42,22 @@ function init(io) {
   pubsub.topic(TOPIC_UPDATED).subscribe(function(usernotification) {
     handler(io, NOTIFICATION_EVENT_UPDATED, usernotification);
   });
+
+  io.of(NAMESPACE)
+    .on('connection', function(socket) {
+
+      logger.info('User', socket.handshake.query.user, ': new connection on ' + NAMESPACE);
+
+      socket.on('subscribe', function(uuid) {
+        logger.info('User', socket.handshake.query.user, ': joining room ', NAMESPACE, '/', uuid);
+        socket.join(uuid);
+      });
+
+      socket.on('unsubscribe', function(uuid) {
+        logger.info('User', socket.handshake.query.user, ': leaving room ', NAMESPACE, '/', uuid);
+        socket.leave(uuid);
+      });
+    });
 
   initialized = true;
 }
