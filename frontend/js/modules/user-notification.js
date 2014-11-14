d94298e65d4663ea701cbdf197c6c21084bd41f4
Romain PIGNOLET <rpignolet@linagora.com>
2014-11-14 17:52:48 +0100

    OR-797 Set `acknowledged` to `true` for accepted/declined notification

--- frontend/js/modules/user-notification.js
+++ frontend/js/modules/user-notification.js
@@ -177,17 +177,27 @@ angular.module('esn.user-notification',
       };
     }
   ])
-  .controller('requestMembershipActionNotificationController', ['$scope', 'objectTypeResolver', function($scope, objectTypeResolver) {
+  .controller('requestMembershipActionNotificationController', ['$scope', 'objectTypeResolver', 'userNotificationAPI', function($scope, objectTypeResolver, userNotificationAPI) {
     $scope.error = false;
     $scope.loading = true;
     objectTypeResolver.resolve($scope.notification.complement.objectType, $scope.notification.complement.id)
       .then(function(result) {
         $scope.community = result.data;
+
+        userNotificationAPI.setAcknowledged($scope.notification._id, true).then(
+          function() {
+            $scope.notification.acknowledged = true;
+          },
+          function(error) {
+            $scope.error = error;
+          }
+        ).finally (function() {
+          $scope.loading = false;
+        });
+
       }, function() {
         $scope.error = true;
-      }).finally (function() {
-      $scope.loading = false;
-    });
+      });
   }])
   .directive('notificationTemplateDisplayer', function() {
     return {
