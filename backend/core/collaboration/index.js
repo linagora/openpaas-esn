ac0234b0c5b3398a3a7354fdfa3c1232fcc968ea
Michael Bailly <mbailly@linagora.com>
2014-11-26 19:30:54 +0100

    OR-834 Add registration facilities for collaboration derived models
    Those methods can be consumed by third party modules to declare a new collaboration model

--- backend/core/collaboration/index.js
+++ backend/core/collaboration/index.js
@@ -16,4 +16,15 @@ function query(objectType, q, callback) {
   return Model.find(q, callback);
 }
 
+function registerCollaborationModel(name, modelName, schema) {
+  if (collaborationModels[name]) {
+    throw new Error('Collaboration model ' + name + 'is already registered');
+  }
+  var model = mongoose.model(modelName, schema);
+  collaborationModels[name] = modelName;
+  return model;
+}
+
 module.exports.query = query;
+module.exports.schemaBuilder = require('../db/mongo/models/base-collaboration');
+module.exports.registerCollaborationModel = registerCollaborationModel;
