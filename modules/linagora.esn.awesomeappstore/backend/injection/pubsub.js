7ecb847b3327fedeb8d144fdb2ae1edf250fb58a
Stephen LE MAISTRE <slemaistre@linagora.com>
2014-12-15 11:03:48 +0100

    OR-793 create the pubsub manager in the appstore module for the futur injection module

---
+++ modules/linagora.esn.awesomeappstore/backend/injection/pubsub.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var initialized, injectionModule, loggerModule;
+var callback = function(err) {
+  if (err) {
+    loggerModule.error('Failed injection', err.message);
+  } else {
+    loggerModule.info('Successful injection');
+  }
+};
+
+var getInjectionsWithSource = function(appId, injections) {
+  return injections.map(function(injection) {
+    var newInjection = injection.toJSON();
+    newInjection.source = { objectType: 'application', id: appId };
+    return newInjection;
+  });
+};
+
+function hasTargetInjections(application) {
+  return application.targetInjections && application.targetInjections.length !== 0;
+}
+
+function hasDomainInjections(application) {
+  return application.domainInjections && application.domainInjections.length !== 0;
+}
+
+function applicationInstalledHandler(event) {
+  if (!event.application || !event.target || !event.domain) {
+    return;
+  }
+
+  if (hasTargetInjections(event.application)) {
+    injectionModule.inject(event.target, getInjectionsWithSource(event.application._id, event.application.targetInjections), callback);
+  }
+
+  if (hasDomainInjections(event.application)) {
+    injectionModule.inject(event.domain, getInjectionsWithSource(event.application._id, event.application.domainInjections), callback);
+  }
+}
+
+function applicationUninstalledHandler(event) {
+  if (!event.application || !event.target || !event.domains) {
+    return;
+  }
+
+  if (hasTargetInjections(event.application)) {
+    injectionModule.removeInjections(event.application, event.target, callback);
+  }
+
+  if (hasDomainInjections(event.application)) {
+    event.domains.forEach(function(domain) {
+      injectionModule.removeInjections(event.application, domain, callback);
+    });
+  }
+}
+
+function init(localpubsub, injectModule, logger) {
+  if (initialized) {
+    logger.warn('Appstore Pubsub is already initialized');
+    return;
+  }
+  injectionModule = injectModule;
+  loggerModule = logger;
+  localpubsub.topic('appstore:application:installed').subscribe(applicationInstalledHandler);
+  localpubsub.topic('appstore:application:uninstalled').subscribe(applicationUninstalledHandler);
+  initialized = true;
+}
+module.exports.init = init;
