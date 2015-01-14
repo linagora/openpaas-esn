4028015a5b7a914fedb79ae96ff5d3eeffebe82d
Stephen LE MAISTRE <slemaistre@linagora.com>
2015-01-14 17:35:06 +0100

    OR-858 duplicate the communityInviteUser directive into the collaboration module

--- frontend/js/modules/collaboration.js
+++ frontend/js/modules/collaboration.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('esn.collaboration', ['restangular'])
+  .factory('collaborationService', function() {
+
+    function isManager(collaboration, user) {
+      return collaboration.creator === user._id;
+    }
+
+    return {
+      isManager: isManager
+    };
+  })
   .factory('collaborationAPI', ['Restangular', function(Restangular) {
     function getWhereMember(tuple) {
       return Restangular.all('collaborations/membersearch').getList(tuple);
@@ -18,11 +28,22 @@ angular.module('esn.collaboration', ['restangular'])
       return Restangular.one('collaborations').one(objectType, id).getList('externalcompanies', options);
     }
 
+    function getInvitablePeople(objectType, id, options) {
+      var query = options || {};
+      return Restangular.one('collaborations').one(objectType, id).all('invitablepeople').getList(query);
+    }
+
+    function requestMembership(objectType, id, member) {
+      return Restangular.one('collaborations').one(objectType, id).one('membership', member).put();
+    }
+
     return {
       getMembers: getMembers,
       getMember: getMember,
       getWhereMember: getWhereMember,
-      getExternalCompanies: getExternalCompanies
+      getExternalCompanies: getExternalCompanies,
+      getInvitablePeople: getInvitablePeople,
+      requestMembership: requestMembership
     };
   }])
   .controller('collaborationListController', ['$scope', 'domain', 'user', function($scope, domain, user) {
@@ -112,4 +133,140 @@ angular.module('esn.collaboration', ['restangular'])
         }
       }
     };
-  });
+  })
+  .directive('collaborationInviteUsers', ['$q', 'collaborationAPI', 'collaborationService', 'session',
+    function($q, collaborationAPI, collaborationService, session) {
+      return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+          objectType: '@',
+          collaboration: '='
+        },
+        templateUrl: '/views/modules/collaboration/collaboration-invite-users.html',
+        link: function($scope, $element) {
+          $scope.placeholder = 'User name';
+          $scope.displayProperty = 'displayName';
+          $scope.running = false;
+
+          $scope.getErrorDiv = function() {
+            return $element.find('[error-container]');
+          };
+          $scope.getRunningDiv = function() {
+            return $element.children('.form-container').children('form').find('[running-container]');
+          };
+          $scope.getButtonContent = function() {
+            return $element.children('.form-container').children('form').find('[button-content]');
+          };
+          $scope.getSuccessDiv = function() {
+            return $element.children('.form-container').children('form').find('[success-container]');
+          };
+
+          $scope.showErrorMessage = function() {
+            $scope.getErrorDiv().removeClass('hidden');
+          };
+          $scope.hideErrorMessage = function() {
+            $scope.getErrorDiv().addClass('hidden');
+          };
+
+          $scope.showRunning = function() {
+            $scope.getRunningDiv().removeClass('hidden');
+            $scope.getButtonContent().addClass('hidden');
+          };
+          $scope.hideRunning = function() {
+            $scope.getRunningDiv().addClass('hidden');
+            $scope.getButtonContent().removeClass('hidden');
+          };
+
+          $scope.showSuccessMessage = function() {
+            $scope.getSuccessDiv().removeClass('hidden');
+          };
+          $scope.hideSuccessMessage = function() {
+            $scope.getSuccessDiv().addClass('hidden');
+          };
+
+          $scope.resetMessages = function() {
+            $scope.hideErrorMessage();
+            $scope.hideSuccessMessage();
+          };
+
+          $scope.getInvitablePeople = function(query) {
+            $scope.query = query;
+            var deferred = $q.defer();
+            collaborationAPI.getInvitablePeople($scope.objectType, $scope.collaboration._id, {search: query, limit: 5}).then(
+              function(response) {
+                response.data.forEach(function(user) {
+                  if (user.firstname && user.lastname) {
+                    user.displayName = user.firstname + ' ' + user.lastname;
+                  }
+                  else {
+                    user.displayName = user.emails[0];
+                  }
+                  $scope.query = '';
+                });
+                deferred.resolve(response);
+              },
+              function(error) {
+                deferred.resolve(error);
+              }
+            );
+            return deferred.promise;
+          };
+
+          $scope.inviteUsers = function() {
+            $scope.hideSuccessMessage();
+            $scope.hideErrorMessage();
+            $scope.noUser = false;
+            $scope.invalidUser = false;
+            if ($scope.query && $scope.query !== '') {
+              $scope.invalidUser = $scope.query;
+              $scope.showErrorMessage();
+              if (!$scope.users || $scope.users.length === 0) {
+                $scope.query = '';
+                return;
+              }
+            } else if (!$scope.users || $scope.users.length === 0) {
+              $scope.noUser = true;
+              $scope.showErrorMessage();
+              return;
+            }
+            if ($scope.running) {
+              return;
+            }
+            $scope.resetMessages();
+            $scope.running = true;
+            $scope.showRunning();
+
+            var promises = [];
+            $scope.users.forEach(function(user) {
+              promises.push(collaborationAPI.requestMembership($scope.objectType, $scope.collaboration._id, user._id));
+            });
+
+            $q.all(promises).then(
+              function() {
+                $scope.users = [];
+                $scope.running = false;
+                $scope.hideRunning();
+                $scope.showSuccessMessage();
+                if ($scope.query && $scope.query !== '') {
+                  $scope.invalidUser = $scope.query;
+                  $scope.showErrorMessage();
+                }
+              },
+              function(error) {
+                $scope.users = [];
+                $scope.error = error.data;
+                $scope.running = false;
+                $scope.hideRunning();
+                $scope.showErrorMessage();
+              }
+            );
+          };
+
+          if (collaborationService.isManager($scope.collaboration, session.user)) {
+            $element.removeClass('hidden');
+          }
+        }
+      };
+    }]);
+
