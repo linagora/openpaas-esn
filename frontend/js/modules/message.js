5924d6def25252811a79609f34708ba24f856314
Christophe Hamerling <christophe.hamerling@gmail.com>
2015-01-27 15:18:58 +0100

    OR-849 Remov not so useful default flag

--- frontend/js/modules/message.js
+++ frontend/js/modules/message.js
@@ -527,7 +527,7 @@ angular.module('esn.message', ['esn.maps', 'esn.file', 'esn.calendar', 'esn.back
           }
         }
         if ($scope.activitystream) {
-          $scope.writable = $scope.activitystream.writable || false;
+          $scope.writable = $scope.activitystream.writable;
         } else {
           $scope.writable = false;
         }
