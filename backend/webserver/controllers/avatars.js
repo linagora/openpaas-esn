e67d1ed99619649f81a3e5ae2c094cf7849ef648
Christophe Hamerling <christophe.hamerling@gmail.com>
2015-01-29 16:22:14 +0100

    OR-1056 Add the image avatar in /api/avatars

--- backend/webserver/controllers/avatars.js
+++ backend/webserver/controllers/avatars.js
@@ -3,6 +3,7 @@
 var userModule = require('../../core/user');
 var communityController = require('./communities');
 var userController = require('./users');
+var imageModule = require('../../core/image');
 
 function getUserAvatarFromEmail(req, res) {
   userModule.findByEmail(req.query.email, function(err, user) {
@@ -29,6 +30,30 @@ function getCommunityAvatar(req, res) {
   });
 }
 
+function getAvatar(req, res) {
+  if (!req.query.id) {
+    return res.json(400, { error: { status: 400, message: 'Bad request', details: 'Avatar id is mandatory'}});
+  }
+
+  imageModule.getAvatar(req.query.id, req.query.format, function(err, fileStoreMeta, readable) {
+    if (err) {
+      return res.redirect('/images/activitystream.png');
+    }
+
+    if (!readable) {
+      return res.redirect('/images/activitystream.png');
+    }
+
+    if (req.headers['if-modified-since'] && Number(new Date(req.headers['if-modified-since']).setMilliseconds(0)) === Number(fileStoreMeta.uploadDate.setMilliseconds(0))) {
+      return res.send(304);
+    } else {
+      res.header('Last-Modified', fileStoreMeta.uploadDate);
+      res.status(200);
+      return readable.pipe(res);
+    }
+  });
+}
+
 module.exports.get = function(req, res) {
   if (!req.query.objectType) {
     return res.json(400, { error: { status: 400, message: 'Bad request', details: 'objectType parameter is mandatory'}});
@@ -40,6 +65,10 @@ module.exports.get = function(req, res) {
 
   if (req.query.objectType === 'community') {
     return getCommunityAvatar(req, res);
+  }
+
+  if (req.query.objectType === 'image') {
+    return getAvatar(req, res);
   }
 
   return res.json(400, { error: { status: 400, message: 'Bad request', details: 'Unknown objectType parameter'}});
