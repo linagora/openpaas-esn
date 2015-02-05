7fadebb0eb1667766c80343e4a71c56dba412dd9
Graham Crosmarie <gcrosmarie@linagora.com>
2015-02-05 10:58:58 +0100

    OR-1109 update target helper to match current community member data model

--- backend/helpers/targets.js
+++ backend/helpers/targets.js
@@ -42,7 +42,7 @@ module.exports.getUserIds = function(targets, callback) {
           return callback(err);
         }
         members.forEach(function(member) {
-          addUsersIfNotFoundOrContextUndefined(member.user._id.toString(), target.id);
+          addUsersIfNotFoundOrContextUndefined(member.id.toString(), target.id);
         });
         callback();
       });
