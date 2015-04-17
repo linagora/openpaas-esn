02b2b3e3131617a2d13cc0621696af9e279f057f
Christophe Hamerling <christophe.hamerling@gmail.com>
2015-04-17 14:34:22 +0200

    OR-1185 Add more tests to array helper

--- test/unit-backend/helpers/array.js
+++ test/unit-backend/helpers/array.js
@@ -5,9 +5,18 @@ var expect = require('chai').expect;
 describe('The array helper', function() {
   describe('The isNullOrEmpty fn', function() {
 
-    it('should return true when null or undefined', function() {
+    it('should return true when null', function() {
       var module = this.helpers.requireBackend('helpers/array');
       expect(module.isNullOrEmpty(null)).to.be.true;
+    });
+
+    it('should return true when undefined', function() {
+      var module = this.helpers.requireBackend('helpers/array');
+      expect(module.isNullOrEmpty()).to.be.true;
+    });
+
+    it('should return true when undefined empty', function() {
+      var module = this.helpers.requireBackend('helpers/array');
       expect(module.isNullOrEmpty([])).to.be.true;
     });
 
