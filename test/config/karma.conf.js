6e9c2f3d89ad685e2a052d7d55e4c47104bc250f
Stéphen Le Maistre <slemaistre@linagora.com>
2015-06-05 14:32:02 +0200

    OR-1315 remove warning for unit-frontend tests

--- test/config/karma.conf.js
+++ test/config/karma.conf.js
@@ -37,7 +37,6 @@ module.exports = function(config) {
       'frontend/components/angular-uuid4/angular-uuid4.min.js',
       'frontend/components/localforage/dist/localforage.min.js',
       'frontend/components/angular-localforage/dist/angular-localForage.js',
-      'node_modules/easyrtc/api/easyrtc.js',
       'node_modules/async/lib/async.js',
       'node_modules/chai-jquery/chai-jquery.js',
       'frontend/components/angular-bootstrap-switch/dist/angular-bootstrap-switch.js',
@@ -62,9 +61,12 @@ module.exports = function(config) {
       'frontend/views/modules/maps/**/*.jade',
       'frontend/views/modules/domain/**/*.jade',
 
-      // fixtures
-      'test/unit-frontend/fixtures/**'
+      {pattern: "frontend/images/*.png", watched: false, included: false, served: true}
     ],
+
+    proxies: {
+       "/images/": "http://localhost:9876/images/"
+    },
 
     frameworks: ['mocha'],
     colors: true,
