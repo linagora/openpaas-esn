843f366197c64154daddeb5c01f9576491960719
Christophe Hamerling <christophe.hamerling@gmail.com>
2015-06-11 11:13:23 +0200

    OR-1342 Sort original array

--- test/unit-frontend/modules/array-helper.js
+++ test/unit-frontend/modules/array-helper.js
@@ -34,19 +34,19 @@ describe('The Array Helper module', function() {
 
       it('should order array', function() {
 
-        var data = [
-          {firstName: 'DBC', lastName: 'DEF'},
-          {firstName: 'aBC', lastName: 'DEF'},
-          {firstName: 'EBC', lastName: 'DEF'},
-          {firstName: 'zBC', lastName: 'DEF'}
-        ];
+        var d = {firstName: 'DBC', lastName: 'DEF'};
+        var a = {firstName: 'aBC', lastName: 'DEF'};
+        var e = {firstName: 'EBC', lastName: 'DEF'};
+        var z = {firstName: 'zBC', lastName: 'DEF'};
+
+        var data = [d, a, e, z];
 
         var result = this.arrayHelperService.sortHashArrayBy(data, 'firstName');
         expect(result).to.have.length(data.length);
-        expect(result[0]).to.deep.equal(data[1]);
-        expect(result[1]).to.deep.equal(data[0]);
-        expect(result[2]).to.deep.equal(data[2]);
-        expect(result[3]).to.deep.equal(data[3]);
+        expect(result[0]).to.deep.equal(a);
+        expect(result[1]).to.deep.equal(d);
+        expect(result[2]).to.deep.equal(e);
+        expect(result[3]).to.deep.equal(z);
       });
     });
   });
