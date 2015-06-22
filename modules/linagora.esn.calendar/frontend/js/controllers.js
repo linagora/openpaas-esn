f4ede2f499176e06ca37722bdd7c4ebf257bded6
Stéphen Le Maistre <slemaistre@linagora.com>
2015-06-22 11:26:22 +0200

    OR-1368 create a contants.js file in the calendar module

--- modules/linagora.esn.calendar/frontend/js/controllers.js
+++ modules/linagora.esn.calendar/frontend/js/controllers.js
@@ -1,46 +1,6 @@
 'use strict';
 
 angular.module('esn.calendar')
-  .constant('COMMUNITY_UI_CONFIG', {
-    calendar: {
-      height: 450,
-      editable: true,
-      timezone: 'local',
-      forceEventDuration: true,
-      weekNumbers: true,
-      firstDay: 1,
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay'
-      }
-    }
-  })
-  .constant('USER_UI_CONFIG', {
-    calendar: {
-      defaultView: 'agendaWeek',
-      height: 450,
-      editable: true,
-      selectable: true,
-      timezone: 'local',
-      forceEventDuration: true,
-      weekNumbers: true,
-      firstDay: 1,
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay'
-      },
-      // TODO: i18n
-      buttonText: {
-        today: 'Today',
-        month: 'Month',
-        week: 'Week',
-        day: 'Day'
-      },
-      handleWindowResize: false
-    }
-  })
 
   .controller('eventFormController', ['$rootScope', '$scope', '$alert', 'dateService', 'calendarService', 'moment', 'notificationFactory',
     function($rootScope, $scope, $alert, dateService, calendarService, moment, notificationFactory) {
