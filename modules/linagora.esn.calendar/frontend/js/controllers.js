60a63c74b7ea813027cfa2c4200968b582315a34
Stéphen Le Maistre <slemaistre@linagora.com>
2015-07-03 17:59:25 +0200

    OR-1287 reindent calendar/controllers.js

--- modules/linagora.esn.calendar/frontend/js/controllers.js
+++ modules/linagora.esn.calendar/frontend/js/controllers.js
@@ -178,100 +178,100 @@ angular.module('esn.calendar')
     $scope.uiConfig = USER_UI_CONFIG;
   })
   .controller('calendarController', function($scope, $rootScope, $window, $modal, $timeout, uiCalendarConfig, calendarService, calendarUtils, eventService, notificationFactory, calendarEventSource,  livenotification) {
-      $scope.eventSources = [calendarEventSource($scope.calendarId)];
+    $scope.eventSources = [calendarEventSource($scope.calendarId)];
 
-      var windowJQuery = angular.element($window);
+    var windowJQuery = angular.element($window);
 
-      $scope.resizeCalendarHeight = function() {
-        var calendar = uiCalendarConfig.calendars[$scope.calendarId];
-        calendar.fullCalendar('option', 'height', windowJQuery.height() - calendar.offset().top - 10);
-      };
+    $scope.resizeCalendarHeight = function() {
+      var calendar = uiCalendarConfig.calendars[$scope.calendarId];
+      calendar.fullCalendar('option', 'height', windowJQuery.height() - calendar.offset().top - 10);
+    };
 
-      $scope.eventClick = function(event) {
-        $scope.event = event;
-        $scope.modal = $modal({scope: $scope, template: '/calendar/views/partials/event-create-quick-form-modal', backdrop: 'static'});
-      };
+    $scope.eventClick = function(event) {
+      $scope.event = event;
+      $scope.modal = $modal({scope: $scope, template: '/calendar/views/partials/event-create-quick-form-modal', backdrop: 'static'});
+    };
 
-      $scope.eventDropAndResize = function(event) {
-        var path = '/calendars/' + $scope.calendarId + '/events/' + event.id + '.ics';
-        calendarService.modify(path, event).then(function() {
-          notificationFactory.weakInfo('Event modified', event.title + ' has been modified');
-        });
-      };
-
-      windowJQuery.resize($scope.resizeCalendarHeight);
-
-      calendarService.calendarId = $scope.calendarId;
-
-      $scope.eventRender = eventService.render;
-      $scope.uiConfig.calendar.eventRender = $scope.eventRender;
-
-      /*
-       * "eventAfterAllRender" is called when all events are fetched but it
-       * is not called when the davserver is unreachable so the "viewRender"
-       * event is used to set the correct height but the event is called too
-       * early and the calendar offset is wrong so wait with a timeout.
-       */
-      $scope.uiConfig.calendar.eventAfterAllRender = $scope.resizeCalendarHeight;
-      $scope.uiConfig.calendar.viewRender = function() {
-        $timeout($scope.resizeCalendarHeight, 1000);
-      };
-      $scope.uiConfig.calendar.eventClick = $scope.eventClick;
-      $scope.uiConfig.calendar.eventResize = $scope.eventDropAndResize;
-      $scope.uiConfig.calendar.eventDrop = $scope.eventDropAndResize;
-      $scope.uiConfig.calendar.select = function(start, end) {
-        var date = calendarUtils.getDateOnCalendarSelect(start, end);
-        $scope.event = {
-          start: date.start,
-          end: date.end,
-          allDay: !start.hasTime(end)
-        };
-        $scope.modal = $modal({scope: $scope, template: '/calendar/views/partials/event-create-quick-form-modal', backdrop: 'static'});
-      };
-      $scope.eventSources = [calendarEventSource($scope.calendarId)];
-
-      function _modifiedCalendarItem(data) {
-        uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('updateEvent', data);
-        var events = uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('clientEvents', data.id);
-        eventService.copyNonStandardProperties(data, events[0]);
-      }
-
-      $rootScope.$on('modifiedCalendarItem', function(event, data) {
-        _modifiedCalendarItem(data);
+    $scope.eventDropAndResize = function(event) {
+      var path = '/calendars/' + $scope.calendarId + '/events/' + event.id + '.ics';
+      calendarService.modify(path, event).then(function() {
+        notificationFactory.weakInfo('Event modified', event.title + ' has been modified');
       });
-      $rootScope.$on('removedCalendarItem', function(event, data) {
-        uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('removeEvents', data);
-      });
-      $rootScope.$on('addedCalendarItem', function(event, data) {
-        uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('renderEvent', data);
-      });
+    };
 
-      function liveNotificationHandlerOnCreate(msg) {
-        uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('renderEvent', calendarService.icalToShell(msg));
-      }
-      function liveNotificationHandlerOnUpdate(msg) {
-        var newEvent = calendarService.icalToShell(msg);
-        var oldEvent = uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('clientEvents', newEvent.id)[0];
+    windowJQuery.resize($scope.resizeCalendarHeight);
 
-        newEvent._allDay = oldEvent._allDay;
-        newEvent._end = oldEvent._end;
-        newEvent._id = oldEvent._id;
-        newEvent._start = oldEvent._start;
-        uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('updateEvent', newEvent);
-        eventService.copyNonStandardProperties(newEvent, oldEvent);
-      }
-      function liveNotificationHandlerOnDelete(msg) {
-        uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('removeEvents', calendarService.icalToShell(msg).id);
-      }
-      var sio = livenotification('/calendars');
+    calendarService.calendarId = $scope.calendarId;
 
-      sio.on('event:created', liveNotificationHandlerOnCreate);
-      sio.on('event:updated', liveNotificationHandlerOnUpdate);
-      sio.on('event:deleted', liveNotificationHandlerOnDelete);
+    $scope.eventRender = eventService.render;
+    $scope.uiConfig.calendar.eventRender = $scope.eventRender;
 
-      $scope.$on('$destroy', function() {
-        sio.removeListener('event:created', liveNotificationHandlerOnCreate);
-        sio.removeListener('event:updated', liveNotificationHandlerOnUpdate);
-        sio.removeListener('event:deleted', liveNotificationHandlerOnDelete);
-      });
+    /*
+     * "eventAfterAllRender" is called when all events are fetched but it
+     * is not called when the davserver is unreachable so the "viewRender"
+     * event is used to set the correct height but the event is called too
+     * early and the calendar offset is wrong so wait with a timeout.
+     */
+    $scope.uiConfig.calendar.eventAfterAllRender = $scope.resizeCalendarHeight;
+    $scope.uiConfig.calendar.viewRender = function() {
+      $timeout($scope.resizeCalendarHeight, 1000);
+    };
+    $scope.uiConfig.calendar.eventClick = $scope.eventClick;
+    $scope.uiConfig.calendar.eventResize = $scope.eventDropAndResize;
+    $scope.uiConfig.calendar.eventDrop = $scope.eventDropAndResize;
+    $scope.uiConfig.calendar.select = function(start, end) {
+      var date = calendarUtils.getDateOnCalendarSelect(start, end);
+      $scope.event = {
+        start: date.start,
+        end: date.end,
+        allDay: !start.hasTime(end)
+      };
+      $scope.modal = $modal({scope: $scope, template: '/calendar/views/partials/event-create-quick-form-modal', backdrop: 'static'});
+    };
+    $scope.eventSources = [calendarEventSource($scope.calendarId)];
+
+    function _modifiedCalendarItem(data) {
+      uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('updateEvent', data);
+      var events = uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('clientEvents', data.id);
+      eventService.copyNonStandardProperties(data, events[0]);
+    }
+
+    $rootScope.$on('modifiedCalendarItem', function(event, data) {
+      _modifiedCalendarItem(data);
     });
+    $rootScope.$on('removedCalendarItem', function(event, data) {
+      uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('removeEvents', data);
+    });
+    $rootScope.$on('addedCalendarItem', function(event, data) {
+      uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('renderEvent', data);
+    });
+
+    function liveNotificationHandlerOnCreate(msg) {
+      uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('renderEvent', calendarService.icalToShell(msg));
+    }
+    function liveNotificationHandlerOnUpdate(msg) {
+      var newEvent = calendarService.icalToShell(msg);
+      var oldEvent = uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('clientEvents', newEvent.id)[0];
+
+      newEvent._allDay = oldEvent._allDay;
+      newEvent._end = oldEvent._end;
+      newEvent._id = oldEvent._id;
+      newEvent._start = oldEvent._start;
+      uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('updateEvent', newEvent);
+      eventService.copyNonStandardProperties(newEvent, oldEvent);
+    }
+    function liveNotificationHandlerOnDelete(msg) {
+      uiCalendarConfig.calendars[$scope.calendarId].fullCalendar('removeEvents', calendarService.icalToShell(msg).id);
+    }
+    var sio = livenotification('/calendars');
+
+    sio.on('event:created', liveNotificationHandlerOnCreate);
+    sio.on('event:updated', liveNotificationHandlerOnUpdate);
+    sio.on('event:deleted', liveNotificationHandlerOnDelete);
+
+    $scope.$on('$destroy', function() {
+      sio.removeListener('event:created', liveNotificationHandlerOnCreate);
+      sio.removeListener('event:updated', liveNotificationHandlerOnUpdate);
+      sio.removeListener('event:deleted', liveNotificationHandlerOnDelete);
+    });
+  });
