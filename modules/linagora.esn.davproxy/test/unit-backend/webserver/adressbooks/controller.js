5e79d5dd7c10746c880e561923aeb131be3d29e3
Christophe Hamerling <christophe.hamerling@gmail.com>
2015-07-10 17:23:04 +0200

    OR-1494 Emit events on contact add/update/delete

--- modules/linagora.esn.davproxy/test/unit-backend/webserver/adressbooks/controller.js
+++ modules/linagora.esn.davproxy/test/unit-backend/webserver/adressbooks/controller.js
@@ -19,7 +19,16 @@ describe('The addressbooks module', function() {
         };
       },
       logger: {
-        error: function() {
+        error: function() {},
+        debug: function() {}
+      },
+      pubsub: {
+        local: {
+          topic: function() {
+            return {
+              publish: function() {}
+            };
+          }
         }
       }
     };
@@ -96,7 +105,11 @@ describe('The addressbooks module', function() {
           token: 123
         },
         davserver: 'http://dav:8080',
-        url: '/foo/bar'
+        url: '/foo/bar',
+        params: {
+          bookId: 'book123',
+          cardId: 'card123'
+        }
       };
     });
 
@@ -141,9 +154,87 @@ describe('The addressbooks module', function() {
         }
       });
     });
+
+    it('should publish a "contacts:contact:update" event if request is an update', function(done) {
+      var statusCode = 200;
+      req.body = {foo: 'bar'};
+      req.headers = {
+        'if-match': 123
+      };
+      var called = false;
+
+      dependencies.pubsub.local.topic = function(name) {
+        expect(name).to.equal('contacts:contact:update');
+        return {
+          publish: function(data) {
+            called = true;
+            expect(data.contactId).to.equal(req.params.contactId);
+            expect(data.bookId).to.equal(req.params.bookId);
+            expect(data.vcard).to.equal(req.body);
+          }
+        };
+      };
+
+      mockery.registerMock('../proxy/http-client', function(options, callback) {
+        return callback(null, {statusCode: statusCode}, {});
+      });
+
+      getController().updateContact(req, {
+        json: function() {
+          expect(called).to.be.true;
+          done();
+        }
+      });
+    });
+
+    it('should publish a "contacts:contact:add" event if request is a creation', function(done) {
+      var statusCode = 200;
+      req.body = {foo: 'bar'};
+      req.headers = {
+      };
+      var called = false;
+
+      dependencies.pubsub.local.topic = function(name) {
+        expect(name).to.equal('contacts:contact:add');
+        return {
+          publish: function(data) {
+            called = true;
+            expect(data.contactId).to.equal(req.params.contactId);
+            expect(data.bookId).to.equal(req.params.bookId);
+            expect(data.vcard).to.equal(req.body);
+          }
+        };
+      };
+
+      mockery.registerMock('../proxy/http-client', function(options, callback) {
+        return callback(null, {statusCode: statusCode}, {});
+      });
+
+      getController().updateContact(req, {
+        json: function() {
+          expect(called).to.be.true;
+          done();
+        }
+      });
+    });
   });
 
   describe('The deleteContact function', function() {
+
+    var req;
+    beforeEach(function() {
+      req = {
+        token: {
+          token: 123
+        },
+        davserver: 'http://dav:8080',
+        url: '/foo/bar',
+        params: {
+          bookId: 'book123',
+          cardId: 'card123'
+        }
+      };
+    });
 
     it('should call the proxy module', function(done) {
       mockery.registerMock('../proxy', function() {
@@ -160,6 +251,37 @@ describe('The addressbooks module', function() {
         };
       });
       getController().deleteContact();
+    });
+
+    it('should publish a "contacts:contact:delete" event if request is a delete and is successful', function(done) {
+      var called = false;
+      dependencies.pubsub.local.topic = function(name) {
+        expect(name).to.equal('contacts:contact:delete');
+        return {
+          publish: function(data) {
+            called = true;
+            expect(data.contactId).to.equal(req.params.contactId);
+            expect(data.bookId).to.equal(req.params.bookId);
+            expect(data.vcard).to.not.exist;
+          }
+        };
+      };
+
+      mockery.registerMock('../proxy', function() {
+        return function() {
+          return {
+            handle: function(options) {
+              return function(req, res) {
+                options.onSuccess({}, {}, req, res, function() {
+                  done();
+                });
+              };
+            }
+          };
+        };
+      });
+      getController().deleteContact(req);
+
     });
   });
 
