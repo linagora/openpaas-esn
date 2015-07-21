48bd61294a4aa5585e0153ae280e0a62ab793252
Stéphen Le Maistre <slemaistre@linagora.com>
2015-07-21 09:12:59 +0200

    OR-1472 add correct middleware for the calendar proxy *all* route

--- modules/linagora.esn.davproxy/backend/webserver/calendars/router.js
+++ modules/linagora.esn.davproxy/backend/webserver/calendars/router.js
@@ -5,10 +5,11 @@ var express = require('express');
 module.exports = function(dependencies) {
 
   var router = express.Router();
+  var authorizationMW = dependencies('authorizationMW');
   var proxy = require('../proxy')(dependencies)('calendars');
   var davMiddleware = dependencies('davserver').davMiddleware;
 
-  router.all('/*', davMiddleware.getDavEndpoint, proxy.handle());
+  router.all('/*', authorizationMW.requiresAPILogin, davMiddleware.generateNewToken, davMiddleware.getDavEndpoint, proxy.handle());
 
   return router;
 };
