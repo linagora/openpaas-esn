dba349b01caae890614cfb38d2e1dbdc80ab7ead
Romain PIGNOLET <rpignolet@linagora.com>
2015-07-28 11:41:36 +0200

    OR-1478 Create a sidebar menu entry with the email address of the logged-in user

--- modules/linagora.esn.unifiedinbox/frontend/js/services.js
+++ modules/linagora.esn.unifiedinbox/frontend/js/services.js
@@ -1,3 +1,31 @@
 'use strict';
 
-angular.module('linagora.esn.unifiedinbox');
+angular.module('linagora.esn.unifiedinbox')
+
+  .factory('JmapAPI', function($q) {
+
+    function getMailboxes() {
+      return $q.when([{
+        name: 'Inbox',
+        href: '/#/unifiedinbox',
+        unreadMessages: 5
+      }, {
+        name: 'Draft',
+        href: '/#/unifiedinbox',
+        unreadMessages: 42
+      }, {
+        name: 'Spam',
+        href: '/#/unifiedinbox',
+        unreadMessages: 0
+      }, {
+        name: 'Trash',
+        href: '/#/unifiedinbox',
+        unreadMessages: 1
+      }]);
+    }
+
+    return {
+      getMailboxes: getMailboxes
+    };
+
+  });
