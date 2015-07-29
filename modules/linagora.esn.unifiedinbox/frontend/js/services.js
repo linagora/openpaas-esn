1ae4f4f809859dc771760ae18dec924a7f961519
Thomas Hilaire <thilaire@linagora.com>
2015-07-29 16:54:54 +0200

    OR-1481 Login before returning mailboxes

--- modules/linagora.esn.unifiedinbox/frontend/js/services.js
+++ modules/linagora.esn.unifiedinbox/frontend/js/services.js
@@ -1,10 +1,32 @@
 'use strict';
 
+/*global JMAP */
+
 angular.module('linagora.esn.unifiedinbox')
 
-  .factory('JmapAPI', function($q) {
+  .service('JmapAuth', function(session) {
+
+    function login() {
+      var userEmail = session.user.emails[0];
+      var userPath = 'ba9b0e4c-3501-11e5-b419-0242ac110012';
+      var jmapServer = 'http://localhost:8888';
+      JMAP.auth.didAuthenticate(userEmail, '', {
+        apiUrl: jmapServer + '/jmap/' + userPath,
+        eventSourceUrl: jmapServer + '/events/' + userPath,
+        uploadUrl: jmapServer + '/upload/' + userPath,
+        downloadUrl: jmapServer + '/raw/' + userPath + '/{blobId}/{name}'
+      });
+    }
+
+    return {
+      login: login
+    };
+  })
+
+  .factory('JmapAPI', function($q, JmapAuth) {
 
     function getMailboxes() {
+      JmapAuth.login();
       return $q.when([{
         name: 'Inbox',
         role: 'inbox',
--- modules/linagora.esn.unifiedinbox/test/unit-frontend/services.js
+++ modules/linagora.esn.unifiedinbox/test/unit-frontend/services.js
@@ -6,8 +6,91 @@ var expect = chai.expect;
 
 describe('The Unified Inbox Angular module services', function() {
 
-  it('should work', function() {
-    expect(true).to.be.true;
+  beforeEach(function() {
+    this.JMAP = {};
+    window.JMAP = this.JMAP;
+    angular.mock.module('esn.session');
+    angular.mock.module('esn.core');
+    angular.mock.module('linagora.esn.unifiedinbox');
   });
 
+  describe('JmapAuth service', function() {
+
+    beforeEach(angular.mock.inject(function(JmapAuth, session) {
+      this.JmapAuth = JmapAuth;
+      this.session = session;
+    }));
+
+    describe('login method', function() {
+
+      it('should delegate to JMAP', function() {
+        this.session.user = {emails: ['user@domain']};
+        var called = false;
+        this.JMAP.auth = {
+          didAuthenticate: function(user, token, args) {
+            called = true;
+          }
+        };
+
+        this.JmapAuth.login();
+
+        expect(called).to.be.true;
+      });
+
+    });
+  });
+
+  describe('JmapAPI service', function() {
+
+    beforeEach(angular.mock.inject(function(JmapAPI, JmapAuth, session, $rootScope) {
+      this.JmapAPI = JmapAPI;
+      this.JmapAuth = JmapAuth;
+      this.session = session;
+      this.$rootScope = $rootScope;
+    }));
+
+    describe('getMailboxes method', function() {
+
+      it('should login first', function() {
+        var called = false;
+        this.JmapAuth.login = function() {
+          called = true;
+        };
+
+        this.JmapAPI.getMailboxes();
+
+        expect(called).to.be.true;
+      });
+
+      it('should return a promise with mailboxes', function(done) {
+        this.JmapAuth.login = function() {};
+        this.JmapAPI.getMailboxes().then(function(result) {
+          expect(result).to.deep.equal([{
+            name: 'Inbox',
+            role: 'inbox',
+            href: '/#/unifiedinbox',
+            unreadMessages: 5
+          }, {
+            name: 'Draft',
+            role: 'drafts',
+            href: '/#/unifiedinbox',
+            unreadMessages: 42
+          }, {
+            name: 'Sent',
+            role: 'sent',
+            href: '/#/unifiedinbox',
+            unreadMessages: 0
+          }, {
+            name: 'Trash',
+            role: 'trash',
+            href: '/#/unifiedinbox',
+            unreadMessages: 1
+          }]);
+          done();
+        });
+        this.$rootScope.$apply();
+      });
+
+    });
+  });
 });
