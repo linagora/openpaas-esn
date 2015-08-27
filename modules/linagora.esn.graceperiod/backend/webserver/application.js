9c2db02d70ed0014bd352182e42329ce294ad22f
Michael Bailly <mbailly@linagora.com>
2015-08-27 20:16:53 +0200

    OR-1657 Inject graceperiod module less files

--- modules/linagora.esn.graceperiod/backend/webserver/application.js
+++ modules/linagora.esn.graceperiod/backend/webserver/application.js
@@ -5,8 +5,6 @@ var FRONTEND_PATH = require('./constants').FRONTEND_PATH;
 
 module.exports = function(dependencies) {
   var app = express();
-
-  require('./config/less')(dependencies, app);
   app.use(express.static(FRONTEND_PATH));
 
   return app;
