d9e1a6650dc2f68e5c315b9e36bb5c71d4a23b80
Thomas Hilaire <thilaire@linagora.com>
2015-09-01 15:15:39 +0200

    OR-1674 Can delete an email

--- modules/linagora.esn.unifiedinbox/frontend/js/controllers.js
+++ modules/linagora.esn.unifiedinbox/frontend/js/controllers.js
@@ -5,8 +5,13 @@ angular.module('linagora.esn.unifiedinbox')
     $scope.mailbox = $route.current.params.mailbox;
     $scope.groupedEmails = JmapAPI.getEmails($scope.mailbox);
   })
-  .controller('viewEmailController', function($scope, $route, JmapAPI) {
+  .controller('viewEmailController', function($scope, $route, $location, JmapAPI, MAILBOX_ROLES) {
     $scope.mailbox = $route.current.params.mailbox;
     $scope.emailId = $route.current.params.emailId;
     $scope.email = JmapAPI.getEmail($scope.emailId);
+
+    $scope.moveToTrash = function() {
+      JmapAPI.moveToByRole($scope.emailId, MAILBOX_ROLES.trash);
+      $location.path('/unifiedinbox/' + $scope.mailbox);
+    };
   });
