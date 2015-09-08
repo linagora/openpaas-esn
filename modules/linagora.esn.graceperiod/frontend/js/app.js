acd4cc240d57f8e1001a072cf638d4262065c4cd
Christophe Hamerling <chamerling@linagora.com>
2015-09-08 16:56:36 +0200

    OR-1675 Graceperiod events in backend forwarded to frontend

---
+++ modules/linagora.esn.graceperiod/frontend/js/app.js
@@ -0,0 +1,3 @@
+'use strict';
+
+angular.module('linagora.esn.graceperiod', ['restangular', 'esn.websocket']);
---
+++ modules/linagora.esn.graceperiod/lib/constants.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+  GRACEPERIOD_ERROR: 'graceperiod:error',
+  GRACEPERIOD_DONE: 'graceperiod:done'
+};
