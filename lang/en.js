a06ea8488a87aebd747cfcb5300e7d44e903d9ad
Michael Bailly <mbailly@linagora.com>
2015-09-24 15:00:09 +0200

    Homepage: first take

--- lang/en.js
+++ lang/en.js
@@ -1,5 +1,9 @@
 var languages = languages || {};
 
 languages.en = {
-  TITLE: 'OpenPaaS'
+  TITLE: 'OpenPaaS',
+  MAIN_TITLE: 'OpenPaaS virtual desktop',
+  MAIN_TAGLINE: 'Professional collaboration made simple',
+  VISION_TITLE: 'Vision',
+  VISION_CONTENTS: 'Yolo <strong>hey !</strong> and more'
 };
--- lang/fr.js
+++ lang/fr.js
@@ -1,5 +1,9 @@
 var languages = languages || {};
 
 languages.fr = {
-  TITLE: 'OpenPaaS'
+  TITLE: 'OpenPaaS',
+  MAIN_TITLE: 'Bureau virtuel OpenPaaS',
+  MAIN_TAGLINE: 'La collaboration professionnelle, simple',
+  VISION_TITLE: 'Vision',
+  VISION_CONTENTS: 'Yolo <strong>hey !</strong> et plus'
 };
