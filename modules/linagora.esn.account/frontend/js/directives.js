253a244781f8efdfbd62cc721d4717cae5c7984a
Christophe Hamerling <chamerling@linagora.com>
2015-10-06 14:12:48 +0200

    OR-1840 FAB Dropup

--- modules/linagora.esn.account/frontend/js/directives.js
+++ modules/linagora.esn.account/frontend/js/directives.js
@@ -1,3 +1,23 @@
 'use strict';
 
-angular.module('linagora.esn.account');
+angular.module('linagora.esn.account')
+
+  .directive('fabAccountDropup', function() {
+    return {
+      restrict: 'E',
+      templateUrl: '/account/views/partials/fab.html',
+      link: function($scope, $element) {
+        $scope.onClick = function() {
+          var e = angular.element($element[0].querySelector('.modal-accounts-list'));
+          if (!e) {
+            return;
+          }
+          if (e.hasClass('active')) {
+            e.removeClass('active');
+          } else {
+            e.addClass('active');
+          }
+        };
+      }
+    };
+  });
