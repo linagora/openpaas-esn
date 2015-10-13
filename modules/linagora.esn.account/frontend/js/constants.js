4c00f8146de5d7372ec6e698b99e2fbafc4b350f
Christophe Hamerling <chamerling@linagora.com>
2015-10-13 11:07:55 +0200

    OR-1841 Only keep supported accounts

--- modules/linagora.esn.account/frontend/js/constants.js
+++ modules/linagora.esn.account/frontend/js/constants.js
@@ -1,5 +1,6 @@
 'use strict';
 
 angular.module('linagora.esn.account')
-  .constant('FAB_ANCHOR_POINT', 'accounts-item-anchorpoint');
+  .constant('FAB_ANCHOR_POINT', 'accounts-item-anchorpoint')
+  .constant('SUPPORTED_ACCOUNTS', ['oauth']);
 
--- modules/linagora.esn.account/frontend/js/controllers.js
+++ modules/linagora.esn.account/frontend/js/controllers.js
@@ -2,6 +2,13 @@
 
 angular.module('linagora.esn.account')
 
-  .controller('accountListController', function($scope, accounts) {
-    $scope.accounts = accounts;
+  .controller('accountListController', function($scope, accounts, SUPPORTED_ACCOUNTS) {
+    $scope.accounts = accounts.map(function(account) {
+      if (SUPPORTED_ACCOUNTS.indexOf(account.type.toLowerCase()) > -1) {
+        account.provider = account.data.provider;
+      }
+      return account;
+    }).filter(function(account) {
+      return !!account.provider;
+    });
   });
