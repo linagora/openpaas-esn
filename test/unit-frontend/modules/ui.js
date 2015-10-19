61153f9e5c233c26d68444f4c83494872e911ec8
ddolcimascolo <ddolcimascolo@linagora.com>
2015-10-19 18:07:14 +0200

    OR-1897 Refactored tests.

--- test/unit-frontend/modules/ui.js
+++ test/unit-frontend/modules/ui.js
@@ -6,51 +6,70 @@ var expect = chai.expect;
 
 describe('The UI module', function() {
 
+  var $compile, $rootScope, $scope, element;
+
+  function initDirective(html) {
+    element = $compile(html)($scope);
+    $scope.$digest();
+  }
+
+  beforeEach(module('esn.ui'));
+  beforeEach(module('jadeTemplates'));
+
   describe('The fab directive', function() {
 
-    beforeEach(module('esn.ui'));
-    beforeEach(module('jadeTemplates'));
+    beforeEach(inject(function(_$compile_, _$rootScope_) {
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+      $scope = $rootScope.$new();
+    }));
 
-    beforeEach(inject(['$compile', '$rootScope', function($c, $r) {
-      this.$compile = $c;
-      this.$rootScope = $r;
-      this.$scope = this.$rootScope.$new();
+    it('should set icon class to the one matching the icon attribute', function() {
+      initDirective('<fab icon="next"></fab>');
 
-      this.initDirective = function(scope) {
-        var html = '<fab on-click="action()" type="{{type}}"/></fab>';
-        var element = this.$compile(html)(scope);
-        scope.$digest();
-        return element;
-      };
-    }]));
-
-    it('should set class to the one matching the fab action', function() {
-      this.$scope.type = 'create';
-      var element = this.initDirective(this.$scope);
-      var i = element.find('i');
-      expect(i.hasClass('mdi-plus')).to.be.true;
+      expect(element.find('i').hasClass('mdi-arrow-right')).to.equal(true);
     });
 
-    it('should set class to the default one when action is unknown', function() {
-      this.$scope.type = 'unknownaction';
-      var element = this.initDirective(this.$scope);
-      var i = element.find('i');
-      expect(i.hasClass('mdi-content-create')).to.be.true;
+    it('should set icon class to the default one when icon attribute is not defined', function() {
+      initDirective('<fab></fab>');
+
+      expect(element.find('i').hasClass('mdi-plus')).to.equal(true);
     });
 
-    it('should call the scope action when clicking on the fab', function(done) {
-      this.$scope.type = 'create';
-      this.$scope.action = done();
-      var element = this.initDirective(this.$scope);
-      element.find('.btn').click();
+    it('should set icon class to the default one when icon attribute is not known', function() {
+      initDirective('<fab icon="makeMeASandwitch"></fab>');
+
+      expect(element.find('i').hasClass('mdi-plus')).to.equal(true);
+    });
+
+    it('should set color class to the one matching the color attribute', function() {
+      initDirective('<fab color="success"></fab>');
+
+      expect(element.hasClass('btn-success')).to.equal(true);
+    });
+
+    it('should set color class to the default one when color attribute is not defined', function() {
+      initDirective('<fab></fab>');
+
+      expect(element.hasClass('btn-accent')).to.equal(true);
+    });
+
+    it('should set type to the value of the "type" attribute when defined', function() {
+      initDirective('<fab type="submit"></fab>');
+
+      expect(element[0].type).to.equal('submit');
+    });
+
+    it('should set type to "button" when not defined', function() {
+      initDirective('<fab></fab>');
+
+      expect(element[0].type).to.equal('button');
     });
   });
 
   describe('The dynamicFabDropup directive', function() {
 
-    beforeEach(module('esn.ui'));
     beforeEach(module('esn.core'));
-    beforeEach(module('jadeTemplates'));
 
     beforeEach(inject(function(_$compile_, _$rootScope_, _$document_) {
       this.$compile = _$compile_;
