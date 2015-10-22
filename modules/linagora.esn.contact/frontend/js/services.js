d851b42b17f73c92bfb0361e883cacd826819f34
Sang NGO DOAN <sngodoan@linagora.com>
2015-10-22 14:40:58 +0700

    OR-1793 Make the website could be formatted name

--- modules/linagora.esn.contact/frontend/js/services.js
+++ modules/linagora.esn.contact/frontend/js/services.js
@@ -139,6 +139,10 @@ angular.module('linagora.esn.contact')
         }
       }
 
+      if (notNullNorEmpty(contact.urls)) {
+        return contact.urls[0].value;
+      }
+
       if (notNullNorEmpty(contact.notes)) {
         return contact.notes;
       }
