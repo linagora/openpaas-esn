b36474ba9fd60f2523322161b573a4da68184698
Abdulkader <abenchi@linagora.com>
2015-11-05 18:00:54 +0100

    INBOX-26 fix cursor position on empty content-editable for mobile FF

--- frontend/js/modules/esn.summernote.js
+++ frontend/js/modules/esn.summernote.js
@@ -14,7 +14,7 @@ angular.module('esn.summernote-wrapper', ['summernote', 'ng.deviceDetector'])
   * Firefox adds a <p> tag when pressing ENTER. The problem is that the tag <p>
   * does not force the cursor to go to a newline in firefox. This plugin is used
   * to change the default behavior of firefox when pressing enter so as to make
-  * it adds a <br> tag for each newline.
+  * it adds a <br/> tag for each newline.
   * Bug description and the plugin are provided at:
   * https://github.com/summernote/summernote/issues/702
   */
@@ -24,37 +24,64 @@ angular.module('esn.summernote-wrapper', ['summernote', 'ng.deviceDetector'])
       if ($window.getSelection) {
         // get the text from the focused content-editable div
         selection = $window.getSelection();
-
         // put the text in a range for future operations
         range = selection.getRangeAt(0);
 
         // delete the text from the content-editable div
         range.deleteContents();
 
+        // define the DOM node after which a newline is to be inserted
         var frag = $window.document.createDocumentFragment();
-        var lastNode = frag.appendChild(angular.element(newlineHtmlElement)[0]);
+        var newLineNode = frag.appendChild(angular.element(newlineHtmlElement)[0]);
 
-        // append the newline tag to the end of the rang
+        // append the newline tag to the end of the range
         range.insertNode(frag);
 
-        // insert the modified rang again in the content-editable div
-        if (lastNode) {
-          range = range.cloneRange();
-          range.setStartAfter(lastNode);
-          range.collapse(true);
-          selection.removeAllRanges();
-          selection.addRange(range);
-        }
+        // insert the modified range again in the content-editable div
+        range = range.cloneRange();
+        range.setStartAfter(newLineNode);
+        range.collapse(true);
+        selection.removeAllRanges();
+        selection.addRange(range);
       }
     }
 
     return {
-      name: 'MobileFirefoxEnter',
+      name: 'MobileFirefoxNewline',
       events: {
         insertParagraph: function(event) {
-          pasteHtmlAtCaret('<br>');
+          pasteHtmlAtCaret('<br/>');
           event.preventDefault();
         }
+      }
+    };
+  })
+
+  /**
+   * In firefox, the caret is invisible/placed at wrong place in an empty content
+   * -editable node. This plugin prevent the content-editable node from being empty.
+   * FYI: the minimum content for a content-editable node in summernote to be
+   * non-empty is equals to = <p><br></p>, which is called the emptyPara
+   * Bug description: https://github.com/summernote/summernote/pull/313
+   */
+  .factory('PreventEmptyAreaFirefoxPlugin', function($timeout) {
+    return {
+      name: 'PreventEmptyAreaFirefox',
+      init: function(layoutInfo) {
+
+        // the timeout is used to invoke the function after the ng-model is linked to the DOM
+        $timeout(function() {
+          if (layoutInfo.editable().code() === '') {
+            layoutInfo.editable().append('<p><br/></p>');
+          }
+        }, 0);
+
+        layoutInfo.holder().on('summernote.change', function(event, node) {
+          if (node.indexOf('<br>') === 0 || node === '') {
+            layoutInfo.editable().empty();
+            layoutInfo.editable().append('<p><br/></p>');
+          }
+        });
       }
     };
   })
@@ -94,10 +121,14 @@ angular.module('esn.summernote-wrapper', ['summernote', 'ng.deviceDetector'])
     };
   })
 
-  .run(function(summernote, FullscreenPlugin, MobileFirefoxNewlinePlugin, deviceDetector) {
+  .run(function(summernote, FullscreenPlugin, MobileFirefoxNewlinePlugin, PreventEmptyAreaFirefoxPlugin, deviceDetector) {
     summernote.addPlugin(FullscreenPlugin);
 
-    if (deviceDetector.browser === 'firefox' && deviceDetector.isMobile()) {
-      summernote.addPlugin(MobileFirefoxNewlinePlugin);
+    if (deviceDetector.browser === 'firefox') {
+      summernote.addPlugin(PreventEmptyAreaFirefoxPlugin);
+
+      if (deviceDetector.isMobile()) {
+        summernote.addPlugin(MobileFirefoxNewlinePlugin);
+      }
     }
   });
--- test/unit-frontend/esn.summernote.js
+++ test/unit-frontend/esn.summernote.js
@@ -6,28 +6,13 @@
 var expect = chai.expect;
 
 describe('The esn.summernote Angular module', function() {
-
   var $compile, $rootScope, $scope, element;
 
-  beforeEach(function() {
-    angular.mock.module('esn.summernote-wrapper');
+  afterEach(function() {
+    if (element) {
+      element.remove();
+    }
   });
-
-  beforeEach(inject(function(_$compile_, _$rootScope_) {
-    $compile = _$compile_;
-    $rootScope = _$rootScope_;
-    $scope = $rootScope.$new();
-  }));
-
-  /**
-   * destroying method in summernote does not behave correctly.
-   * Bug reported: https://github.com/summernote/summernote/issues/821
-   */
-  //afterEach(function() {
-  //  if (element) {
-  //    element.remove();
-  //  }
-  //});
 
   function compileDirective(html) {
     element = $compile(html)($scope);
@@ -36,15 +21,16 @@ describe('The esn.summernote Angular module', function() {
     return element;
   }
 
-  describe('the summernote editor', function() {
-    it('should not have an empty editor', function() {
-      var element = compileDirective('<div><summernote/></div>');
-      var editor = element.find('.note-editable');
-      expect(editor.code()).to.equal('<p><br></p>');
-    });
-  });
-
   describe('the fullscreen plugin', function() {
+    beforeEach(function() {
+      angular.mock.module('esn.summernote-wrapper');
+      angular.mock.inject(function(_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+        $scope = $rootScope.$new();
+      });
+    });
+
     it('should broadcast a hide/show in alternance while toggling the icon every time the fullscreen button of summernote toolbar is clicked', function() {
       var hide = sinon.spy(), show = sinon.spy();
       $scope.$on('header:hide', function() {
@@ -53,9 +39,10 @@ describe('The esn.summernote Angular module', function() {
       $scope.$on('header:show', function() {
         show();
       });
-      var element = compileDirective('<div><summernote/></div>');
-      var btn = element.find('[data-name="fullscreen"]');
-      var icon = element.find('[data-name="fullscreen"] > i');
+      var element = compileDirective('<summernote/>');
+      element.summernote('focus');
+      var btn = angular.element('[data-name="fullscreen"]');
+      var icon = angular.element('[data-name="fullscreen"] > i');
       btn.click();
       expect(hide).to.be.called;
       expect(icon.hasClass('fa-compress')).to.be.true;
@@ -68,24 +55,173 @@ describe('The esn.summernote Angular module', function() {
   });
 
   describe('the MobileFirefoxNewline plugin', function() {
-    var MobileFirefoxNewlinePlugin;
+    var $window, MobileFirefoxNewlinePlugin, preventDefaultSpy, event;
+
     beforeEach(function() {
-      angular.mock.inject(function(_MobileFirefoxNewlinePlugin_) {
+      preventDefaultSpy = sinon.spy();
+      event = {
+        preventDefault: preventDefaultSpy
+      };
+
+      angular.mock.module('esn.summernote-wrapper');
+      angular.mock.inject(function(_$compile_, _$window_, _$rootScope_, _MobileFirefoxNewlinePlugin_) {
+        $compile = _$compile_;
+        $window = _$window_;
+        $rootScope = _$rootScope_;
+        $scope = $rootScope.$new();
         MobileFirefoxNewlinePlugin = _MobileFirefoxNewlinePlugin_;
       });
     });
 
-    it('should insert a <br> tag at the current cursor position', function() {
-      var preventDefaultSpy = sinon.spy();
-      var event = {
-        preventDefault: preventDefaultSpy
-      };
+    function moveCursorInFocesedSelection(node, position) {
+      var selection = $window.getSelection();
+      var range = $window.document.createRange();
+      range.setStart(node, position);
+      range.collapse(true);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+
+    it('should insert a <br> tag', function() {
       var contentEditable = compileDirective('<div contenteditable="true">Hello world!</div>');
       contentEditable.focus();
       MobileFirefoxNewlinePlugin.events.insertParagraph(event);
       expect(preventDefaultSpy).to.be.called;
       expect(contentEditable.code()).to.equal('<br>Hello world!');
     });
+
+    it('should insert a <br> tag at the current cursor position', function() {
+      var contentEditable = compileDirective('<div contenteditable="true">The first line The second line</div>');
+      contentEditable.focus();
+      // move the cursor to be after "The first line"
+      moveCursorInFocesedSelection(contentEditable[0].childNodes[0], 14);
+
+      MobileFirefoxNewlinePlugin.events.insertParagraph(event);
+      expect(preventDefaultSpy).to.be.called;
+      expect(contentEditable.code()).to.equal('The first line<br> The second line');
+    });
+
+    it('should insert a <br> tag at end of a paragraph', function() {
+      var contentEditable = compileDirective('<div contenteditable="true"><pr>The first line</pr><p>The second line</p></div>');
+      contentEditable.focus();
+      // move the cursor to the end of the second paragraph
+      moveCursorInFocesedSelection(contentEditable[0].childNodes[1], 1);
+
+      MobileFirefoxNewlinePlugin.events.insertParagraph(event);
+      expect(preventDefaultSpy).to.be.called;
+      expect(contentEditable.code()).to.equal('<pr>The first line</pr><p>The second line<br></p>');
+    });
+
+    it('should insert a <br> tag inside a paragraph', function() {
+      var contentEditable = compileDirective('<div contenteditable="true"><pr>The first line</pr><p>The second lineThe third line</p></div>');
+      contentEditable.focus();
+      // set the cursor between "The second line" and "The third line"
+      moveCursorInFocesedSelection(contentEditable[0].childNodes[1].childNodes[0], 15);
+
+      MobileFirefoxNewlinePlugin.events.insertParagraph(event);
+      expect(preventDefaultSpy).to.be.called;
+      expect(contentEditable.code()).to.equal('<pr>The first line</pr><p>The second line<br>The third line</p>');
+    });
   });
 
+  describe('the MobileFirefoxNewline plugin for a non-supported browser', function() {
+    var MobileFirefoxNewlinePlugin, preventDefaultSpy, event;
+
+    beforeEach(function() {
+      preventDefaultSpy = sinon.spy();
+      event = {
+        preventDefault: preventDefaultSpy
+      };
+
+      angular.mock.module('esn.summernote-wrapper', function($provide) {
+        $provide.value('$window', {
+          getSelection: undefined,
+          navigator: {
+            userAgent: 'mobile'
+          }
+        });
+
+        $provide.value('$document', [{
+          documentMode: {}
+        }]);
+      });
+
+      angular.mock.inject(function(_$compile_, _$rootScope_, _MobileFirefoxNewlinePlugin_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+        $scope = $rootScope.$new();
+        MobileFirefoxNewlinePlugin = _MobileFirefoxNewlinePlugin_;
+      });
+    });
+
+    it('should not modify the content editable div', function() {
+      var contentEditable = compileDirective('<div contenteditable="true">Hello world!</div>');
+      contentEditable.focus();
+      MobileFirefoxNewlinePlugin.events.insertParagraph(event);
+      expect(preventDefaultSpy).to.be.called;
+      expect(contentEditable.code()).to.equal('Hello world!');
+    });
+  });
+
+  describe('the PreventEmptyAreaFirefox plugin with firefox browser', function() {
+    beforeEach(function() {
+      angular.mock.module('esn.summernote-wrapper', function($provide) {
+        $provide.value('deviceDetector', {
+          browser: 'firefox',
+          isMobile: function() { return false; }
+        });
+      });
+
+      angular.mock.inject(function(_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+        $scope = $rootScope.$new();
+      });
+    });
+
+    it('should prevent the editor from being empty', function() {
+      var element = compileDirective('<summernote/>');
+      element.summernote('focus');
+      var editor = angular.element('.note-editable');
+      expect(editor.code()).to.equal('<p><br></p>');
+
+      element.summernote('insertText', '');
+      expect(editor.code()).to.equal('<p><br></p>');
+
+      element.summernote('insertText', 'Hello world!');
+      expect(editor.code()).to.equal('<p>Hello world!<br></p>');
+    });
+  });
+
+  describe('the PreventEmptyAreaFirefox plugin with a non-firefox browser', function() {
+    var PreventEmptyAreaFirefoxPlugin;
+
+    beforeEach(function() {
+      PreventEmptyAreaFirefoxPlugin = sinon.spy();
+
+      angular.mock.module('esn.summernote-wrapper', function($provide) {
+        $provide.value('deviceDetector', {
+          browser: 'chrome',
+          isMobile: function() { return false; }
+        });
+        $provide.value('summernote', {
+          addPlugin: function(plugin) {
+            plugin();
+          }
+        });
+        $provide.value('PreventEmptyAreaFirefoxPlugin', PreventEmptyAreaFirefoxPlugin);
+        $provide.value('FullscreenPlugin', function() {});
+      });
+
+      angular.mock.inject(function(_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+        $scope = $rootScope.$new();
+      });
+    });
+
+    it('should not be added with a non-firefox browser', function() {
+      expect(PreventEmptyAreaFirefoxPlugin).to.not.be.called;
+    });
+  });
 });
