3b33ddf6940cab335bc19991f097685486b9eaae
Ba Tuan <btngo@linagora.com>
2015-11-12 19:35:18 +0100

    OR-1969 Register twitterDisplayShell

--- modules/linagora.esn.contact.twitter/frontend/js/app.js
+++ modules/linagora.esn.contact.twitter/frontend/js/app.js
@@ -2,4 +2,12 @@
 
 angular.module('linagora.esn.contact.twitter', [
   'linagora.esn.contact'
-]);
+])
+  .run(function(DisplayShellProvider, TwitterDisplayShell) {
+    var isTwitterContact = function(shell) {
+      return shell.social.some(function (element) {
+        return (element.type === 'Twitter') && (element.value[0] === '@');
+      });
+    };
+    DisplayShellProvider.addDisplayShell(TwitterDisplayShell, isTwitterContact);
+  });
