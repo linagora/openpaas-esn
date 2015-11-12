cc968d68e537e65de8876e5e6ea4096868b81f76
ymaligoy <ymaligoy@linagora.com>
2015-11-12 14:25:57 +0100

    OR-1966 Create the displays shell provider
    Conflicts:
    	modules/linagora.esn.contact/index.js

---
+++ modules/linagora.esn.contact/frontend/js/displayshellprovider.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular.module('linagora.esn.contact')
+  .factory('DisplayShellProvider', function(ContactDisplayShell) {
+
+    var displayShellsRegistered = [];
+
+    function addDisplayShell(displayShell, fnMatchingContact) {
+      displayShellsRegistered.push({
+        displayShell: displayShell,
+        fnMatchingContact: fnMatchingContact
+      });
+    }
+
+    function resetDisplayShell() {
+      displayShellsRegistered = [];
+    }
+
+    function convertToDisplayShell(contactShell) {
+      var displayShellMatched = null;
+
+      displayShellsRegistered.some(function(elt) {
+        if (elt.fnMatchingContact(contactShell)) {
+          displayShellMatched = elt;
+          return true;
+        }
+      });
+
+      if (displayShellMatched) {
+        return new displayShellMatched.displayShell(contactShell);
+      }
+
+      return new ContactDisplayShell(contactShell);
+    }
+
+    return {
+      addDisplayShell: addDisplayShell,
+      resetDisplayShell: resetDisplayShell,
+      convertToDisplayShell: convertToDisplayShell
+    };
+  });
