31fc9870969c827549c5f5cad849b49deb67c2f3
Philipp Kewisch <pkewisch@linagora.com>
2015-12-07 15:11:01 +0100

    [OR-2041] - Disallow trailing comma for objects and arrays - Fix linter error in inbox module

--- modules/linagora.esn.unifiedinbox/index.js
+++ modules/linagora.esn.unifiedinbox/index.js
@@ -8,7 +8,7 @@ var unifiedInboxModule = new AwesomeModule('linagora.esn.unifiedinbox', {
   dependencies: [
     new Dependency(Dependency.TYPE_NAME, 'linagora.esn.core.logger', 'logger'),
     new Dependency(Dependency.TYPE_NAME, 'linagora.esn.core.webserver.wrapper', 'webserver-wrapper'),
-    new Dependency(Dependency.TYPE_NAME, 'linagora.io.mailer', 'mailer'),
+    new Dependency(Dependency.TYPE_NAME, 'linagora.io.mailer', 'mailer')
   ],
   states: {
     lib: function(dependencies, callback) {
