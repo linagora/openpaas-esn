0d485370ba1277ade93bbfa3ad506edae851bad8
Luc DUZAN <lduzan@linagora.com>
2015-12-17 14:07:33 +0100

    OR-2036 correcting generateActionLink
    Signed-off-by: Luc DUZAN <lduzan@linagora.com>

--- modules/linagora.esn.calendar/backend/webserver/api/calendar/core.js
+++ modules/linagora.esn.calendar/backend/webserver/api/calendar/core.js
@@ -164,7 +164,7 @@ function generateActionLink(baseUrl, jwtPayload, action) {
     if (err) {
       return deferred.reject(err);
     }
-    return deferred.resolve(urlBuilder.resolve(baseUrl, '/api/calendars/event/participation/?jwt=' + token));
+    return deferred.resolve(urlBuilder.resolve(baseUrl, '/calendar/api/calendars/event/participation/?jwt=' + token));
   });
   return deferred.promise;
 }
