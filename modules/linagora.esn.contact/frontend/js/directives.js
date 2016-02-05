222fa41c8796770937d3507784788dbfee7070f2
Ba Tuan <btngo@linagora.com>
2016-02-05 16:38:18 +0100

    CONTACT-10 Allow contact birthday store any value

--- modules/linagora.esn.contact/frontend/js/directives.js
+++ modules/linagora.esn.contact/frontend/js/directives.js
@@ -178,18 +178,24 @@ angular.module('linagora.esn.contact')
       templateUrl: '/contact/views/partials/contact-search-form.html'
     };
   })
-  .directive('relaxedDate', function(CONTACT_DATE_FORMAT, $dateParser, $dateFormatter) {
+  .directive('relaxedDateForBsDatepicker', function(CONTACT_DATE_FORMAT, $dateFormatter) {
     return {
       restrict: 'A',
       require: 'ngModel',
       link: function(scope, element, attrs, controller) {
         element.attr('placeholder', CONTACT_DATE_FORMAT);
 
-        controller.$parsers.push(function(text) {
-          return $dateParser({ format: CONTACT_DATE_FORMAT }).parse(text) || text;
-        });
-        controller.$formatters.push(function(dateOrText) {
-          return $dateFormatter.formatDate(dateOrText, CONTACT_DATE_FORMAT);
+        // Change bs-datepick behavior and put the string birthday in input
+        scope.$applyAsync(function() {
+          controller.$parsers = [];
+
+          controller.$render = function() {
+            element.val($dateFormatter.formatDate(controller.$viewValue, CONTACT_DATE_FORMAT));
+          };
+
+          if (angular.isString(scope.contact.birthday)) {
+            element.val(scope.contact.birthday);
+          }
         });
       }
     };
