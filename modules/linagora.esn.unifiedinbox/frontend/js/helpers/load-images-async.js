b77152eac066d40f390c03ec43430508cd37e462
ddolcimascolo <ddolcimascolo@linagora.com>
2016-02-18 12:36:53 +0100

    INBOX-141 Do not try to be smart when there is an error loading images.

--- modules/linagora.esn.unifiedinbox/frontend/js/helpers/load-images-async.js
+++ modules/linagora.esn.unifiedinbox/frontend/js/helpers/load-images-async.js
@@ -10,7 +10,6 @@ AsyncImageLoader.prototype.load = function() {
     var node = this.nodes.item(i);
 
     node.onload = this.onLoad.bind(this);
-    node.onerror = this.onError.bind(this, node);
     node.src = node.getAttribute('data-async-src');
   }
 };
@@ -19,14 +18,6 @@ AsyncImageLoader.prototype.onLoad = function() {
   if (++this.nbLoaded === this.nodes.length) {
     window.parentIFrame.size();
   }
-};
-
-AsyncImageLoader.prototype.onError = function(image) {
-  image.src = '/images/ic_broken_image_black_48px.svg';
-  image.width = 48;
-  image.height = 48; // Width and height of the "broken image" icon
-
-  return true;
 };
 
 new AsyncImageLoader().load();
