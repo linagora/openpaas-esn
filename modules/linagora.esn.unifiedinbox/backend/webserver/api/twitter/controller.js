555222f2e7500f954dbec363f2bebd827596db18
Stephen Le Maistre <slemaistre@linagora.com>
2016-03-15 13:34:34 +0100

    INBOX-256 transform mentions datas received by the twitter api

--- modules/linagora.esn.unifiedinbox/backend/webserver/api/twitter/controller.js
+++ modules/linagora.esn.unifiedinbox/backend/webserver/api/twitter/controller.js
@@ -1,40 +1,38 @@
 'use strict';
 
-var Twitter = require('twitter-node-client').Twitter;
+var twitter = require('./core.js');
+var q = require('q');
 
 module.exports = function(dependencies) {
-
-  var error = function (err, response, body) {
-    console.log('ERROR [%s]', err);
-  };
-
-  var success = function (data) {
-    console.log('Data [%s]', data);
-  };
+  var esnconfig = dependencies('esn-config');
+  var logger = dependencies('logger');
 
   function getTweets(req, res) {
     var account = req.account;
-    var config = dependencies('esn-config');
 
-    config('oauth').get(function(err, oauth) {
-
-      if (!(oauth && oauth.twitter)) {
-        return defer.reject('Can not get oauth configuration for twitter importer');
-      }
-
-      var twitterConfig = {
-        consumerKey: oauth.twitter.consumer_key,
-        consumerSecret: oauth.twitter.consumer_secret,
-        accessToken: account.data.token,
-        accessTokenSecret: account.data.token_secret,
-        callBackUrl: ''
-      };
-      var twitter = new Twitter(twitterConfig);
-      twitter.getMentionsTimeline({ count: '10'}, error, success);
-      twitter.getCustomApiCall('/direct_messages.json',{ count: '10'}, error, success);
-
-      return res.status(200).end();
-    });
+    q.ninvoke(esnconfig('oauth'), 'get')
+      .then(function(oauth) {
+        if (!(oauth && oauth.twitter)) {
+          logger.error('Can not get oauth configuration for twitter configuration');
+          return res.status(400).json({error: {code: 400, message: 'Can not get oauth configuration for twitter configuration', details: 'Can not get oauth configuration for twitter configuration'}});
+        }
+        var twitterConfig = {
+          consumerKey: oauth.twitter.consumer_key,
+          consumerSecret: oauth.twitter.consumer_secret,
+          accessToken: account.data.token,
+          accessTokenSecret: account.data.token_secret,
+          callBackUrl: ''
+        };
+        return twitter.getTweets(twitterConfig);
+      })
+      .then(function(tweets) {
+        logger.info('Successfully fetched tweets.');
+        return res.status(200).json(tweets);
+      })
+      .catch(function(err) {
+        logger.error('Error when trying to fetch tweets', err);
+        return res.status(500).json({error: {code: 500, message: 'Error when trying to fetch tweets', details: err.message}});
+      });
   }
 
   return {
