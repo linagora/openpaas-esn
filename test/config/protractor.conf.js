7b35ab5be505dd7cc0a3bd1b8580e06b13658794
Thomas Hilaire <thilaire@linagora.com>
2016-03-23 10:57:27 +0100

    OR-2201 Use global phantomjs bin if found

--- test/config/protractor.conf.js
+++ test/config/protractor.conf.js
@@ -3,6 +3,7 @@
 //jscs:disable
 
 var testConfig = require('./servers-conf.js');
+var globalPhantomjsBin = require('fs').existsSync('/usr/local/bin/phantomjs') ? '/usr/local/bin/phantomjs' : undefined;
 
 exports.config = {
   baseUrl: 'http://' + testConfig.host + ':' + testConfig.express.port,
@@ -10,7 +11,7 @@ exports.config = {
   framework: 'mocha',
   capabilities: {
     'browserName': process.env.BROWSER || 'phantomjs',
-    'phantomjs.binary.path': './node_modules/karma-phantomjs-launcher/node_modules/phantomjs/bin/phantomjs',
+    'phantomjs.binary.path': process.env.PHANTOMJS_BIN || globalPhantomjsBin || './node_modules/karma-phantomjs-launcher/node_modules/phantomjs/bin/phantomjs',
     'phantomjs.ghostdriver.cli.args': ''
   },
   suites: {
