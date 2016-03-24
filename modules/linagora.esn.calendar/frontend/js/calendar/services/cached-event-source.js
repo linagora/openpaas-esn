a9975342ee96d2a006bf358ac7e7e7b8a6991e76
Luc DUZAN <lduzan@linagora.com>
2016-03-24 17:38:01 +0100

    CAL-272 add a service to store fetched calendar period

--- modules/linagora.esn.calendar/frontend/js/calendar/services/cached-event-source.js
+++ modules/linagora.esn.calendar/frontend/js/calendar/services/cached-event-source.js
@@ -97,4 +97,97 @@ angular.module('esn.calendar').factory('cachedEventSource', function($timeout, C
     deleteRegistration: deleteRegistration,
     wrapEventSource: wrapEventSource
   };
+}).factory('calendarExploredPeriodService', function(_) {
+
+  var exploredCalsPeriods = {};
+
+  function min(a, b) {
+    return a.isBefore(b, 'day') ? a : b;
+  }
+
+  function max(a, b) {
+    return a.isAfter(b, 'day') ? a : b;
+  }
+
+  function merge(a, b) {
+    return {
+      start: min(a.start, b.start),
+      end: max(a.end, b.end)
+    };
+  }
+
+  //return the part of a that is not in b
+  function difference(a, b) {
+    var result = [];
+
+    if (a.start.isBefore(b.start, 'day')) {
+      result.push({
+        start: a.start,
+        end: min(a.end, b.start.clone().subtract(1, 'day'))
+      });
+    }
+
+    if (a.end.isAfter(b.end, 'day')) {
+      result.push({
+        start: max(a.start, b.end.clone().add(1, 'day')),
+        end: a.end
+      });
+    }
+
+    return result;
+  }
+
+  function periodAreConnected(a, b) {
+    if (a.start.isAfter(b.start)) {
+      return periodAreConnected(b, a);
+    }
+
+    return b.start.isBefore(a.end.clone().add(2, 'day'), 'day');
+  }
+
+  function getExploredPeriods(calId) {
+    exploredCalsPeriods[calId] = exploredCalsPeriods[calId] || [];
+    return exploredCalsPeriods[calId];
+  }
+
+  function registerExploredPeriod(calId, period) {
+    var exploredPeriods = getExploredPeriods(calId);
+    if (!exploredPeriods.length) {
+      exploredPeriods.push(period);
+      return;
+    }
+
+    var insertionIndex = 0;
+    var numOfMergedPeriod = 0;
+
+    exploredPeriods.forEach(function(exploredPeriod, i) {
+      if (exploredPeriod.end.isBefore(period.start.clone().subtract(1, 'day'), 'day')) {
+        insertionIndex = i + 1;
+      } else if (periodAreConnected(exploredPeriod, period)) {
+        period = merge(exploredPeriod, period);
+        numOfMergedPeriod++;
+      }
+    });
+
+    exploredPeriods.splice(insertionIndex, numOfMergedPeriod, period);
+  }
+
+  function reset(calId) {
+    delete exploredCalsPeriods[calId];
+  }
+
+  function getUnexploredPeriodsInPeriod(calId, period) {
+    var exploredPeriods = getExploredPeriods(calId);
+    return exploredPeriods.reduce(function(prev, exploredPeriod) {
+      return _.flatten(prev.map(function(unexploredPeriod) {
+        return difference(unexploredPeriod, exploredPeriod);
+      }));
+    }, [period]);
+  }
+
+  return {
+    registerExploredPeriod: registerExploredPeriod,
+    reset: reset,
+    getUnexploredPeriodsInPeriod: getUnexploredPeriodsInPeriod
+  };
 });
--- modules/linagora.esn.calendar/test/unit-frontend/calendar/services/cached-event-source.js
+++ modules/linagora.esn.calendar/test/unit-frontend/calendar/services/cached-event-source.js
@@ -203,3 +203,104 @@ describe('The cachedEventSource service', function() {
 
   });
 });
+
+describe('the calendarExploredPeriodService service', function() {
+
+  var self;
+
+  function buildPeriod(start, end) {
+    return {
+      start: self.fcMoment([2000, 1, start]).stripTime(),
+      end: self.fcMoment([2000, 1, end]).stripTime()
+    };
+  }
+
+  function periodToComparablePeriod(period) {
+    return {
+      start: period.start.format('YYYY-MM-DD'),
+      end: period.end.format('YYYY-MM-DD')
+    };
+  }
+
+  beforeEach(function() {
+    self = this;
+    angular.mock.module('esn.calendar');
+  });
+
+  beforeEach(angular.mock.inject(function(fcMoment, calendarExploredPeriodService) {
+    this.calendarExploredPeriodService = calendarExploredPeriodService;
+    this.fcMoment = fcMoment;
+    this.aPeriod = buildPeriod(1, 15);
+  }));
+
+  describe('the reset function', function() {
+
+    it('should remove all registeredExploredPeriod', function() {
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', this.aPeriod);
+      this.calendarExploredPeriodService.reset('calId');
+      expect(this.calendarExploredPeriodService.getUnexploredPeriodsInPeriod('calId', this.aPeriod).map(periodToComparablePeriod)).to.deep.equals([this.aPeriod].map(periodToComparablePeriod));
+    });
+  });
+
+  describe('the regiserExploredPeriod', function() {
+
+    it('should save a explored period in his calendar', function() {
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', this.aPeriod);
+      expect(this.calendarExploredPeriodService.getUnexploredPeriodsInPeriod('calId', this.aPeriod)).to.deep.equals([]);
+      expect(this.calendarExploredPeriodService.getUnexploredPeriodsInPeriod('calId2', this.aPeriod)).to.not.deep.equals();
+    });
+
+    it('should groups adjudent period', function() {
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(1, 2));
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(3, 4));
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(5, 6));
+      expect(this.calendarExploredPeriodService.getUnexploredPeriodsInPeriod('calId', buildPeriod(1, 6))).to.deep.equals([]);
+    });
+
+    it('should not groups non adjacent period', function() {
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(1, 2));
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(4, 5));
+      expect(this.calendarExploredPeriodService.getUnexploredPeriodsInPeriod('calId', buildPeriod(1, 4))).to.not.deep.equals([]);
+    });
+
+    it('should remove periods included by a bigger one', function() {
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(1, 2));
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(4, 5));
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(1, 5));
+      expect(this.calendarExploredPeriodService.getUnexploredPeriodsInPeriod('calId', buildPeriod(1, 5))).to.deep.equals([]);
+    });
+
+    it('should remove periods included by a bigger one and groups resulting adjacent period', function() {
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(1, 2));
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(4, 5));
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(6, 7));
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(1, 5));
+      expect(this.calendarExploredPeriodService.getUnexploredPeriodsInPeriod('calId', buildPeriod(1, 7))).to.deep.equals([]);
+    });
+  });
+
+  describe('the getUnexploredPeriodsInPeriod', function() {
+    it('should return the full period if non of it has been explored', function() {
+      expect(this.calendarExploredPeriodService.getUnexploredPeriodsInPeriod('calId', this.aPeriod).map(periodToComparablePeriod)).to.deep.equals([this.aPeriod].map(periodToComparablePeriod));
+    });
+
+    it('should return nothing if the period already has been explored', function() {
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(1, 4));
+      expect(this.calendarExploredPeriodService.getUnexploredPeriodsInPeriod('calId', buildPeriod(2, 3))).to.deep.equals([]);
+    });
+
+    it('should return only non explored part of a period that has been partially explored', function() {
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(3, 4));
+      this.calendarExploredPeriodService.registerExploredPeriod('calId', buildPeriod(6, 7));
+
+      var result = this.calendarExploredPeriodService.getUnexploredPeriodsInPeriod('calId', buildPeriod(1, 9));
+      expect(result.map(periodToComparablePeriod)).to.deep.equals([buildPeriod(1, 2), buildPeriod(5, 5), buildPeriod(8, 9)].map(periodToComparablePeriod));
+
+      result = this.calendarExploredPeriodService.getUnexploredPeriodsInPeriod('calId', buildPeriod(1, 3));
+      expect(result.map(periodToComparablePeriod)).to.deep.equals([buildPeriod(1, 2)].map(periodToComparablePeriod));
+
+      result = this.calendarExploredPeriodService.getUnexploredPeriodsInPeriod('calId', buildPeriod(6, 10));
+      expect(result.map(periodToComparablePeriod)).to.deep.equals([buildPeriod(8, 10)].map(periodToComparablePeriod));
+    });
+  });
+});
