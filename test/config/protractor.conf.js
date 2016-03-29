dc4d4bd0fa88b3e50aa07787af3ec1e9f1efa2cf
Thomas Hilaire <thilaire@linagora.com>
2016-03-29 12:43:45 +0200

    OR-2202 Use Jasmine as runner until OR-2211

--- test/config/protractor.conf.js
+++ test/config/protractor.conf.js
@@ -8,7 +8,7 @@ var globalPhantomjsBin = require('fs').existsSync('/usr/local/bin/phantomjs') ? 
 exports.config = {
   baseUrl: 'http://' + testConfig.host + ':8080',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  framework: 'mocha',
+  framework: 'jasmine',
   capabilities: {
     'browserName': process.env.BROWSER || 'phantomjs',
     'phantomjs.binary.path': process.env.PHANTOMJS_BIN || globalPhantomjsBin || './node_modules/karma-phantomjs-launcher/node_modules/phantomjs/bin/phantomjs',
