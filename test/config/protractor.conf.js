d757b2e151d931c9f6cd628276495061c24cf33a
Sang NGO DOAN <sngodoan@linagora.com>
2016-04-09 11:41:14 +0700

    OR-2237 Upgrade to cucumber 0.10 and use standalone PhantomJS 2

--- test/config/protractor.conf.js
+++ test/config/protractor.conf.js
@@ -6,15 +6,15 @@ var testConfig = require('./servers-conf.js'),
     globalPhantomjsBin = require('fs').existsSync('/usr/local/bin/phantomjs') ? '/usr/local/bin/phantomjs' : undefined,
     seleniumWebapp = 'http://localhost:4444';
 
-
 exports.config = {
   baseUrl: 'http://' + testConfig.host + ':8080',
   seleniumWebapp: seleniumWebapp,
   seleniumAddress: seleniumWebapp + '/wd/hub',
-  framework: 'cucumber',
+  framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
   capabilities: {
     'browserName': process.env.BROWSER || 'phantomjs',
-    'phantomjs.binary.path': process.env.PHANTOMJS_BIN || globalPhantomjsBin || './node_modules/karma-phantomjs-launcher/node_modules/phantomjs/bin/phantomjs',
+    'phantomjs.binary.path': process.env.PHANTOMJS_BIN || globalPhantomjsBin || require('phantomjs').path,
     'phantomjs.ghostdriver.cli.args': ''
   },
   suites: {
