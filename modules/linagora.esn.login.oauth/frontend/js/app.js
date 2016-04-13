67de3bf291bb42b05d194f1199e0202af42bc3b7
Christophe Hamerling <chamerling@linagora.com>
2016-04-13 11:22:53 +0200

    OR-2244 Login with twitter

--- modules/linagora.esn.login.oauth/frontend/js/app.js
+++ modules/linagora.esn.login.oauth/frontend/js/app.js
@@ -2,9 +2,4 @@
 
 angular.module('linagora.esn.login.oauth', [
   'op.dynamicDirective'
-])
-  .run(function(dynamicDirectiveService) {
-    dynamicDirectiveService.addInjection('esn-login-oauth', new dynamicDirectiveService.DynamicDirective(function() {
-      return true;
-    }, 'login-oauth-facebook-button'));
-  });
+]);
