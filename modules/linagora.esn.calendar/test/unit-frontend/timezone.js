42409e088379949bf456d61fc8f03e89d12d15bb
Luc DUZAN <lduzan@linagora.com>
2016-05-13 16:16:11 +0200

    CAL-275 register timezones in ICAL.js

---
+++ modules/linagora.esn.calendar/test/unit-frontend/timezone.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/* global chai: false */
+
+var expect = chai.expect;
+
+describe('The registerTimezones factory', function() {
+  var ICAL, TIMEZONES;
+
+  beforeEach(function() {
+    angular.mock.module('esn.calendar');
+    angular.mock.inject(function(registerTimezones, _ICAL_, _TIMEZONES_) {
+      registerTimezones();
+      ICAL = _ICAL_;
+      TIMEZONES = _TIMEZONES_;
+    });
+
+  });
+
+  it('should have register timezone', function() {
+    angular.forEach(TIMEZONES.zones, function(_data, key) {
+      expect(ICAL.TimezoneService.get(key)).to.be.ok;
+      expect(ICAL.TimezoneService.get(key).tzid).to.equal(key);
+    });
+  });
+
+  it('should have register alias', function() {
+    angular.forEach(TIMEZONES.aliases, function(_data, key) {
+      expect(ICAL.TimezoneService.get(key)).to.be.ok;
+    });
+  });
+
+  it('should correctly follow alias', function() {
+    angular.forEach(TIMEZONES.aliases, function(data, key) {
+      expect(ICAL.TimezoneService.get(key)).to.equal(ICAL.TimezoneService.get(data.aliasTo));
+    });
+  });
+
+});
