495e28fdc3a6a39c3fc44e3394fdd04f877c82cb
Christophe Hamerling <chamerling@linagora.com>
2016-06-08 10:45:42 +0200

    OR-2264 Swagger documentation

--- backend/webserver/api/resource-link.js
+++ backend/webserver/api/resource-link.js
@@ -6,6 +6,36 @@ var linkController = require('../controllers/resource-link');
 
 module.exports = function(router) {
 
+  /**
+   * @swagger
+   * /api/resource-links:
+   *   post:
+   *     description:
+   *       Create a new typed link between resources. For example, create a 'like' link between a user (source) and a message (target)
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - name: resource-link
+   *         in: body
+   *         required: true
+   *         schema:
+   *           $ref: "#/definitions/ResourceLinkRequest"
+   *     responses:
+   *       "201":
+   *         description: Created.
+   *         schema:
+   *           type: object
+   *           items:
+   *             $ref: "#/definitions/ResourceLink"
+   *       "400":
+   *         description: Bad request
+   *       "500":
+   *         description: Internal server error - Something went bad on the server side.
+   *       default:
+   *         description: Error
+   *         schema:
+   *           $ref: "#/definitions/ErrorResponse"
+   */
   router.post('/resource-links',
     authorize.requiresAPILogin,
     linkMiddleware.isResourceLink,
---
+++ doc/REST_API/swagger/definitions/tuples.js
@@ -0,0 +1,13 @@
+/**
+ * @swagger
+ * definition:
+ *   Tuple:
+ *     type: object
+ *     properties:
+ *       id:
+ *        type: string
+ *        description: ID of the tuple.
+ *       objectType:
+ *         type: string
+ *         description: type of the tuple (user, esn.message, ...)
+ */
