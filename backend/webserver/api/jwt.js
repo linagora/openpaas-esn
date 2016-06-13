4de311e8e1a7c1768cc6c478b7c084100d3579a8
ymaligoy <ymaligoy@linagora.com>
2016-06-13 10:37:23 +0200

    OR-2286: add the jwt swagger documentation

--- backend/webserver/api/jwt.js
+++ backend/webserver/api/jwt.js
@@ -4,5 +4,21 @@ var authorize = require('../middleware/authorization');
 var jwt = require('../controllers/authjwt');
 
 module.exports = function(router) {
+
+  /**
+   * @swagger
+   * /jwt/generate:
+   *   post:
+   *     tags:
+   *       - JWT
+   *     description: Generate a new JWT for the logged in user.
+   *     responses:
+   *       200:
+   *         $ref: "#/responses/jw_generate"
+   *       401:
+   *         $ref: "#/responses/cm_401"
+   *       500:
+   *         $ref: "#/responses/cm_500"
+   */
   router.post('/jwt/generate', authorize.requiresAPILogin, jwt.generateWebToken);
 };
