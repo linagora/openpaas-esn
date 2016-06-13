ce2e5b148236a676adaf41b704a2e8914adeaaeb
ymaligoy <ymaligoy@linagora.com>
2016-06-13 10:23:46 +0200

    OR-2286: add the authentication swagger documentation

---
+++ doc/REST_API/swagger/definitions/authentication.js
@@ -0,0 +1,15 @@
+/**
+  * @swagger
+  * definition:
+  *   at_token:
+  *     description: token content
+  *     properties:
+  *       token:
+  *         type: string
+  *       ttl:
+  *         type: integer
+  *       user:
+  *         $ref: "#/definitions/cm_tuple_id"
+  *       created_at:
+  *         $ref: "#/definitions/cm_date"
+  */
