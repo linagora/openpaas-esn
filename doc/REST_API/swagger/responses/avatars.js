a36ba2f5e9d21f447ca90c3d09d377622a80ea85
ymaligoy <ymaligoy@linagora.com>
2016-06-13 10:27:53 +0200

    OR-2286: add the avatars swagger documentation

---
+++ doc/REST_API/swagger/responses/avatars.js
@@ -0,0 +1,13 @@
+/**
+  * @swagger
+  * response:
+  *   av_stream:
+  *     description: Ok. With the stream of the avatar if found or a default avatar
+  *     headers:
+  *       "Last-Modified":
+  *         description: indicates the date and time at which the origin server believes the avatar was last modified.
+  *         type: string
+  *         format: date-time
+  *     schema:
+  *       type: file
+  */
