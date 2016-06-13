9353e8011f2483e547f1cc2eeab68bdbf5981719
ymaligoy <ymaligoy@linagora.com>
2016-06-13 10:38:08 +0200

    OR-2286: add the locales swagger documentation

--- backend/webserver/api/locales.js
+++ backend/webserver/api/locales.js
@@ -3,7 +3,45 @@
 var locale = require('../controllers/locale');
 
 module.exports = function(router) {
+
+  /**
+   * @swagger
+   * /locales:
+   *   get:
+   *     tags:
+   *       - Locale
+   *     description: Get the list of available locales.
+   *     responses:
+   *       200:
+   *         $ref: "#/responses/lo_list"
+   */
   router.get('/locales', locale.getAll);
+
+  /**
+   * @swagger
+   * /locales/current:
+   *   get:
+   *     tags:
+   *       - Locale
+   *     description: Get the current locale data.
+   *     responses:
+   *       200:
+   *         $ref: "#/responses/lo_current"
+   */
   router.get('/locales/current', locale.get);
+
+  /**
+   * @swagger
+   * /locales/{locale}:
+   *   get:
+   *     tags:
+   *       - Locale
+   *     description: Get the locale data.
+   *     parameters:
+   *       - $ref: "#/parameters/lo_name"
+   *     responses:
+   *       200:
+   *         $ref: "#/responses/lo_locale"
+   */
   router.get('/locales/:locale', locale.set);
 };
