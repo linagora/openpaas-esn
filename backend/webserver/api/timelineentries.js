b5047333b537a17bf3a259b96da9bd2b2bf06a0c
Christophe Hamerling <chamerling@linagora.com>
2016-06-22 23:39:56 +0200

    COM-222 Update the swagger doc

--- backend/webserver/api/timelineentries.js
+++ backend/webserver/api/timelineentries.js
@@ -7,9 +7,13 @@ var timelineentries = require('../controllers/timelineentries');
 module.exports = function(router) {
   /**
    * @swagger
-   * /timelineentries :
+   * /timelineentries:
    *   get:
-   *     description: Query the timeline of the current user
+   *     description:
+   *       Query the timeline of the current user
+   *     parameters:
+   *       - $ref: "#/parameters/tl_limit"
+   *       - $ref: "#/parameters/tl_offset"
    *     responses:
    *       200:
    *         $ref: "#/responses/tl_entry"
---
+++ doc/REST_API/swagger/parameters/timelineentries.js
@@ -0,0 +1,17 @@
+/**
+  * @swagger
+  * parameter:
+  *   tl_limit:
+  *     name: limit
+  *     in: query
+  *     description: The maximum number of timeline entries to include in the response.
+  *     required: false
+  *     type: integer
+  *   tl_offset:
+  *     name: offset
+  *     in: query
+  *     description: Start the list of results after skipping N results (where N=offset).
+  *     required: false
+  *     default: 0
+  *     type: integer
+  */
