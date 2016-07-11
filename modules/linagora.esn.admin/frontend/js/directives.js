bbc9b7d703580863456c2c9dd6935aac31baa6dc
tlcong <tlcong@linagora.com>
2016-07-11 16:26:33 +0700

    ADM-15 Insert admin item to application grid

---
+++ modules/linagora.esn.admin/frontend/js/directives.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('linagora.esn.admin')
+
+.directive('adminApplicationMenu', function(applicationMenuTemplateBuilder) {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: applicationMenuTemplateBuilder('/#/admin', 'mdi-server-security', 'Administrator')
+  };
+});
