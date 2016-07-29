02f3c1847856f704e17b76581a9be40b73507350
Christophe Hamerling <chamerling@linagora.com>
2016-07-29 16:57:42 +0200

    OR-2363 Make i18n configurable

--- backend/i18n/index.js
+++ backend/i18n/index.js
@@ -1,16 +1,24 @@
 'use strict';
 
+var extend = require('extend');
 var i18n = require('i18n');
-i18n.configure(
-  {
-    defaultLocale: 'en',
-    locales: ['en', 'fr'],
-    directory: __dirname + '/locales',
-    updateFiles: false,
-    indent: '  ',
-    extension: '.json',
-    cookie: 'locale'
-  }
-);
+var path = require('path');
+var isAbsolute = require('path-is-absolute');
+var localConfig = require('../core/config')('default');
+var ROOT_FOLDER = path.normalize(__dirname + '../../..');
+
+var i18nConfig = {
+  defaultLocale: 'en',
+  locales: ['en', 'fr'],
+  directory: './backend/i18n/locales',
+  updateFiles: false,
+  indent: '  ',
+  extension: '.json',
+  cookie: 'locale'
+};
+
+extend(i18nConfig, localConfig.i18n || {});
+i18nConfig.directory = isAbsolute(i18nConfig.directory) ? i18nConfig.directory : path.join(ROOT_FOLDER, i18nConfig.directory);
+i18n.configure(i18nConfig);
 
 exports = module.exports = i18n;
