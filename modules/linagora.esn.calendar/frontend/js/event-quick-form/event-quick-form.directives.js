ac8adc4af756b3cbd11adcd71917f021f59ab61a
Stephen Le Maistre <slemaistre@linagora.com>
2016-09-19 16:30:12 +0200

    CAL-402 rewrite event-quick-form feature

---
+++ modules/linagora.esn.calendar/frontend/js/event-quick-form/event-quick-form.directives.js
@@ -0,0 +1,45 @@
+(function() {
+  'use strict';
+
+  angular.module('esn.calendar')
+         .directive('eventQuickForm', eventQuickForm);
+
+  eventQuickForm.$inject = [
+    '$timeout',
+    'eventUtils'
+  ];
+
+  function eventQuickForm($timeout, eventUtils) {
+    var directive = {
+      restrict: 'E',
+      templateUrl: '/calendar/views/event-quick-form/event-quick-form.html',
+      link: link,
+      replace: true,
+      controller: 'eventFormController'
+    };
+
+    return directive;
+
+    ////////////
+
+    function link(scope, element) {
+      $timeout(focusTitle, 0);
+      element.on('$destroy', eventUtils.resetStoredEvents);
+      scope.$on('$locationChangeStart', hideModal);
+
+      ////////////
+
+      function focusTitle() {
+        element.find('.title')[0].focus();
+      }
+
+      function hideModal(event) {
+        if (scope.$isShown) {
+          event.preventDefault();
+          scope.$hide();
+        }
+      }
+    }
+  }
+
+})();
