026b51e8772d7b64154a4061a634b261b99d4cc6
Sang NGO DOAN <sngodoan@linagora.com>
2016-09-19 11:34:51 +0700

    ADM-53 Correct LDAP configuration fixture data

--- fixtures/esn-config/data/ldap.js
+++ fixtures/esn-config/data/ldap.js
@@ -1,11 +1,14 @@
 'use strict';
 
 module.exports = function() {
-  return {
-    url: 'ldap://localhost:1389',
-    adminDn: 'uid=admin,ou=passport-ldapauth',
-    adminPassword: 'secret',
-    searchBase: 'ou=passport-ldapauth',
-    searchFilter: '(uid={{username}})'
-  };
+  return [{
+    name: 'OpenPaas',
+    configuration: {
+      url: 'ldap://localhost:1389',
+      adminDn: 'uid=admin,ou=passport-ldapauth',
+      adminPassword: 'secret',
+      searchBase: 'ou=passport-ldapauth',
+      searchFilter: '(uid={{username}})'
+    }
+  }];
 };
