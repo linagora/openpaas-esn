93dfc0113d3bdc7c22cbcc84cdbff394766919d5
Stephen Le Maistre <slemaistre@linagora.com>
2016-09-21 15:16:14 +0200

    CAL-402 fix missing IIFE

--- modules/linagora.esn.calendar/frontend/app/services/ical.js
+++ modules/linagora.esn.calendar/frontend/app/services/ical.js
@@ -1,22 +1,23 @@
-/*global ICAL */
+/* global ICAL */
+(function() {
+  'use strict';
 
-'use strict';
-
-angular.module('esn.ical', [])
-  .constant('ICAL', ICAL)
-  .constant('ICAL_PROPERTIES', {
-    partstat: {
-      needsaction: 'NEEDS-ACTION',
-      accepted: 'ACCEPTED',
-      declined: 'DECLINED',
-      tentative: 'TENTATIVE'
-    },
-    rsvp: {
-      true: 'TRUE',
-      false: 'FALSE'
-    },
-    role: {
-      reqparticipant: 'REQ-PARTICIPANT',
-      chair: 'CHAIR'
-    }
-  });
+  angular.module('esn.ical', [])
+    .constant('ICAL', ICAL)
+    .constant('ICAL_PROPERTIES', {
+      partstat: {
+        needsaction: 'NEEDS-ACTION',
+        accepted: 'ACCEPTED',
+        declined: 'DECLINED',
+        tentative: 'TENTATIVE'
+      },
+      rsvp: {
+        true: 'TRUE',
+        false: 'FALSE'
+      },
+      role: {
+        reqparticipant: 'REQ-PARTICIPANT',
+        chair: 'CHAIR'
+      }
+    });
+})();
