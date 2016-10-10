fca6808f11551b9bde57cb5b582054dae4b973fd
arouis <arouis@linagora.com>
2016-10-10 17:37:48 +0100

    TIC-18 Extract common code with OP to the core

--- modules/linagora.esn.unifiedinbox/backend/webserver/constants.js
+++ modules/linagora.esn.unifiedinbox/backend/webserver/constants.js
@@ -3,3 +3,4 @@
 // add your constants here.
 var path = require('path');
 module.exports.FRONTEND_PATH = path.normalize(__dirname + '/../../frontend');
+module.exports.CORE_FRONTEND_PATH = path.normalize(__dirname + '/../../../../frontend');
