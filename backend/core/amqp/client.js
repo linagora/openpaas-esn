7290f237cdc149c8e088820fa572e0a22d7ce562
Thomas Hilaire <thilaire@linagora.com>
2016-11-16 11:44:10 +0100

    CAL-437 Add EventEmitter API aliases

--- backend/core/amqp/client.js
+++ backend/core/amqp/client.js
@@ -66,6 +66,19 @@ class AmqpClient {
     return q.when();
   }
 
+  // aliases to fit the EventEmitter API
+  emit(topic, data) {
+    return this.publish(topic, data);
+  }
+
+  on(topic, callback) {
+    return this.subscribe(topic, callback);
+  }
+
+  removeListener(topic, callback) {
+    return this.unsubscribe(topic, callback);
+  }
+
 }
 
 module.exports = AmqpClient;
