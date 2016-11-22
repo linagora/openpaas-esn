aca6f5622bf978bf7c618c484703ff44085dd673
vietpham <vpham@linagora.com>
2016-11-22 15:26:36 +0700

    ADM-104 Add a back button in a user detail page

--- modules/linagora.esn.profile/frontend/js/controllers.js
+++ modules/linagora.esn.profile/frontend/js/controllers.js
@@ -2,9 +2,12 @@
 
 angular.module('linagora.esn.profile')
 
-  .controller('profileController', function($scope, user, session) {
+  .controller('profileController', function($scope, $window, user, session) {
     $scope.user = user;
     $scope.me = session.user._id === $scope.user._id;
+    $scope.back = function() {
+      $window.history.back();
+    }
   })
   .controller('profileEditionController', function($scope, $log, user, session, profileAPI, notificationFactory) {
     $scope.user = user;
