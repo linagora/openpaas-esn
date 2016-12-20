b998beef836ab493f59148940b035102033f6b45
Thomas Bille <tbille@linagora.com>
2016-12-20 16:29:05 +0100

    CAL-270 Fix error when using equals with undefined object and Add tests to valarm-shell

---
+++ modules/linagora.esn.calendar/frontend/app/services/shells/valarm-shell.spec.js
@@ -0,0 +1,93 @@
+'use strict';
+
+/* global chai, sinon, _, __FIXTURES__: false */
+
+var expect = chai.expect;
+
+describe('VAlarmShell factory', function() {
+  var CalendarShell, ICAL, CalVAlarmShell;
+
+  beforeEach(function() {
+    angular.mock.module('esn.calendar');
+    angular.mock.inject(function(_CalendarShell_, _ICAL_, _CalVAlarmShell_) {
+      CalendarShell = _CalendarShell_;
+      ICAL = _ICAL_;
+      CalVAlarmShell = _CalVAlarmShell_;
+    });
+
+    function getComponentFromFixture(string) {
+      var path = 'modules/linagora.esn.calendar/frontend/app/fixtures/calendar/valarm_test/' + string;
+
+      return new ICAL.Component(JSON.parse(__FIXTURES__[path]));
+    }
+
+    this.alarm = getComponentFromFixture('valarm.json');
+    this.event = getComponentFromFixture('vevent.json');
+
+    this.myAlarmShell = new CalVAlarmShell(this.alarm, this.event);
+  });
+
+  describe('action fn', function() {
+    it('should return the action type', function() {
+      expect(this.myAlarmShell.action).to.equal('EMAIL');
+    });
+  });
+
+  describe('trigger fn', function() {
+    it('should return the trigger', function() {
+      expect(this.myAlarmShell.trigger.toICALString()).to.equal('-PT1M');
+    });
+  });
+
+  describe('description fn', function() {
+    it('should return the description', function() {
+      expect(this.myAlarmShell.description).to.equal('This is a description');
+    });
+  });
+
+  describe('summary fn', function() {
+    it('should return the summary', function() {
+      expect(this.myAlarmShell.summary).to.equal('Pending event! ');
+    });
+  });
+
+  describe('attendee fn', function() {
+    it('should return the attendee', function() {
+      expect(this.myAlarmShell.attendee).to.equal('mailto:admin@open-paas.org');
+    });
+  });
+
+  describe('equals fn', function() {
+    it('should return false if the value is undefined', function() {
+      expect(this.myAlarmShell.equals()).to.be.false;
+    });
+
+    it('should return true if the value is the same object alam shell', function() {
+      expect(this.myAlarmShell.equals(this.myAlarmShell)).to.be.true;
+    });
+
+    it('should return true if the value is the same alam shell', function() {
+      var myAlarmShellEqual = new CalVAlarmShell(this.alarm, this.event);
+
+      expect(this.myAlarmShell.equals(myAlarmShellEqual)).to.be.true;
+    });
+
+    it('should return false if the trigger value is different', function() {
+      var newAlarm = ICAL.Component.fromString(this.alarm.toString());
+
+      newAlarm.updatePropertyWithValue('trigger', '-PT12H');
+      var myAlarmShellDifferent = new CalVAlarmShell(newAlarm, this.event);
+
+      expect(this.myAlarmShell.equals(myAlarmShellDifferent)).to.be.false;
+    });
+
+    it('should return false if one of the other value is different', function() {
+      var newAlarm = ICAL.Component.fromString(this.alarm.toString());
+
+      newAlarm.updatePropertyWithValue('summary', 'My new summary');
+      var myAlarmShellDifferent = new CalVAlarmShell(newAlarm, this.event);
+
+      expect(this.myAlarmShell.equals(myAlarmShellDifferent)).to.be.false;
+    });
+  });
+});
