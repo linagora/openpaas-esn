99a3d5b711ade03cfd6a6d5c8f833bfa9715a14b
Sang NGO DOAN <sngodoan@linagora.com>
2017-01-13 14:57:16 +0700

    ADM-129 Fix model is not of type number in mail configuration page
    This fixes an issue of `input(type="number")` in Angular, if ng-model's value
    (comes from database) is a string, it will throw an error
    https://docs.angularjs.org/error/ngModel/numfmt

--- fixtures/esn-config/data/redis.js
+++ fixtures/esn-config/data/redis.js
@@ -3,6 +3,6 @@
 module.exports = function() {
   return {
     host: process.env.REDIS_HOST || 'localhost',
-    port: process.env.REDIS_PORT || '6379'
+    port: +process.env.REDIS_PORT || 6379
   };
 };
