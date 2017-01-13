f88730196d7051810d5eec1dc3ad73ba0fbc75a0
Sang NGO DOAN <sngodoan@linagora.com>
2017-01-13 13:48:40 +0700

    OR-2497 Update provision command run in Docker

--- docker/scripts/provision.sh
+++ docker/scripts/provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo 'Provisioning ESN configuration and data'
-node ./bin/cli configure --host $MONGO_HOST --port $MONGO_PORT --database $MONGO_DBNAME
+node ./bin/cli configure
 node ./bin/cli elasticsearch --host $ELASTICSEARCH_HOST --port $ELASTICSEARCH_PORT
-node ./bin/cli populate --host $MONGO_HOST --port $MONGO_PORT --database $MONGO_DBNAME
+node ./bin/cli populate
