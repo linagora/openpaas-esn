d6eb5a92b4a10379507e96122db635fae61ecfde
tlcong <tlcong@linagora.com>
2017-03-27 18:56:42 +0700

    ADM-159 create controlcenterGeneralService

---
+++ modules/linagora.esn.controlcenter/frontend/app/general/controlcenter-general.service.spec.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/* global chai: false */
+
+var expect = chai.expect;
+
+describe('The controlcenterGeneralService factory', function() {
+  var esnModuleRegistry, controlcenterGeneralService;
+  var modulesMock;
+
+  beforeEach(function() {
+    module('linagora.esn.controlcenter');
+
+    modulesMock = [
+      { homePage: 'a', title: 'A' },
+      { homePage: 'b', title: 'B' }
+    ];
+
+    inject(function(_esnModuleRegistry_, _controlcenterGeneralService_) {
+      esnModuleRegistry = _esnModuleRegistry_;
+      controlcenterGeneralService = _controlcenterGeneralService_;
+    });
+
+    esnModuleRegistry.getAll = function() {
+      return modulesMock;
+    };
+  });
+
+  describe('The getHomePageCandidates fn', function() {
+    it('should return a list homePage', function() {
+      var expectResult = {a: 'A', b: 'B'};
+
+      expect(controlcenterGeneralService.getHomePageCandidates()).to.deep.equal(expectResult);
+    });
+  });
+});
