da2a6b6b31966ac4ef3d032d3fea1fef3a568d94
Dung Dung T PHAM <dtpham@linagora.com>
2017-03-28 16:48:34 +0700

    OR-2582 Create Registry factory in esn-config to store config metadata

--- backend/core/esn-config/index.js
+++ backend/core/esn-config/index.js
@@ -5,15 +5,18 @@ const EsnConfig = require('./esn-config');
 const constants = require('./constants');
 const fallbackModule = require('./fallback');
 const configurations = require('./configurations');
-
-function getConfigsForUser(user) {
-  return fallbackModule.getConfiguration(user.preferredDomainId, user._id);
-}
+const registry = require('./registry');
 
 module.exports = function(configName) {
   return new Adapter(configName);
 };
+
 module.exports.EsnConfig = EsnConfig;
 module.exports.constants = constants;
 module.exports.getConfigsForUser = getConfigsForUser;
 module.exports.configurations = configurations;
+module.exports.registry = registry;
+
+function getConfigsForUser(user) {
+  return fallbackModule.getConfiguration(user.preferredDomainId, user._id);
+}
