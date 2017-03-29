c8b1f73edeb3854bda20e8519b750b2a68836bd4
Christophe Hamerling <chamerling@linagora.com>
2017-03-29 15:26:49 +0200

    CAL-578 ES6ify websocket api

--- modules/linagora.esn.calendar/backend/ws/calendar.js
+++ modules/linagora.esn.calendar/backend/ws/calendar.js
@@ -1,17 +1,56 @@
 'use strict';
 
-var initialized = false;
-var NAMESPACE = '/calendars';
-var PUBSUB_EVENT = 'calendar:event:updated';
-var CONSTANTS = require('../lib/constants');
-var WS_EVENT = CONSTANTS.WS_EVENT;
-var NOTIFICATIONS = CONSTANTS.NOTIFICATIONS;
-var ICAL = require('ical.js');
-var _ = require('lodash');
+const NAMESPACE = '/calendars';
+const PUBSUB_EVENT = 'calendar:event:updated';
+const CONSTANTS = require('../lib/constants');
+const WS_EVENT = CONSTANTS.WS_EVENT;
+const NOTIFICATIONS = CONSTANTS.NOTIFICATIONS;
+const ICAL = require('ical.js');
+const _ = require('lodash');
+let initialized = false;
+
+module.exports = {
+  init
+};
+
+function init(dependencies) {
+  const logger = dependencies('logger');
+  const pubsub = dependencies('pubsub');
+  const io = dependencies('wsserver').io;
+  const ioHelper = dependencies('wsserver').ioHelper;
+
+  if (initialized) {
+    logger.warn('The calendar notification service is already initialized');
+
+    return;
+  }
+
+  pubsub.global.topic(PUBSUB_EVENT).subscribe(msg => {
+    pubsub.local.topic(PUBSUB_EVENT).publish(msg);
+    notify(io, ioHelper, msg.websocketEvent, msg);
+    emitElasticSearchEvent(pubsub, msg);
+  });
+
+  io.of(NAMESPACE)
+    .on('connection', socket => {
+      logger.info('New connection on ' + NAMESPACE);
+
+      socket.on('subscribe', uuid => {
+        logger.info('Joining room', uuid);
+        socket.join(uuid);
+      });
+
+      socket.on('unsubscribe', uuid => {
+        logger.info('Leaving room', uuid);
+        socket.leave(uuid);
+      });
+    });
+  initialized = true;
+}
 
 function parseEventPath(eventPath) {
   // The eventPath is in this form : /calendars/{{userId}}/calendarId/{{eventUid}}
-  var pathParts = eventPath.replace(/^\//, '').split('/');
+  const pathParts = eventPath.replace(/^\//, '').split('/');
 
   return {
     userId: pathParts[1],
@@ -22,31 +61,30 @@ function parseEventPath(eventPath) {
 
 function parseUserPrincipal(userPrincipal) {
   // The userPrincipal is in this form : principals/users/{{userId}}
-  var pathParts = userPrincipal.split('/');
+  const pathParts = userPrincipal.split('/');
 
   return pathParts[2];
 }
 
 function notify(io, ioHelper, event, msg) {
-  var userIds = [parseEventPath(msg.eventPath).userId];
+  const userIds = [parseEventPath(msg.eventPath).userId];
 
   if (msg.shareeIds) {
-    msg.shareeIds.forEach(function(shareePrincipals) {
-      userIds.push(parseUserPrincipal(shareePrincipals));
-    });
+    msg.shareeIds.forEach(shareePrincipals => userIds.push(parseUserPrincipal(shareePrincipals)));
   }
 
   delete msg.shareeIds;
 
-  userIds.forEach(function(userId) {
-    var clientSockets = ioHelper.getUserSocketsFromNamespace(userId, io.of(NAMESPACE).sockets) || [];
+  userIds.forEach(userId => {
+    const clientSockets = ioHelper.getUserSocketsFromNamespace(userId, io.of(NAMESPACE).sockets) || [];
+
     _.invokeMap(clientSockets, 'emit', event, msg);
   });
 }
 
 function emitElasticSearchEvent(pubsub, msg) {
-  var data = parseEventPath(msg.eventPath);
-  var action = msg.websocketEvent;
+  const data = parseEventPath(msg.eventPath);
+  const action = msg.websocketEvent;
 
   data.ics = (new ICAL.Component(msg.event)).toString();
 
@@ -60,40 +98,3 @@ function emitElasticSearchEvent(pubsub, msg) {
     throw new Error('Unknow ws_event for calendar CRUD', action);
   }
 }
-
-function init(dependencies) {
-  var logger = dependencies('logger');
-  var pubsub = dependencies('pubsub');
-  var io = dependencies('wsserver').io;
-  var ioHelper = dependencies('wsserver').ioHelper;
-
-  if (initialized) {
-    logger.warn('The calendar notification service is already initialized');
-
-    return;
-  }
-
-  pubsub.global.topic(PUBSUB_EVENT).subscribe(function(msg) {
-    pubsub.local.topic(PUBSUB_EVENT).publish(msg);
-    notify(io, ioHelper, msg.websocketEvent, msg);
-    emitElasticSearchEvent(pubsub, msg);
-  });
-
-  io.of(NAMESPACE)
-    .on('connection', function(socket) {
-      logger.info('New connection on ' + NAMESPACE);
-
-      socket.on('subscribe', function(uuid) {
-        logger.info('Joining room', uuid);
-        socket.join(uuid);
-      });
-
-      socket.on('unsubscribe', function(uuid) {
-        logger.info('Leaving room', uuid);
-        socket.leave(uuid);
-      });
-    });
-  initialized = true;
-}
-
-module.exports.init = init;
