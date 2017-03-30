fbb9112d53ec77448a1ccff3f3ac60a53ba19538
Michael Bailly <mbailly@linagora.com>
2017-03-30 12:10:09 +0200

    OR-2567 Add locals.assets for Jade access to assets

--- backend/webserver/middleware/setup-routes.js
+++ backend/webserver/middleware/setup-routes.js
@@ -2,11 +2,15 @@
 
 var core = require('../../core');
 
+const assetRegistry = core.assets;
+
 function setupRoute(app) {
   app.get('/', function(req, res, next) {
     if (core.configured()) {
       return next();
     }
+
+    res.locals.assets = assetRegistry.app('setup');
     res.render('setup/index');
   });
 }
