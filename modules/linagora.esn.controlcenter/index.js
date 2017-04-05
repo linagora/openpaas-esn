73e6003a629ee2a6bf2d6abc287821dab29a2bbd
Michael Bailly <mbailly@linagora.com>
2017-04-05 17:40:04 +0200

    OR-2567 Set full path of JavaScript files in other modules
    - linagora.esn.controlcenter
    - linagora.esn.signup.local
    - linagora.esn.signup
    - linagora.esn.sync
    - linagora.esn.user.status

--- modules/linagora.esn.controlcenter/index.js
+++ modules/linagora.esn.controlcenter/index.js
@@ -6,6 +6,13 @@ var path = require('path');
 
 var MODULE_NAME = 'controlcenter';
 var AWESOME_MODULE_NAME = 'linagora.esn.' + MODULE_NAME;
+const moduleFiles = [
+  'general/controlcenter-general.component.js',
+  'general/controlcenter-general.controller.js',
+  'general/controlcenter-general-subheader.component.js',
+  'general/controlcenter-general.service.js'
+];
+const FRONTEND_JS_PATH = `${__dirname}/frontend/app/`;
 
 var controlCenterModule = new AwesomeModule(AWESOME_MODULE_NAME, {
   dependencies: [
@@ -36,12 +43,9 @@ var controlCenterModule = new AwesomeModule(AWESOME_MODULE_NAME, {
         'controllers.js'
       ], [AWESOME_MODULE_NAME], ['esn']);
 
-      webserverWrapper.injectAngularAppModules(MODULE_NAME, [
-        'general/controlcenter-general.component.js',
-        'general/controlcenter-general.controller.js',
-        'general/controlcenter-general-subheader.component.js',
-        'general/controlcenter-general.service.js'
-      ], [AWESOME_MODULE_NAME], ['esn']);
+      webserverWrapper.injectAngularAppModules(MODULE_NAME, moduleFiles, [AWESOME_MODULE_NAME], ['esn'], {
+        localJsFiles: moduleFiles.map(file => path.join(FRONTEND_JS_PATH, file))
+      });
 
       webserverWrapper.injectLess(MODULE_NAME, [lessFile], 'esn');
       webserverWrapper.addApp(MODULE_NAME, app);
