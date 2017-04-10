4a74e6bea92dd45cf4624b90509362758bfc20e5
lamine.zaineb <lamine.zaineb@gmailcom>
2017-04-10 15:59:24 +0100

    CHAT-268: Display the members avatars at the beginning of the confidential conversation

---
+++ frontend/js/modules/avatar/list/avatar-list.component.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular.module('esn.avatar')
+    .component('esnAvatarList', esnAvatarList());
+
+  function esnAvatarList() {
+    return {
+      templateUrl: '/views/modules/avatar/list/avatar-list.html',
+      controller: 'ESNAvatarListController',
+      bindings: {
+        members: '<',
+        limit: '='
+      }
+    };
+  }
+})();
