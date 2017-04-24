e724e1e6d169cc52ef727e47e3c4c6015cb07058
Abdulkader BENCHI <abenchi@linagora.com>
2017-04-24 16:27:19 +0200

    CAL-579 use rights directly from the calendar object instead of doing a useless rest call

--- modules/linagora.esn.calendar/frontend/app/calendar-configuration/calendar-configuration.controller.js
+++ modules/linagora.esn.calendar/frontend/app/calendar-configuration/calendar-configuration.controller.js
@@ -26,7 +26,7 @@
     CalDelegationEditionHelper
   ) {
     var self = this;
-    var calendarRight, originalCalendarRight;
+    var calendarRightShell, originalCalendarRight;
     var CaldelegationEditionHelperInstance = new CalDelegationEditionHelper();
 
     self.submit = submit;
@@ -73,27 +73,25 @@
       self.selectedTab = 'main';
 
       if (self.newCalendar) {
-        calendarRight = $q.when(new CalendarRightShell());
+        calendarRightShell = new CalendarRightShell();
       } else {
-        calendarRight = calendarService.getRight(self.calendarHomeId, self.calendar);
+        calendarRightShell = self.calendar.rights;
       }
 
       angular.copy(self.calendar, self.oldCalendar);
       self.delegations = self.delegations || [];
       self.selectedShareeRight = CAL_CALENDAR_SHARED_RIGHT.NONE;
 
-      calendarRight.then(function(calendarRightShell) {
-        self.publicSelection = calendarRightShell.getPublicRight();
-        var allShareeRights = calendarRightShell.getAllShareeRights();
+      self.publicSelection = calendarRightShell.getPublicRight();
+      var allShareeRights = calendarRightShell.getAllShareeRights();
 
-        $q.all(_.chain(allShareeRights).map('userId').map(userAPI.user).values()).then(function(users) {
-          _.chain(users).map('data').zip(allShareeRights).forEach(function(array) {
-            var user = array[0];
-            var right = array[1].right;
+      $q.all(_.chain(allShareeRights).map('userId').map(userAPI.user).values()).then(function(users) {
+        _.chain(users).map('data').zip(allShareeRights).forEach(function(array) {
+          var user = array[0];
+          var right = array[1].right;
 
-            user.displayName = userUtils.displayNameOf(user);
-            self.delegations = CaldelegationEditionHelperInstance.addUserGroup([user], right);
-          });
+          user.displayName = userUtils.displayNameOf(user);
+          self.delegations = CaldelegationEditionHelperInstance.addUserGroup([user], right);
         });
       });
 
@@ -127,57 +125,55 @@
             $state.go('calendar.main');
           });
       } else {
-        calendarRight.then(function(calendarRightShell) {
-          originalCalendarRight = calendarRightShell.clone();
-          CaldelegationEditionHelperInstance.getAllRemovedUsersId().map(function(removedUserId) {
-            calendarRightShell.removeShareeRight(removedUserId);
-          });
+        originalCalendarRight = calendarRightShell.clone();
+        CaldelegationEditionHelperInstance.getAllRemovedUsersId().map(function(removedUserId) {
+          calendarRightShell.removeShareeRight(removedUserId);
+        });
 
-          self.delegations.forEach(function(line) {
-            calendarRightShell.updateSharee(line.user._id, line.user.preferredEmail, line.selection);
-          });
+        self.delegations.forEach(function(line) {
+          calendarRightShell.updateSharee(line.user._id, line.user.preferredEmail, line.selection);
+        });
 
-          var rightChanged = !calendarRightShell.equals(originalCalendarRight);
-          var calendarChanged = _hasModifications(self.oldCalendar, self.calendar);
-          var updateActions = [];
-          var publicRightChanged = self.publicSelection !== calendarRightShell.getPublicRight();
+        var rightChanged = !calendarRightShell.equals(originalCalendarRight);
+        var calendarChanged = _hasModifications(self.oldCalendar, self.calendar);
+        var updateActions = [];
+        var publicRightChanged = self.publicSelection !== calendarRightShell.getPublicRight();
 
-          if (!rightChanged && !calendarChanged && !publicRightChanged) {
-            if (matchmedia.is(SM_XS_MEDIA_QUERY)) {
-              $state.go('calendar.list');
-            } else {
-              $state.go('calendar.main');
-            }
-
-            return;
-          }
-
-          if (calendarChanged) {
-            updateActions.push(calendarService.modifyCalendar(self.calendarHomeId, calendarCollectionShell, calendarRightShell));
-          }
-
-          if (rightChanged) {
-            updateActions.push(calendarService.modifyRights(self.calendarHomeId, calendarCollectionShell, calendarRightShell, originalCalendarRight));
-          }
-
-          if (publicRightChanged) {
-            switch (self.publicSelection) {
-              case CAL_CALENDAR_PUBLIC_RIGHT.READ:
-                updateActions.push(calendarAPI.modifyPublicRights(self.calendarHomeId, self.calendar.id, { public_right: '{DAV:}read' }));
-                break;
-              case CAL_CALENDAR_PUBLIC_RIGHT.READ_WRITE:
-                updateActions.push(calendarAPI.modifyPublicRights(self.calendarHomeId, self.calendar.id, { public_right: '{DAV:}write' }));
-                break;
-              case CAL_CALENDAR_PUBLIC_RIGHT.FREE_BUSY:
-                updateActions.push(calendarAPI.modifyPublicRights(self.calendarHomeId, self.calendar.id, { public_right: '{urn:ietf:params:xml:ns:caldav}read-free-busy' }));
-                break;
-            }
-          }
-
-          $q.all(updateActions).then(function() {
-            notificationFactory.weakInfo('Calendar - ', self.calendar.name + ' has been modified.');
+        if (!rightChanged && !calendarChanged && !publicRightChanged) {
+          if (matchmedia.is(SM_XS_MEDIA_QUERY)) {
+            $state.go('calendar.list');
+          } else {
             $state.go('calendar.main');
-          });
+          }
+
+          return;
+        }
+
+        if (calendarChanged) {
+          updateActions.push(calendarService.modifyCalendar(self.calendarHomeId, calendarCollectionShell, calendarRightShell));
+        }
+
+        if (rightChanged) {
+          updateActions.push(calendarService.modifyRights(self.calendarHomeId, calendarCollectionShell, calendarRightShell, originalCalendarRight));
+        }
+
+        if (publicRightChanged) {
+          switch (self.publicSelection) {
+            case CAL_CALENDAR_PUBLIC_RIGHT.READ:
+              updateActions.push(calendarAPI.modifyPublicRights(self.calendarHomeId, self.calendar.id, { public_right: '{DAV:}read' }));
+              break;
+            case CAL_CALENDAR_PUBLIC_RIGHT.READ_WRITE:
+              updateActions.push(calendarAPI.modifyPublicRights(self.calendarHomeId, self.calendar.id, { public_right: '{DAV:}write' }));
+              break;
+            case CAL_CALENDAR_PUBLIC_RIGHT.FREE_BUSY:
+              updateActions.push(calendarAPI.modifyPublicRights(self.calendarHomeId, self.calendar.id, { public_right: '{urn:ietf:params:xml:ns:caldav}read-free-busy' }));
+              break;
+          }
+        }
+
+        $q.all(updateActions).then(function() {
+          notificationFactory.weakInfo('Calendar - ', self.calendar.name + ' has been modified.');
+          $state.go('calendar.main');
         });
       }
     }
