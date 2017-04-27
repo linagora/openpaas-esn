2a5998659c53c7399e543bacf6b4397d1dcd1b03
Christophe Hamerling <chamerling@linagora.com>
2017-04-27 16:10:52 +0200

    CAL-641 iife attendee components

--- modules/linagora.esn.calendar/frontend/app/components/attendee-list-item/attendee-list-item.component.js
+++ modules/linagora.esn.calendar/frontend/app/components/attendee-list-item/attendee-list-item.component.js
@@ -1,12 +1,14 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('esn.calendar')
-  .component('calAttendeeListItem', {
-    templateUrl: '/calendar/app/components/attendee-list-item/attendee-list-item.html',
-    bindings: {
-      attendee: '=',
-      canModifyAttendee: '<',
-      isOrganizer: '<'
-    },
-    controllerAs: 'ctrl'
-  });
+  angular.module('esn.calendar')
+    .component('calAttendeeListItem', {
+      templateUrl: '/calendar/app/components/attendee-list-item/attendee-list-item.html',
+      bindings: {
+        attendee: '=',
+        canModifyAttendee: '<',
+        isOrganizer: '<'
+      },
+      controllerAs: 'ctrl'
+    });
+})();
