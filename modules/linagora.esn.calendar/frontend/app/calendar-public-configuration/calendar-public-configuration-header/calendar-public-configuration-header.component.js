660998d731c28b1b421597a42da557130a7b0d7a
Christophe Hamerling <chamerling@linagora.com>
2017-05-18 18:21:46 +0200

    CAL-585 Remove useless header

--- modules/linagora.esn.calendar/frontend/app/calendar-public-configuration/calendar-public-configuration-header/calendar-public-configuration-header.component.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-
-  angular.module('esn.calendar')
-    .component('calendarPublicConfigurationHeader', {
-        templateUrl: '/calendar/app/calendar-public-configuration/calendar-public-configuration-header/calendar-public-configuration-header.html'
-      }
-    );
-})();
