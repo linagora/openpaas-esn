8ee991ba2e5cfabd8b8b18320c4e4026da528f48
Luc DUZAN <lduzan@linagora.com>
2017-06-14 17:55:06 +0200

    Fix up various awesome bugs

--- frontend/js/modules/esn.offline.detector.js
+++ frontend/js/modules/esn.offline.detector.js
@@ -17,11 +17,26 @@
    * @desc Service to detect offline/online event
    * @memberOf esn.offline.detector
    */
-  function offlineDetectorApi($rootScope, ioSocketConnection) {
+  function offlineDetectorApi($rootScope, $window, ioSocketConnection) {
     var service = {
       activate: activate,
       online: ioSocketConnection.isConnected()
     };
+
+    $window.luc = $window.setForceOffline = function() {
+      service.forceOffline = true;
+      service.online = false;
+
+      return 'May the force be with you';
+    };
+
+    $window.han = $window.unsetForceOffline = function() {
+      service.forceOffline = false;
+      setNetworkActivity(ioSocketConnection.isConnected());
+
+      return 'Han shot first!';
+    };
+
     return service;
 
     ////////////
@@ -43,8 +58,13 @@
      * @memberOf esn.offline.detector.offlineDetectorApi
      */
     function setNetworkActivity(connected) {
+      if (service.forceOffline) {
+        return;
+      }
+
       service.online = connected;
       $rootScope.$broadcast('network:available', service.online);
     }
+
   }
 })();
