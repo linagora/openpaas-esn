0dd6aa0b958ca64d6ca6c1b3d39e101308dc5495
Michael Bailly <mbailly@linagora.com>
2017-06-14 12:10:14 +0200

    OR-2673 Use httpConfigurer for graceperiod Restangular

--- modules/linagora.esn.graceperiod/frontend/js/grace-period-restangular.service.js
+++ modules/linagora.esn.graceperiod/frontend/js/grace-period-restangular.service.js
@@ -3,10 +3,13 @@
 
   angular.module('linagora.esn.graceperiod')
 
-    .factory('gracePeriodRestangularService', function(Restangular) {
-      return Restangular.withConfig(function(RestangularConfigurer) {
-        RestangularConfigurer.setBaseUrl('/graceperiod/api');
+    .factory('gracePeriodRestangularService', function(Restangular, httpConfigurer) {
+      var restangularInstance = Restangular.withConfig(function(RestangularConfigurer) {
         RestangularConfigurer.setFullResponse(true);
       });
+
+      httpConfigurer.manageRestangular(restangularInstance, '/graceperiod/api');
+
+      return restangularInstance;
     });
 })();
