bd1a93ada326d6da9cbf1c6b4497eada467807d1
tthpham <tthpham@linagora.com>
2017-07-10 14:53:25 +0700

    Attachment image and video viewer

--- frontend/js/modules/attachment/attachment.component.js
+++ frontend/js/modules/attachment/attachment.component.js
@@ -1,17 +1,35 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('esn.attachment')
-    .component('esnAttachment', esnAttachment());
+    angular.module('esn.attachment')
+        .component('esnAttachment', esnAttachment())
+        .directive('esnAttachmentViewerVideo', function() {
+            return {
+                scope: {
+                    file: '='
+                },
+                controller: ['$scope',function($scope) {
+                    $scope.videoUrl = '/api/files/'+$scope.file._id;
+                }],
+                link: function(scope, element, attrs){
+                    var modal = element.find('.modal');
+                    var video = element.find('video');
+                    modal.on('hidden.bs.modal', function () {
+                       video[0].pause();
+                    });
+                },
+                templateUrl: '/views/modules/attachment/attachment-viewer-video.html'
+            };
+        });
 
-  function esnAttachment() {
-    return {
-      bindings: {
-        attachment: '='
-      },
-      controllerAs: 'ctrl',
-      templateUrl: '/views/modules/attachment/attachment.html'
-    };
-  }
+    function esnAttachment() {
+        return {
+            bindings: {
+                attachment: '='
+            },
+            controllerAs: 'ctrl',
+            templateUrl: '/views/modules/attachment/attachment.html'
+        };
+    }
 
 })();
--- frontend/js/modules/file-preview/image.js
+++ frontend/js/modules/file-preview/image.js
@@ -2,21 +2,55 @@
 
 (function() {
 
-  var provider = {
-    name: 'image',
-    contentType: ['image/png', 'image/x-png', 'image/jpeg', 'image/pjpeg', 'image/gif']
-  };
-  angular.module('esn.file-preview.image', ['esn.file-preview'])
-    .run(function(filePreviewService) {
-      filePreviewService.addFilePreviewProvider(provider);
-    })
-    .directive('filePreviewImage', function() {
-      return {
-        restrict: 'E',
-        scope: {
-            file: '='
-          },
-        templateUrl: '/views/modules/file-preview/file-preview-image.html'
-      };
-    });
+    var provider = {
+        name: 'image',
+        contentType: ['image/png', 'image/x-png', 'image/jpeg', 'image/pjpeg', 'image/gif']
+    };
+    angular.module('esn.file-preview.image', ['esn.file-preview'])
+        .run(function(filePreviewService) {
+            filePreviewService.addFilePreviewProvider(provider);
+        })
+        .directive('filePreviewImage', function() {
+            return {
+                restrict: 'E',
+                scope: {
+                    file: '='
+                },
+                templateUrl: '/views/modules/file-preview/file-preview-image.html'
+            };
+        })
+        .directive('imageViewer', function() {
+            return {
+                restrict: 'E',
+                scope: {
+                    file: '=targetFile'
+                },
+                controller: ['$scope', '$window', function($scope, $window) {
+                    $scope.image = {
+                        path: "",
+                        width: 0,
+                        height: 0
+                    }
+                    var img = new Image();
+                    img.onload = function() {
+                        $scope.$apply(function() {
+                            $scope.image.width = img.width;
+                            $scope.image.height = img.height;
+                            $scope.image.path = '/api/files/'+$scope.file._id;
+                            if ($scope.image.width > 800) {
+                                $scope.image.width = 0.7 * $(window).width();
+                            }
+                            $scope.modalDialogStyle = {
+                                'width': $scope.image.width
+                            };
+                            $scope.imgStyle = {
+                                'max-height':'100%'
+                            };
+                        });
+                    }
+                    img.src = '/api/files/'+$scope.file._id;
+                }],
+                templateUrl: '/views/modules/file-preview/image-viewer.html'
+            };
+        });
 })();
