11d1c7ea4b316cfe0a9c80a0f718dd4e9bc1d50e
Michael Bailly <mbailly@linagora.com>
2017-07-12 16:58:08 +0200

    OR-2686 linagora.esn.sync module

--- modules/linagora.esn.sync/index.js
+++ modules/linagora.esn.sync/index.js
@@ -4,15 +4,29 @@ const path = require('path'),
       AwesomeModule = require('awesome-module');
 const Dependency = AwesomeModule.AwesomeModuleDependency;
 
-const moduleFiles = [
+const FRONTEND_JS_PATH = `${__dirname}/frontend/app/`;
+const innerApps = ['esn'];
+const angularAppModuleFiles = [
   'app.js',
   'components/main/main.js',
   'components/android/android.js',
   'components/controlcenter-menu-entry/controlcenter-menu-entry.js'
 ];
-const FRONTEND_JS_PATH = `${__dirname}/frontend/app/`;
+const modulesOptions = {
+  localJsFiles: angularAppModuleFiles.map(file => path.resolve(FRONTEND_JS_PATH, file))
+};
 
-module.exports = new AwesomeModule('linagora.esn.sync', {
+const moduleData = {
+  shortName: 'sync',
+  fullName: 'linagora.esn.sync',
+  lessFiles: [],
+  angularAppModules: []
+};
+
+moduleData.lessFiles.push([moduleData.shortName, [path.resolve(FRONTEND_JS_PATH, 'styles.less')], innerApps]);
+moduleData.angularAppModules.push([moduleData.shortName, angularAppModuleFiles, moduleData.fullName, innerApps, modulesOptions]);
+
+module.exports = new AwesomeModule(moduleData.fullName, {
   dependencies: [
     new Dependency(Dependency.TYPE_NAME, 'linagora.esn.core.logger', 'logger'),
     new Dependency(Dependency.TYPE_NAME, 'linagora.esn.core.esn-config', 'esn-config'),
@@ -20,6 +34,7 @@ module.exports = new AwesomeModule('linagora.esn.sync', {
     new Dependency(Dependency.TYPE_NAME, 'linagora.esn.core.webserver.wrapper', 'webserver-wrapper'),
     new Dependency(Dependency.TYPE_NAME, 'linagora.esn.core.webserver.middleware.authorization', 'authorizationMW')
   ],
+  data: moduleData,
   states: {
     lib: (dependencies, callback) => callback(),
 
@@ -27,11 +42,9 @@ module.exports = new AwesomeModule('linagora.esn.sync', {
       const webserverWrapper = dependencies('webserver-wrapper'),
             app = require('./backend/webserver/application')(dependencies);
 
-      webserverWrapper.injectLess('sync', [path.resolve(__dirname, 'frontend/app/styles.less')], 'esn');
-      webserverWrapper.injectAngularAppModules('sync', moduleFiles, ['linagora.esn.sync'], ['esn'], {
-        localJsFiles: moduleFiles.map(file => path.join(FRONTEND_JS_PATH, file))
-      });
-      webserverWrapper.addApp('sync', app);
+      moduleData.angularAppModules.forEach(mod => webserverWrapper.injectAngularAppModules.apply(webserverWrapper, mod));
+      moduleData.lessFiles.forEach(lessSet => webserverWrapper.injectLess.apply(webserverWrapper, lessSet));
+      webserverWrapper.addApp(moduleData.shortName, app);
 
       return callback();
     },
