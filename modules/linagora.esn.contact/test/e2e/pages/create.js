bc2e0cff34d7d5e803aae2723f5f073e9fa5bd7f
sghanmi <sghanmi@linagora.com>
2017-07-13 15:22:11 +0100

    OR-2688 fix locators for button Create Contact and text Contact Name

--- modules/linagora.esn.contact/test/e2e/pages/create.js
+++ modules/linagora.esn.contact/test/e2e/pages/create.js
@@ -5,5 +5,5 @@ module.exports = function() {
   this.contactCreateHeader = element(by.css('.contact-header'));
   this.firstname = this.contactCreateForm.element(by.model('contact.firstName'));
   this.lastname = this.contactCreateForm.element(by.model('contact.lastName'));
-  this.createButton = this.contactCreateHeader.element(by.partialButtonText('Create'));
+  this.createButton = this.contactCreateHeader.element(by.css('[ng-click="accept()"]'));
 };
