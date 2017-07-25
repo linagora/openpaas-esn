985cff84292aadc1a0c8165624099f9ad1cd7532
sonnd <sonnd@linagora.com>
2017-07-25 18:01:42 +0700

    OR-2679 Swagger for graceperiod internal module

--- modules/linagora.esn.graceperiod/backend/webserver/api/router.js
+++ modules/linagora.esn.graceperiod/backend/webserver/api/router.js
@@ -10,7 +10,50 @@ module.exports = function(lib, dependencies) {
   var controller = require('./controller')(lib, dependencies);
   var middleware = require('./middleware')(lib, dependencies);
 
+  /**
+   * @swagger
+   * /tasks/{id}:
+   *   delete:
+   *     tags:
+   *       - GracePeriod
+   *     description: Aborts a task which has been added to the grace period component.
+   *     parameters:
+   *       - $ref: "#/parameters/gp_id"
+   *     responses:
+   *       204:
+   *         $ref: "#/responses/cm_204"
+   *       401:
+   *         $ref: "#/responses/cm_401"
+   *       403:
+   *         $ref: "#/responses/cm_403"
+   *       404:
+   *         $ref: "#/responses/cm_404"
+   *       500:
+   *         $ref: "#/responses/cm_500"
+   */
   router.delete('/tasks/:id', authorizationMW.requiresAPILogin, middleware.load, middleware.isUserTask, controller.cancel);
+
+  /**
+   * @swagger
+   * /tasks/{id}:
+   *   put:
+   *     tags:
+   *       - GracePeriod
+   *     description: Flushs a pending task. Its job will be directly processed.
+   *     parameters:
+   *       - $ref: "#/parameters/gp_id"
+   *     responses:
+   *       204:
+   *         $ref: "#/responses/cm_204"
+   *       401:
+   *         $ref: "#/responses/cm_401"
+   *       403:
+   *         $ref: "#/responses/cm_403"
+   *       404:
+   *         $ref: "#/responses/cm_404"
+   *       500:
+   *         $ref: "#/responses/cm_500"
+   */
   router.put('/tasks/:id', authorizationMW.requiresAPILogin, middleware.load, middleware.isUserTask, controller.flush);
 
   return router;
---
+++ modules/linagora.esn.graceperiod/doc/swagger/parameters/graceperiod.js
@@ -0,0 +1,10 @@
+/**
+  * @swagger
+  * parameter:
+  *   gp_id:
+  *     name: id
+  *     in: path
+  *     description: The task ID
+  *     required: true
+  *     type: string
+  */
