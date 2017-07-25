9cdd021bc18f238c6572d5d4248225324b1f671f
sonnd <sonnd@linagora.com>
2017-07-25 17:58:57 +0700

    OR-2679 Swagger for contact internal module

---
+++ modules/linagora.esn.contact/doc/swagger/parameters/contact.js
@@ -0,0 +1,37 @@
+/**
+  * @swagger
+  * parameter:
+  *   contact_address_book_id:
+  *     name: addressBookId
+  *     in: path
+  *     description: The address book ID of user.
+  *     required: true
+  *     schema:
+  *       $ref: "#/definitions/cm_id"
+  *   contact_address_book_name:
+  *     name: addressbookName
+  *     in: path
+  *     description: The address book name of user.
+  *     required: true
+  *     type: string
+  *   contact_id:
+  *     name: contactId
+  *     in: path
+  *     description: The contact ID, color of letter avatar will be based on this ID.
+  *     required: true
+  *     schema:
+  *       $ref: "#/definitions/cm_id"
+  *   contact_size:
+  *     name: size
+  *     in: query
+  *     description: Size of avatar in pixel, default value is 256.
+  *     required: true
+  *     type: string
+  *   contact_user_id:
+  *     name: userId
+  *     in: query
+  *     description: ID of user
+  *     required: true
+  *     schema:
+  *       $ref: "#/definitions/cm_id"
+  */
