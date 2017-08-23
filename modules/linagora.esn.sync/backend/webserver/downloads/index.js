1eb44aef1380ad78442a0b5b055e2dad0628dfee
dsnguyen <dsnguyen@linagora.com>
2017-08-23 16:25:09 +0700

    OR-2679 swagger for sync module in internal modules

--- modules/linagora.esn.sync/backend/webserver/downloads/index.js
+++ modules/linagora.esn.sync/backend/webserver/downloads/index.js
@@ -5,6 +5,21 @@ module.exports = dependencies => {
         controller = require('./controller')(dependencies),
         router = require('express').Router();
 
+  /**
+   * @swagger
+   * /sync/downloads/thunderbird/op-tb-autoconf.xpi:
+   *   get:
+   *     tags:
+   *       - Sync
+   *     description: Downloads ThunderBird extension autoconfig file.
+   *     responses:
+   *       200:
+   *         $ref: "#/responses/cm_200"
+   *       401:
+   *         $ref: "#/responses/cm_401"
+   *       500:
+   *         $ref: "#/responses/cm_500"
+   */
   router.get('/downloads/thunderbird/op-tb-autoconf.xpi', auth.requiresAPILogin, controller.downloadTBExtension);
 
   return router;
