efb72404b30117a4743092b08b0ce33a077eb039
Tuan CONG LE <tlcong@linagora.com>
2017-09-01 10:51:49 +0700

    linagora/lgs/openpaas/linagora.esn.admin#222 read features flipping from features configuration

--- frontend/js/modules/config/config.service.js
+++ frontend/js/modules/config/config.service.js
@@ -2,12 +2,27 @@
 
 angular.module('esn.configuration')
 
-  .factory('esnConfig', function(session, featureFlags) {
+  .factory('esnConfig', function(session, featureFlags, _) {
     var sessionReady = session.ready.then(function() {
       var configurations = session.user.configurations;
+      var featuresConfigName = 'features';
 
       if (configurations) {
         featureFlags.set(configurations.modules.reduce(function(list, module) {
+          var features = _.find(module.configurations, { name: featuresConfigName });
+
+          // flatten the features configuration
+          if (features && features.value) {
+            _.forEach(features.value, function(value, key) {
+              var flattenFeature = {
+                name: featuresConfigName + '.' + key,
+                value: value
+              };
+
+              module.configurations.push(flattenFeature);
+            });
+          }
+
           return list.concat(module.configurations.map(function(config) {
             return {
               key: module.name + '.' + config.name,
