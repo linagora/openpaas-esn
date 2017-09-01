849bcfbbfa261c9dbc8eb9c3641a42fb713aeb5f
Tuan CONG LE <tlcong@linagora.com>
2017-09-01 10:52:41 +0700

    linagora.esn.admin#222 feature flip invitation page in control-center

--- modules/linagora.esn.controlcenter/frontend/app/common/menu-template-builder.service.spec.js
+++ modules/linagora.esn.controlcenter/frontend/app/common/menu-template-builder.service.spec.js
@@ -6,15 +6,37 @@ var expect = chai.expect;
 
 describe('The controlCenterMenuTemplateBuilder service', function() {
 
-  var controlCenterMenuTemplateBuilder;
+  var controlCenterMenuTemplateBuilder, featureFlags;
 
   beforeEach(function() {
     module('linagora.esn.controlcenter');
+
+    inject(function(_featureFlags_) {
+      featureFlags = _featureFlags_;
+    });
   });
 
   beforeEach(inject(function(_controlCenterMenuTemplateBuilder_) {
     controlCenterMenuTemplateBuilder = _controlCenterMenuTemplateBuilder_;
   }));
+
+  it('should not build the menu if flag is turnned off', function() {
+    featureFlags.isOn = function() {
+      return false;
+    };
+
+    expect(controlCenterMenuTemplateBuilder('/myHref', 'myIcon', 'myLabel', 'myFlag')).to.equal('');
+  });
+
+  it('should build the menu if flag is turned on', function() {
+    featureFlags.isOn = function() {
+      return true;
+    };
+
+    var expectedOutput = '<controlcenter-sidebar-menu-item icon="myIcon" href="/myHref" label="myLabel" />';
+
+    expect(controlCenterMenuTemplateBuilder('/myHref', 'myIcon', 'myLabel', 'myFlag')).to.equal(expectedOutput);
+  });
 
   it('should build the menu template with href', function() {
     var expectedOutput = '<controlcenter-sidebar-menu-item icon="myIcon" href="/myHref" label="myLabel" />';
