bd33ccb32cbecf2fb9610f50b7d0137bef729c86
Michael Bailly <mbailly@linagora.com>
2017-10-09 17:05:48 +0200

    #2224 Increase logger initialization logs

--- backend/core/logger/index.js
+++ backend/core/logger/index.js
@@ -19,9 +19,11 @@ if (config.log.console.enabled) {
   logger.add(Winston.transports.Console, config.log.console);
 }
 if (config.log.file.enabled) {
+  logger.info('Logger: registering file logger');
   logger.add(Winston.transports.File, config.log.file);
 }
 if (config.log.rotate && config.log.rotate.enabled) {
+  logger.info('Logger: registering rotate logger');
   logger.add(Winston.transports.Rotate, config.log.rotate);
 }
 
