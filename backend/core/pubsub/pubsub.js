822fb2fb8624492b2d57a37c6a419a1d358b923e
Christophe Hamerling <chamerling@linagora.com>
2017-10-16 14:20:04 +0200

    linagora/lgs/openpaas/linagora.esn.calendar#924 Pubsub as ES2015 class

--- backend/core/pubsub/pubsub.js
+++ backend/core/pubsub/pubsub.js
@@ -1,80 +1,71 @@
-'use strict';
+const { logger } = require('../../core');
 
-var logger = require('../../core').logger;
+class Pubsub {
+  constructor(name, client) {
+    this.name = name;
+    this.client = client;
+    this._channels = {};
+    this._cache = [];
+  }
 
-function Pubsub(name, client) {
-  this.name = name;
-  this.client = client;
-  this._channels = {};
-  this._cache = [];
+  setClient(client) {
+    if (this.client) {
+      logger.warn('pubsub client already set');
+
+      return;
+    }
+    this.client = client;
+    this._cache.forEach(elem => this.topic(elem.topic)[elem.action](elem.data));
+    delete this._cache;
+  }
+
+  _addCache(topic, action, data) {
+    this._cache.push({ topic, action, data });
+  }
+
+  _createInterface(topic) {
+    return {
+      subscribe: handler => {
+        if (!this.client) {
+          return this._addCache(topic, 'subscribe', handler);
+        }
+        logger.debug(this.name + '/SUBSCRIBE to', topic);
+
+        return this.client.on(topic, handler);
+      },
+      unsubscribe: handler => {
+        if (!this.client) {
+          return this._addCache(topic, 'unsubscribe', handler);
+        }
+        logger.debug(this.name + '/UNSUBSCRIBE to', topic);
+
+        return this.client.removeListener(topic, handler);
+      },
+      publish: data => {
+        if (!this.client) {
+          return this._addCache(topic, 'publish', data);
+        }
+
+        return this.client.emit(topic, data);
+      },
+      forward: (pubsub, data) => {
+        if (pubsub instanceof Pubsub) {
+          this._channels[topic].publish(data);
+
+          return pubsub.topic(topic).publish(data);
+        }
+        throw new Error('Invalid pubsub to forward to');
+      }
+    };
+  }
+
+  topic(name) {
+    if (!(name in this._channels)) {
+      this._channels[name] = this._createInterface(name);
+    }
+
+    return this._channels[name];
+  }
 }
-
-Pubsub.prototype.setClient = function(client) {
-  if (this.client) {
-    logger.warn('pubsub client already set');
-    return;
-  }
-  this.client = client;
-  var self = this;
-  this._cache.forEach(function(elem) {
-    self.topic(elem.topic)[elem.action](elem.data);
-  });
-  delete this._cache;
-};
-
-Pubsub.prototype._addCache = function(topic, action, data) {
-  this._cache.push(
-    {
-      topic: topic,
-      action: action,
-      data: data
-    }
-  );
-};
-
-Pubsub.prototype._createInterface = function(topic) {
-  const self = this;
-
-  return {
-    subscribe: function(handler) {
-      if (!self.client) {
-        return self._addCache(topic, 'subscribe', handler);
-      }
-      logger.debug(self.name + '/SUBSCRIBE to', topic);
-
-      return self.client.on(topic, handler);
-    },
-    unsubscribe: function(handler) {
-      if (!self.client) {
-        return self._addCache(topic, 'unsubscribe', handler);
-      }
-      logger.debug(self.name + '/UNSUBSCRIBE to', topic);
-
-      return self.client.removeListener(topic, handler);
-    },
-    publish: function(data) {
-      if (!self.client) {
-        return self._addCache(topic, 'publish', data);
-      }
-
-      return self.client.emit(topic, data);
-    },
-    forward: function(pubsub, data) {
-      if (pubsub instanceof Pubsub) {
-        self._channels[topic].publish(data);
-
-        return pubsub.topic(topic).publish(data);
-      }
-      throw new Error('Invalid pubsub to forward to');
-    }
-  };
-};
-
-Pubsub.prototype.topic = function(name) {
-  if (!(name in this._channels)) {
-    this._channels[name] = this._createInterface(name);
-  }
-  return this._channels[name];
-};
 
 module.exports = Pubsub;
