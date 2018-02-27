8aa171dbdc1bfbc57cfa6d9dd79d927d8e063529
Dung T Pham <dtpham@linagora.com>
2018-02-27 17:08:05 +0700

    linagora/lgs/lgs-projects/XXXXX_MCC_OpenPaaS_Experimentation#146 Load Angular App files that using new john papa convention

--- modules/linagora.esn.contact/backend/webserver/config/views.js
+++ modules/linagora.esn.contact/backend/webserver/config/views.js
@@ -1,14 +1,13 @@
 'use strict';
 
-var express = require('express');
-var FRONTEND_PATH = require('../constants').FRONTEND_PATH;
+const express = require('express');
+const { FRONTEND_PATH, CORE_FRONTEND_PATH } = require('../constants');
 
-module.exports = function(dependencies, application) {
+module.exports = (dependencies, application) => {
   application.use(express.static(FRONTEND_PATH));
-  application.set('views', FRONTEND_PATH + '/views');
-  application.get('/views/*', function(req, res) {
-      var templateName = req.params[0].replace(/\.html$/, '');
-      res.render(templateName);
-    }
-  );
+  application.set('views', [FRONTEND_PATH + '/views', FRONTEND_PATH + '/app']);
+  application.get([
+    '/views/*',
+    '/app/*'
+  ], (req, res) => res.render(req.params[0].replace(/\.html$/, ''), { basedir: CORE_FRONTEND_PATH + '/views' }));
 };
