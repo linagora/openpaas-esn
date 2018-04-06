ada966744c88e16cbe8de8da1edcbde338799149
Sang NGO DOAN <sngodoan@linagora.com>
2018-04-06 18:20:45 +0700

    linagora.esn.contact#358 support fallback for attribute type

--- modules/linagora.esn.contact/frontend/app/contact/shell/shell.service.js
+++ modules/linagora.esn.contact/frontend/app/contact/shell/shell.service.js
@@ -4,41 +4,11 @@
   angular.module('linagora.esn.contact')
     .factory('ContactShell', ContactShellFactory);
 
-  function ContactShellFactory() {
+  function ContactShellFactory(
+    contactVcardHelper,
+    CONTACT_ATTRIBUTES_ORDER
+  ) {
     function ContactShell(vcard, etag) {
-      function getMultiValue(propName) {
-        var props = vcard.getAllProperties(propName);
-
-        return props.map(function(prop) {
-          var data = {
-            value: prop.getFirstValue()
-          };
-          var type = prop.getFirstParameter('type');
-
-          if (type) {
-            data.type = type;
-          }
-
-          return data;
-        });
-      }
-
-      function getMultiAddress(propName) {
-        var props = vcard.getAllProperties(propName);
-
-        return props.map(function(prop) {
-          var propVal = prop.getFirstValue();
-
-          return {
-            type: prop.getFirstParameter('type'),
-            street: propVal[2],
-            city: propVal[3],
-            zip: propVal[5],
-            country: propVal[6]
-          };
-        });
-      }
-
       this.id = vcard.getFirstPropertyValue('uid');
       this.displayName = vcard.getFirstPropertyValue('fn');
 
@@ -51,21 +21,21 @@
       this.orgName = this.org ? this.org[0] : '';
       this.orgRole = vcard.getFirstPropertyValue('role');
 
-      this.emails = getMultiValue('email').map(function(mail) {
+      this.emails = contactVcardHelper.getMultiValue(vcard, 'email', CONTACT_ATTRIBUTES_ORDER.email).map(function(mail) {
         mail.value = mail.value.replace(/^mailto:/i, '');
 
         return mail;
       });
 
-      this.tel = getMultiValue('tel').map(function(tel) {
+      this.tel = contactVcardHelper.getMultiValue(vcard, 'tel', CONTACT_ATTRIBUTES_ORDER.phone).map(function(tel) {
         tel.value = tel.value.replace(/^tel:/i, '');
 
         return tel;
       });
 
-      this.addresses = getMultiAddress('adr');
-      this.social = getMultiValue('socialprofile');
-      this.urls = getMultiValue('url');
+      this.addresses = contactVcardHelper.getMultiAddress(vcard, 'adr', CONTACT_ATTRIBUTES_ORDER.address);
+      this.social = contactVcardHelper.getMultiValue(vcard, 'socialprofile', CONTACT_ATTRIBUTES_ORDER.social);
+      this.urls = contactVcardHelper.getMultiValue(vcard, 'url');
 
       var catprop = vcard.getFirstProperty('categories');
       var cats = catprop && catprop.getValues().concat([]);
