2365afd686159dd0bff73c1de6b0ea3264fc6fa6
Sang NGO DOAN <sngodoan@linagora.com>
2018-05-29 14:56:08 +0700

    linagora.esn.contact#350 workaround to fix issue cannot update contact 2 times sequentially
    When the user clicks Save to update contact, the contact is not really
    saved to database because of graceperiod timeout (usually 8s). To
    display updated contact to user, we use `contactUpdateDataService` to
    cache contact after it is saved from contact edition page (see controller.js line 284)
    
    Although the information of cached contact is up to date, the ETag of it
    is outdated, so when the user tries to save contact again, it is failed
    because ETag is not matched.
    
    To workaround this, we listen to CONTACT_EVENTS.UPDATED to update the
    ETag, we use it in 2 places:
    
    - In `editContactController`: for the case user edits contact before
    graceperiod ends
    - In `contactUpdateDataService`: (in this PR) for the case user edit after
    graceperiod ends

--- modules/linagora.esn.contact/frontend/app/services/contact-update-data.service.js
+++ modules/linagora.esn.contact/frontend/app/services/contact-update-data.service.js
@@ -4,11 +4,21 @@
   angular.module('linagora.esn.contact')
     .factory('contactUpdateDataService', contactUpdateDataService);
 
-  function contactUpdateDataService() {
-    return {
+  function contactUpdateDataService($rootScope, CONTACT_EVENTS) {
+    var state = {
       taskId: null,
       contact: null,
       contactUpdatedIds: []
     };
+
+    // workaround to fix issue that cannot update contact 2 times sequentially
+    // TODO: rework on contactUpdateDataService to avoid dirty hack
+    $rootScope.$on(CONTACT_EVENTS.UPDATED, function(e, data) {
+      if (state.contact && state.contact.id === data.id && data.etag) {
+        state.contact.etag = data.etag;
+      }
+    });
+
+    return state;
   }
 })(angular);
