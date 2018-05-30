711dcd4d2f42b2bff94a69951f838b4aaa497d79
Christophe Hamerling <chamerling@linagora.com>
2018-05-30 10:31:05 +0200

    #2417 Add the long date format

--- frontend/js/modules/i18n/i18n-dateformat.service.js
+++ frontend/js/modules/i18n/i18n-dateformat.service.js
@@ -4,13 +4,22 @@
   angular.module('esn.i18n')
   .factory('esnI18nDateFormatService', esnI18nDateFormatService);
 
-  function esnI18nDateFormatService(esnI18nService, ESN_I18N_DATE_FORMAT, ESN_I18N_DEFAULT_LOCALE) {
+  function esnI18nDateFormatService(esnI18nService, ESN_I18N_DATE_FORMAT, ESN_I18N_LONG_DATE_FORMAT, ESN_I18N_DEFAULT_LOCALE) {
     return {
-      getDateFormat: getDateFormat
+      getDateFormat: getDateFormat,
+      getLongDateFormat: getLongDateFormat
     };
 
     function getDateFormat() {
-      return ESN_I18N_DATE_FORMAT[esnI18nService.getLocale().substring(0, 2)] || ESN_I18N_DATE_FORMAT[ESN_I18N_DEFAULT_LOCALE];
+      return get(ESN_I18N_DATE_FORMAT);
+    }
+
+    function getLongDateFormat() {
+      return get(ESN_I18N_LONG_DATE_FORMAT);
+    }
+
+    function get(format) {
+      return format[esnI18nService.getLocale().substring(0, 2)] || format[ESN_I18N_DEFAULT_LOCALE];
     }
   }
 })();
--- frontend/js/modules/i18n/i18n.constants.js
+++ frontend/js/modules/i18n/i18n.constants.js
@@ -14,5 +14,9 @@
       fr: 'dd/MM/yyyy',
       vi: 'dd/MM/yyyy'
     })
-    ;
+    .constant('ESN_I18N_LONG_DATE_FORMAT', {
+      en: 'EEE yyyy/MM/dd',
+      fr: 'EEE dd/MM/yyyy',
+      vi: 'EEE dd/MM/yyyy'
+    });
 })();
