a3987c5e0b1445dc6b84d9e9eaa93d2c8b4ff036
fmoyon <fmoyon@linagora.com>
2018-08-27 14:37:08 +0200

    Fix #2469 Profile link is injected in profile-menu

---
+++ modules/linagora.esn.profile/frontend/app/app.run.js
@@ -0,0 +1,12 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('linagora.esn.profile')
+
+    .run(function(dynamicDirectiveService, DynamicDirective) {
+      var directive = new DynamicDirective(true, 'profile-menu-profile');
+
+      dynamicDirectiveService.addInjection('profile-menu-link', directive);
+    });
+
+})(angular);
---
+++ modules/linagora.esn.profile/frontend/app/profile-menu-profile/profile-menu-profile.component.js
@@ -0,0 +1,8 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('linagora.esn.profile')
+    .component('profileMenuProfile', {
+      templateUrl: '/profile/app/profile-menu-profile/profile-menu-profile.html'
+    });
+})(angular);
