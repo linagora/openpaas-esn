5fcd11524b58ed780b48a67325d5a7b437004f36
Khac Chien LANG <kclang@linagora.com>
2018-09-20 16:25:02 +0700

    linagora/lgs/openpaas/linagora.esn.admin#472 core esn-config supports time zone in datetime configuration

--- backend/core/esn-config/metadata/datetime.js
+++ backend/core/esn-config/metadata/datetime.js
@@ -1,15 +1,20 @@
-const { createValidator } = require('../validator/helper');
+const momentTimeZone = require('moment-timezone');
+const { buildErrorMessage, createValidateFunction } = require('../validator/helper');
 
 const schema = {
   type: 'object',
   properties: {
     use24hourFormat: {
       type: 'boolean'
+    },
+    timeZone: {
+      type: 'string'
     }
   },
   required: ['use24hourFormat'],
   additionalProperties: false
 };
+const validate = createValidateFunction(schema);
 
 module.exports = {
   rights: {
@@ -17,5 +22,23 @@ module.exports = {
     admin: 'rw',
     user: 'rw'
   },
-  validator: createValidator(schema)
+  validator
 };
+
+function validator(datetime) {
+  const valid = validate(datetime);
+
+  if (!valid) {
+    return buildErrorMessage(validate.errors);
+  }
+
+  return _validateDatetime(datetime);
+}
+
+function _validateDatetime(datetime) {
+  if (!momentTimeZone.tz.zone(datetime.timeZone)) {
+    return `time zone "${datetime.timeZone}" is not supported`;
+  }
+
+  return null;
+}
