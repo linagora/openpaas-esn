88a1f3934e5f814acfd411b7029b9ae845deca70
Dung T Pham <dtpham@linagora.com>
2018-09-25 16:57:29 +0700

    linagora.esn.admin#473 Restyle the language selector, add helper text for domain configuration

--- frontend/js/modules/i18n/language-selector/i18n-language-selector.controller.js
+++ frontend/js/modules/i18n/language-selector/i18n-language-selector.controller.js
@@ -8,18 +8,5 @@
     var self = this;
 
     self.languages = ESN_I18N_AVAILABLE_LANGUAGES;
-    self.setLanguage = setLanguage;
-    self.isLanguageSelected = isLanguageSelected;
-
-    function setLanguage(language, form) {
-      if (language.key !== self.language) {
-        self.language = language.key;
-        form.$setDirty();
-      }
-    }
-
-    function isLanguageSelected(language) {
-      return self.language === language.key;
-    }
   }
 })(angular);
