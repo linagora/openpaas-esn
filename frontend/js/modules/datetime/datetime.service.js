7abde4cc823f1608557a3bcd0a53e256231d119c
Tuan LE CONG <tlcong@linagora.com>
2018-09-28 16:45:51 +0200

    Revert "Merge branch 'linagora.esn.admin#479' into 'master'"
    This reverts merge request !543

--- frontend/js/modules/datetime/datetime.service.js
+++ frontend/js/modules/datetime/datetime.service.js
@@ -5,19 +5,14 @@
     .factory('esnDatetimeService', esnDatetimeService);
 
   function esnDatetimeService(
-    $q,
-    _,
-    moment,
+    $filter,
     esnConfig,
     ESN_DATETIME_DEFAULT_FORMAT,
     ESN_DATETIME_DEFAULT_TIMEZONE,
     ESN_DATETIME_TIME_FORMATS,
-    ESN_DATETIME_AVAILABLE_DATETIME_FORMATS,
-    ESN_DATETIME_DATE_FORMATS
+    ESN_DATETIME_AVAILABLE_DATETIME_FORMATS
   ) {
     var timeFormat = ESN_DATETIME_DEFAULT_FORMAT.time;
-    var timeZone = ESN_DATETIME_DEFAULT_TIMEZONE;
-    var dateFormats = ESN_DATETIME_DATE_FORMATS.en;
 
     return {
       init: init,
@@ -30,16 +25,9 @@
     };
 
     function init() {
-      $q.all([
-        esnConfig('core.datetime'),
-        esnConfig('core.language')
-      ]).then(function(configs) {
-        timeFormat = configs[0].use24hourFormat ? ESN_DATETIME_TIME_FORMATS.format24 : ESN_DATETIME_TIME_FORMATS.format12;
-        timeZone = configs[0].timeZone;
-        moment.locale(configs[1]);
-
-        if (ESN_DATETIME_DATE_FORMATS[configs[1]]) {
-          dateFormats = ESN_DATETIME_DATE_FORMATS[configs[1]];
+      esnConfig('core.datetime').then(function(datetime) {
+        if (datetime) {
+          timeFormat = datetime.use24hourFormat ? ESN_DATETIME_TIME_FORMATS.format24 : ESN_DATETIME_TIME_FORMATS.format12;
         }
       });
     }
@@ -79,14 +67,11 @@
     }
 
     function _getFormattedDatetime(date, format) {
-      date = date instanceof Date ? date : new Date(date);
-      var convertedDateByTimeZone = moment.tz(date, timeZone);
-
       return ESN_DATETIME_AVAILABLE_DATETIME_FORMATS.reduce(function(datetimeString, formatString) {
         var formatRegexp = new RegExp(formatString, 'g');
 
         return datetimeString.replace(formatRegexp, function() {
-          return convertedDateByTimeZone.format(formatString === 'time' ? timeFormat : dateFormats[formatString]);
+            return $filter('date')(date, formatString === 'time' ? timeFormat : formatString, ESN_DATETIME_DEFAULT_TIMEZONE);
         });
       }, format);
     }
