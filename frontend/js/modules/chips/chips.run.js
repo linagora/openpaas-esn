342ed7c0472bad85d206bca5b95801c97f077d0a
Christophe Henry <chenry@linagora.com>
2018-10-09 11:11:32 +0200

    Solves linagora.esn.unifiedinbox#931: emails tags tooltip disappeared

--- frontend/js/modules/chips/chips.run.js
+++ frontend/js/modules/chips/chips.run.js
@@ -5,7 +5,7 @@
 
   function runBlock($templateCache) {
     $templateCache.put('ngTagsInput/tag-item.html',
-      '<div class="esn-chip">' +
+      '<div class="esn-chip" title="{{ data.email }}">' +
         '<span ng-bind="$getDisplayText()"></span>' +
         '<i class="mdi mdi-close-circle" ng-click="$removeTag()"></i>' +
       '</div>'
