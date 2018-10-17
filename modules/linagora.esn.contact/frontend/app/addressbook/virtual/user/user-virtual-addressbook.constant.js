5def93625a7dc5615f1385ea204d7db57e8ad104
Christophe Hamerling <chamerling@linagora.com>
2018-10-17 16:45:25 +0200

    #2506 Rename the virtual book id

---
+++ modules/linagora.esn.contact/frontend/app/addressbook/virtual/user/user-virtual-addressbook.constant.js
@@ -0,0 +1,7 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('linagora.esn.contact')
+    .constant('CONTACT_USER_VIRTUAL_ADDRESSBOOK_ID', 'linagora.esn.contact.virtual.members');
+
+})(angular);
--- modules/linagora.esn.contact/frontend/app/addressbook/virtual/user/user-virtual-addressbook.service.js
+++ modules/linagora.esn.contact/frontend/app/addressbook/virtual/user/user-virtual-addressbook.service.js
@@ -3,13 +3,13 @@
 
   angular.module('linagora.esn.contact').factory('ContactUserVirtualAddressBook', ContactUserVirtualAddressBook);
 
-  function ContactUserVirtualAddressBook(ContactVirtualAddressBook, ContactUserShell, ContactVirtualUsersLoaderService) {
+  function ContactUserVirtualAddressBook(ContactVirtualAddressBook, ContactUserShell, ContactVirtualUsersLoaderService, CONTACT_USER_VIRTUAL_ADDRESSBOOK_ID) {
     var options = {
       configuration: {
         enabled: 'linagora.esn.contact.features.isVirtualUserAddressbookEnabled'
       }
     };
-    var addressbook = new ContactVirtualAddressBook('linagora.esn.contact.virtual.users', 'All members', loadNextItems, options);
+    var addressbook = new ContactVirtualAddressBook(CONTACT_USER_VIRTUAL_ADDRESSBOOK_ID, 'All members', loadNextItems, options);
 
     function loadNextItems(options) {
       return ContactVirtualUsersLoaderService.list(options).then(function(result) {
