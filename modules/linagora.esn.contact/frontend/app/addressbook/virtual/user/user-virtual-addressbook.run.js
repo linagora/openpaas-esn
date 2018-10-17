9fb9a1aa78d7dbb6b0d008a50aaa2cc11ad56523
Christophe Hamerling <chamerling@linagora.com>
2018-10-17 16:45:25 +0200

    #2506 Rename the virtual book id

--- modules/linagora.esn.contact/frontend/app/addressbook/virtual/user/user-virtual-addressbook.run.js
+++ modules/linagora.esn.contact/frontend/app/addressbook/virtual/user/user-virtual-addressbook.run.js
@@ -1,15 +1,25 @@
 (function(angular) {
   'use strict';
 
-  angular.module('linagora.esn.contact')
-    .run(function(ContactVirtualAddressBookRegistry, ContactUserVirtualAddressBook, DisplayShellProvider, ContactUserDisplayShell, ContactUserShellHelper, contactAddressbookDisplayShellRegistry, ContactUserVirtualAddressBookDisplayShell) {
-      ContactVirtualAddressBookRegistry.put(ContactUserVirtualAddressBook);
-      DisplayShellProvider.addDisplayShell(ContactUserDisplayShell, ContactUserShellHelper.isUser);
-      contactAddressbookDisplayShellRegistry.add({
-        id: 'linagora.esn.contact.virtual.users',
-        priority: 20,
-        displayShell: ContactUserVirtualAddressBookDisplayShell,
-        matchingFunction: ContactUserShellHelper.isAddressbook
-      });
+  angular.module('linagora.esn.contact').run(runBlock);
+
+  function runBlock(
+    ContactVirtualAddressBookRegistry,
+    ContactUserVirtualAddressBook,
+    DisplayShellProvider,
+    ContactUserDisplayShell,
+    ContactUserShellHelper,
+    contactAddressbookDisplayShellRegistry,
+    ContactUserVirtualAddressBookDisplayShell,
+    CONTACT_USER_VIRTUAL_ADDRESSBOOK_ID
+  ) {
+    ContactVirtualAddressBookRegistry.put(ContactUserVirtualAddressBook);
+    DisplayShellProvider.addDisplayShell(ContactUserDisplayShell, ContactUserShellHelper.isUser);
+    contactAddressbookDisplayShellRegistry.add({
+      id: CONTACT_USER_VIRTUAL_ADDRESSBOOK_ID,
+      priority: 20,
+      displayShell: ContactUserVirtualAddressBookDisplayShell,
+      matchingFunction: ContactUserShellHelper.isAddressbook
     });
+  }
 })(angular);
