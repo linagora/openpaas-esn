cc58967feeabc89bdbf58b5503312e0b43a3755a
Christophe Hamerling <chamerling@linagora.com>
2018-10-18 14:29:56 +0200

    #2538 Extract the sync module in a separate repository

--- modules/linagora.esn.sync/backend/webserver/config/views.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const FRONTEND_PATH = require('../constants').FRONTEND_PATH;
-
-module.exports = (dependencies, application) => {
-  application.use(require('express').static(FRONTEND_PATH));
-  application.set('views', FRONTEND_PATH + '/app');
-  application.get('/app/*', (req, res) => res.render(req.params[0].replace(/\.html$/, '')));
-};
--- modules/linagora.esn.sync/frontend/app/components/android/android.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('linagora.esn.sync')
-
-  .component('syncAndroid', {
-    templateUrl: '/sync/app/components/android/android.html'
-  });
