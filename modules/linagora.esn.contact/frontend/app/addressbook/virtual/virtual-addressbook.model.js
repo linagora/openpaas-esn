6a9c68966c6d62f5b5645aa57ea6471260d45a12
Dung Dung PHAM <dtpham@linagora.com>
2018-12-11 15:35:02 +0700

    linagora.esn.contact#460 Load number of contacts of virtual addressbooks

--- modules/linagora.esn.contact/frontend/app/addressbook/virtual/virtual-addressbook.model.js
+++ modules/linagora.esn.contact/frontend/app/addressbook/virtual/virtual-addressbook.model.js
@@ -5,15 +5,26 @@
     .factory('ContactVirtualAddressBook', ContactVirtualAddressBook);
 
   function ContactVirtualAddressBook(CONTACT_ADDRESSBOOK_TYPES) {
-    function ContactVirtualAddressBook(id, name, loadNextItems, options) {
+    function ContactVirtualAddressBook(id, name, loadNextItems, getContactsCount, options) {
       this.id = id;
       this.name = name;
       this.bookName = id;
       this.type = CONTACT_ADDRESSBOOK_TYPES.virtual;
       this.description = 'Virtual addressbook';
       this.loadNextItems = loadNextItems;
+      this.getContactsCount = getContactsCount;
       this.options = options;
       this.excludeFromAggregate = options && options.excludeFromAggregate;
+    }
+
+    ContactVirtualAddressBook.prototype.loadContactsCount = loadContactsCount;
+
+    function loadContactsCount() {
+      var self = this;
+
+      return self.getContactsCount().then(function(count) {
+        self.numberOfContacts = count;
+      });
     }
 
     return ContactVirtualAddressBook;
