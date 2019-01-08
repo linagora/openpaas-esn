f181365068cbb1c846823e752f321cb9f2629a00
Loic AUBLET <laublet@linagora.com>
2019-01-08 17:47:18 +0100

    #2454 add chinese language

--- frontend/js/modules/i18n/i18n.constants.js
+++ frontend/js/modules/i18n/i18n.constants.js
@@ -7,26 +7,31 @@
     .constant('ESN_I18N_AVAILABLE_LANGUAGES', [
       { key: 'en', label: 'English' },
       { key: 'fr', label: 'Français' },
-      { key: 'vi', label: 'Tiếng Việt' }
+      { key: 'vi', label: 'Tiếng Việt' },
+      { key: 'zh', label: '中文' }
     ])
     .constant('ESN_I18N_AVAILABLE_LANGUAGE_ALIASES', {
       'en_*': 'en',
       'fr_*': 'fr',
-      'vi_*': 'vi'
+      'vi_*': 'vi',
+      'zh_*': 'zh'
     })
     .constant('ESN_I18N_FULL_LOCALE', {
       en: 'en-US',
       fr: 'fr-FR',
-      vi: 'vi-VN'
+      vi: 'vi-VN',
+      zh: 'zh-TW'
     })
     .constant('ESN_I18N_DATE_FORMAT', {
       en: 'yyyy/MM/dd',
       fr: 'dd/MM/yyyy',
-      vi: 'dd/MM/yyyy'
+      vi: 'dd/MM/yyyy',
+      zh: 'yyyy/MM/dd'
     })
     .constant('ESN_I18N_LONG_DATE_FORMAT', {
       en: 'EEE yyyy/MM/dd',
       fr: 'EEE dd/MM/yyyy',
-      vi: 'EEE dd/MM/yyyy'
+      vi: 'EEE dd/MM/yyyy',
+      zh: 'EEE yyyy/MM/dd'
     });
 })();
