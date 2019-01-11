d314bda7164290b49f9ca13c704d5acb0a8a7701
Christophe HAMERLING <chamerling@linagora.com>
2019-01-11 17:54:23 +0100

    #2574 Increase the numbers of elements to show

--- frontend/js/modules/collaboration/collaboration.constants.js
+++ frontend/js/modules/collaboration/collaboration.constants.js
@@ -5,7 +5,9 @@
     .constant('ESN_COLLABORATION_MEMBERSHIP_EVENTS', {
       JOIN: 'collaboration:join',
       LEAVE: 'collaboration:leave'
-    }).constant('ESN_COLLABORATION_MEMBER_EVENTS', {
+    })
+    .constant('ESN_COLLABORATION_MEMBER_SEARCH_LENGTH', 20)
+    .constant('ESN_COLLABORATION_MEMBER_EVENTS', {
       REMOVED: 'collaboration:member:removed'
     });
 })();
