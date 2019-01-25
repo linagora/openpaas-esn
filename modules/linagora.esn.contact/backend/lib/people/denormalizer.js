050d3aea31caa86fb1a19b35c3f8e8c8da5e01c9
Christophe HAMERLING <chamerling@linagora.com>
2019-01-25 18:37:55 +0100

    #2594 Add contact photos

--- modules/linagora.esn.contact/backend/lib/people/denormalizer.js
+++ modules/linagora.esn.contact/backend/lib/people/denormalizer.js
@@ -4,7 +4,7 @@ module.exports = dependencies => {
   const { Model } = dependencies('people');
 
   return ({ source }) => {
-    const vcard = new ICAL.Component(source);
+    const vcard = new ICAL.Component(source.body);
     const id = vcard.getFirstPropertyValue('uid');
     const fullName = vcard.getFirstPropertyValue('fn');
     const name = vcard.getFirstPropertyValue('n');
@@ -18,16 +18,22 @@ module.exports = dependencies => {
     const displayName = (fullName || `${firstName} ${lastName}`).trim();
     const emailAddresses = emails.map(email => new Model.EmailAddress({ value: email.value, type: email.type }));
     const names = [new Model.Name({ displayName })];
+    const photos = [new Model.Photo({ url: getAvatarPath(source) })];
 
     return Promise.resolve(
       new Model.Person({
         id,
         objectType: 'contact',
         emailAddresses,
-        names
+        names,
+        photos
       })
     );
   };
+
+  function getAvatarPath(source) {
+    return `contact/api/contacts/${source.bookId}/${source.bookName}/${source.contactId}/avatar`;
+  }
 
   function getMultiValue(vcard, propName) {
     var props = vcard.getAllProperties(propName);
