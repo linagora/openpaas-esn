2e6b68e404944e4c3abd2b2fab468e29c5dd6302
Aymane KHOUAJI <akhouaji@linagora.com>
2019-01-30 14:39:59 +0100

    Fix get user-status if api is called with user email

--- modules/linagora.esn.user.status/backend/webserver/controllers/user-status.js
+++ modules/linagora.esn.user.status/backend/webserver/controllers/user-status.js
@@ -6,6 +6,8 @@ const Q = require('q');
 module.exports = function(dependencies, lib) {
 
   const logger = dependencies('logger');
+  const { db } = dependencies('helpers');
+  const userModule = dependencies('user');
 
   return {
     getUserStatus,
@@ -22,26 +24,61 @@ module.exports = function(dependencies, lib) {
     return Q(result);
   }
 
+  function _getUserIdFromEmail(email, callback) {
+    return userModule.findByEmail(email, callback);
+  }
+
   function getConnectedStatus(status) {
     return !!status && (Date.now() - status.last_active) < CONSTANTS.DISCONNECTED_DELAY ? CONSTANTS.STATUS.CONNECTED : CONSTANTS.STATUS.DISCONNECTED;
   }
 
   function getUserStatus(req, res) {
-    lib.userStatus.getStatus(req.params.id)
-    .then(denormalize.bind(null, req.params.id))
-    .then(status => {
-      res.status(200).json(status);
-    }).catch(err => {
-      logger.error(`Error while getting user ${req.params.id} status`, err);
+    if (!db.isValidObjectId(req.params.id)) {
+      // we think here req.params.id is the mail of the user
+      _getUserIdFromEmail(req.params.id, (err, user) => {
+        if (err) {
+          logger.error(`Error while getting user ${req.params.id} status`, err);
 
-      res.status(500).json({
-        error: {
-          code: 500,
-          message: 'Server Error',
-          details: `Error while fetching user status for user ${req.params.id}`
+          return res.status(500).json({
+            error: {
+              code: 500,
+              message: 'Server Error',
+              details: `Error while fetching user status for user ${req.params.id}`
+            }
+          });
         }
+
+        if (!user) {
+          return res.status(404).json({
+            error: {
+              code: 404,
+              message: 'Not Found',
+              details: `${req.params.id} not found`
+            }
+          });
+        }
+
+        req.params.id = user._id;
+
+        return getUserStatus(req, res);
       });
-    });
+    } else {
+      lib.userStatus.getStatus(req.params.id)
+      .then(denormalize.bind(null, req.params.id))
+      .then(status => {
+        res.status(200).json(status);
+      }).catch(err => {
+        logger.error(`Error while getting user ${req.params.id} status`, err);
+
+        res.status(500).json({
+          error: {
+            code: 500,
+            message: 'Server Error',
+            details: `Error while fetching user status for user ${req.params.id}`
+          }
+        });
+      });
+    }
   }
 
   function getUsersStatus(req, res) {
