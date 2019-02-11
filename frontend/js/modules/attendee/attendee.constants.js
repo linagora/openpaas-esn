5f8527624a8b3e220b282d64b53c5ada775cc08e
Christophe HAMERLING <chamerling@linagora.com>
2019-02-11 14:28:33 +0100

    linagora/lgs/openpaas/linagora.esn.contact#466 Update autocomplete templates for attendees/users

--- frontend/js/modules/attendee/attendee.constants.js
+++ frontend/js/modules/attendee/attendee.constants.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('esn.attendee')
-    .constant('ESN_ATTENDEE_DEFAULT_TEMPLATE_URL', '/views/modules/auto-complete/user-auto-complete')
+    .constant('ESN_ATTENDEE_DEFAULT_TEMPLATE_URL', '/views/modules/attendee/attendee-auto-complete.html')
     .constant('ESN_ATTENDEE_DEFAULT_OBJECT_TYPE', 'user');
 
 })(angular);
