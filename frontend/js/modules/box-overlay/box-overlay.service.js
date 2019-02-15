97268d6551b1172dd490623b77164b46440a4328
Christophe HAMERLING <chamerling@linagora.com>
2019-02-15 15:24:02 +0100

    #2629 clean service

--- frontend/js/modules/box-overlay/box-overlay.service.js
+++ frontend/js/modules/box-overlay/box-overlay.service.js
@@ -6,6 +6,14 @@
   function boxOverlayService($rootScope, notificationFactory, MAX_BOX_COUNT) {
     var boxScopes = [];
 
+    return {
+      spaceLeftOnScreen: spaceLeftOnScreen,
+      addBox: addBox,
+      removeBox: removeBox,
+      maximizedBoxExists: maximizedBoxExists,
+      minimizeOthers: minimizeOthers
+    };
+
     function count() {
       return boxScopes.length;
     }
@@ -22,49 +30,49 @@
       return scope.id && boxScopes.some(function(element) { return element.id === scope.id; });
     }
 
-    return {
-      spaceLeftOnScreen: spaceLeftOnScreen,
-      addBox: function(scope) {
-        if (isBoxAlreadyOpened(scope)) {
-          return false;
-        }
+    function addBox(scope) {
+      if (isBoxAlreadyOpened(scope)) {
+        return false;
+      }
 
-        if (!spaceLeftOnScreen()) {
-          notificationFactory.weakError('', 'Cannot open more than ' + MAX_BOX_COUNT + ' windows. Please close one and try again');
+      if (!spaceLeftOnScreen()) {
+        notificationFactory.weakError('', 'Cannot open more than ' + MAX_BOX_COUNT + ' windows. Please close one and try again');
 
-          return false;
-        }
+        return false;
+      }
 
-        boxScopes.push(scope);
+      boxScopes.push(scope);
 
-        if (!spaceLeftOnScreen()) {
-          $rootScope.$broadcast('box-overlay:no-space-left-on-screen');
-        }
+      if (!spaceLeftOnScreen()) {
+        $rootScope.$broadcast('box-overlay:no-space-left-on-screen');
+      }
 
-        return true;
-      },
-      removeBox: function(scope) {
-        if (count() > 0) {
-          var index = boxScopes.indexOf(scope);
+      return true;
+    }
 
-          if (index > -1) {
-            boxScopes.splice(index, 1);
+    function removeBox(scope) {
+      if (count() > 0) {
+        var index = boxScopes.indexOf(scope);
 
-            if (onlyOneSpaceLeftOnScreen()) {
-              $rootScope.$broadcast('box-overlay:space-left-on-screen');
-            }
+        if (index > -1) {
+          boxScopes.splice(index, 1);
+
+          if (onlyOneSpaceLeftOnScreen()) {
+            $rootScope.$broadcast('box-overlay:space-left-on-screen');
           }
         }
-      },
-      maximizedBoxExists: function() {
-        return boxScopes.some(function(scope) { return scope.isMaximized() || scope.isFullScreen(); });
-      },
-      minimizeOthers: function(me) {
-        return boxScopes
-          .filter(function(scope) { return scope !== me; })
-          .forEach(function(scope) { scope.$minimize(); });
       }
-    };
+    }
+
+    function maximizedBoxExists() {
+      return boxScopes.some(function(scope) { return scope.isMaximized() || scope.isFullScreen(); });
+    }
+
+    function minimizeOthers(me) {
+      return boxScopes
+        .filter(function(scope) { return scope !== me; })
+        .forEach(function(scope) { scope.$minimize(); });
+    }
   }
 
 })(angular);
