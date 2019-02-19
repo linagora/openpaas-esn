963689f90c4254af7c4b60ce8fef497a61d8d263
Christophe HAMERLING <chamerling@linagora.com>
2019-02-19 06:57:04 +0100

    #2629 Allow multiple boxes using flex and some magic

--- frontend/js/modules/box-overlay/box-overlay.constants.js
+++ frontend/js/modules/box-overlay/box-overlay.constants.js
@@ -4,6 +4,7 @@
   angular.module('esn.box-overlay')
     .constant('ESN_BOX_OVERLAY_EVENTS', {
       RESIZED: 'esn:box-overlay:resized'
-    });
+    })
+    .constant('ESN_BOX_OVERLAY_MAX_WINDOWS', 30);
 
 })(angular);
--- frontend/js/modules/box-overlay/box-overlay.service.js
+++ frontend/js/modules/box-overlay/box-overlay.service.js
@@ -3,7 +3,7 @@
 
   angular.module('esn.box-overlay').service('boxOverlayService', boxOverlayService);
 
-  function boxOverlayService($rootScope, notificationFactory, MAX_BOX_COUNT) {
+  function boxOverlayService(_, $rootScope, $window, $compile, notificationFactory, ESN_BOX_OVERLAY_MAX_WINDOWS) {
     var boxScopes = [];
 
     return {
@@ -11,37 +11,50 @@
       addBox: addBox,
       removeBox: removeBox,
       maximizedBoxExists: maximizedBoxExists,
-      minimizeOthers: minimizeOthers
+      minimizeOthers: minimizeOthers,
+      minimizeAround: minimizeAround,
+      hideAround: hideAround,
+      overflows: overflows,
+      showAll: showAll,
+      onShow: onShow,
+      onHide: onHide,
+      getContainer: getContainer,
+      ensureContainerExists: ensureContainerExists,
+      removeContainerIfPossible: removeContainerIfPossible
     };
+
+    function overflows() {
+      return getWidth() > getContainer()[0].offsetWidth;
+    }
 
     function count() {
       return boxScopes.length;
     }
 
     function spaceLeftOnScreen() {
-      return count() < MAX_BOX_COUNT;
+      return count() < ESN_BOX_OVERLAY_MAX_WINDOWS;
     }
 
     function onlyOneSpaceLeftOnScreen() {
-      return count() === (MAX_BOX_COUNT - 1);
+      return count() === (ESN_BOX_OVERLAY_MAX_WINDOWS - 1);
     }
 
     function isBoxAlreadyOpened(scope) {
-      return scope.id && boxScopes.some(function(element) { return element.id === scope.id; });
+      return scope.id && boxScopes.some(function(element) { return element.scope.id === scope.id; });
     }
 
-    function addBox(scope) {
+    function addBox(scope, box) {
       if (isBoxAlreadyOpened(scope)) {
         return false;
       }
 
       if (!spaceLeftOnScreen()) {
-        notificationFactory.weakError('', 'Cannot open more than ' + MAX_BOX_COUNT + ' windows. Please close one and try again');
+        notificationFactory.weakError('', 'Cannot open more than ' + ESN_BOX_OVERLAY_MAX_WINDOWS + ' windows. Please close one and try again');
 
         return false;
       }
 
-      boxScopes.push(scope);
+      boxScopes.push({scope: scope, box: box});
 
       if (!spaceLeftOnScreen()) {
         $rootScope.$broadcast('box-overlay:no-space-left-on-screen');
@@ -52,7 +65,7 @@
 
     function removeBox(scope) {
       if (count() > 0) {
-        var index = boxScopes.indexOf(scope);
+        var index = _findBoxIndex(scope);
 
         if (index > -1) {
           boxScopes.splice(index, 1);
@@ -65,13 +78,104 @@
     }
 
     function maximizedBoxExists() {
-      return boxScopes.some(function(scope) { return scope.isMaximized() || scope.isFullScreen(); });
+      return boxScopes.some(function(element) { return element.scope.isMaximized() || element.scope.isFullScreen(); });
     }
 
     function minimizeOthers(me) {
       return boxScopes
-        .filter(function(scope) { return scope !== me; })
-        .forEach(function(scope) { scope.$minimize(); });
+        .filter(function(element) { return element.scope !== me; })
+        .forEach(function(element) { element.scope.$minimize(); });
+    }
+
+    function hideAround(scope) {
+      if (count() === 1) {
+        return;
+      }
+
+      var index = _findBoxIndex(scope);
+      console.log('BOXINDEX', index);
+      console.log('BOX', boxScopes[index]);
+
+      if (index < 0) {
+        return;
+      }
+
+      // TODO: First, try to hide the ones which are minimized...
+      // Then if it does not fit, hide maximized ones...
+      if (index === 0) {
+        // FIXME: hide only if current elemetn does not fit
+        setDisplay(1, 'none');
+      } else {
+        setDisplay(index - 1, 'none');
+      }
+    }
+
+    function setDisplay(index, display) {
+      boxScopes[index].box.$element[0].style.display = display;
+    }
+
+    function showAll() {
+      boxScopes.forEach(function(element) {
+        element.box.$element[0].style.display = '';
+      });
+    }
+
+    function getWidth() {
+      return boxScopes.map(function(bs) {
+        return bs.box.$element[0].offsetWidth;
+      }).reduce(function(accumulator, width) {
+        return accumulator + width;
+      });
+    }
+
+    function minimizeAround(scope) {
+      if (!boxScopes.length || boxScopes.length === 1) {
+        return;
+      }
+
+      if (scope === boxScopes[0].scope) {
+        boxScopes[1].scope.$minimize();
+      }
+
+      if (scope === boxScopes[boxScopes.length - 1].scope) {
+        boxScopes[boxScopes.length - 1].scope.$minimize();
+      }
+    }
+
+    function onShow(scope) {
+      // if it does not fit in, be radical and minimize all others
+      if (getWidth() > getContainer()[0].offsetWidth) {
+        // TODO: Minimize from last until there is enough place
+        minimizeOthers(scope);
+      }
+    }
+
+    function onHide() {
+      removeContainerIfPossible();
+    }
+
+    function getContainer() {
+      return angular.element('body .box-overlay-container');
+    }
+
+    function ensureContainerExists() {
+      if (getContainer().length === 0) {
+        angular.element($window.document.body).append($compile('<box-overlay-container></box-overlay-container>')($rootScope.$new()));
+      }
+
+      return getContainer();
+    }
+
+    function removeContainerIfPossible() {
+      var element = getContainer();
+
+      if (element.children().length === 0) {
+        element.remove();
+      }
+    }
+
+    function _findBoxIndex(scope) {
+      return _.findIndex(boxScopes, {scope: scope});
     }
   }
 
