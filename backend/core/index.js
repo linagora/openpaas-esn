e4c5354f2a8a9a26b893b2954ce0d82d2ed53871
Christophe HAMERLING <chamerling@linagora.com>
2019-04-03 10:33:19 +0200

    #2666 Clean core/index.js

--- backend/core/index.js
+++ backend/core/index.js
@@ -1,20 +1,24 @@
-'use strict';
-
 require('./env');
-var fs = require('fs');
+
+const fs = require('fs');
+
 exports = module.exports = {};
 
-fs.readdirSync(__dirname).forEach(function(filename) {
-  var stat = fs.statSync(__dirname + '/' + filename);
+fs.readdirSync(__dirname).forEach(filename => {
+  const stat = fs.statSync(__dirname + '/' + filename);
+
   if (!stat.isDirectory()) { return; }
+
   function load() { return require('./' + filename); }
   exports.__defineGetter__(filename, load); // eslint-disable-line no-restricted-properties
 });
 
-exports.init = function(callback) {
+exports.init = init;
+
+function init(callback) {
   exports.db.mongo.init();
   exports.pubsub.init();
   if (callback) {
     callback();
   }
-};
+}
