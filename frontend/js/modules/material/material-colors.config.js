d776543615fac800ff7a7a82b34759e8b1d4697e
Fabien MOYON <fmoyon@linagora.com>
2019-05-02 15:22:35 +0200

    esn#2729 create the default openpaas palette and overwrite it the by primaryColor

--- frontend/js/modules/material/material-colors.config.js
+++ frontend/js/modules/material/material-colors.config.js
@@ -5,7 +5,20 @@
 
   function configBlock($mdThemingProvider) {
     var openpaasPalette = $mdThemingProvider.extendPalette('indigo', {
-      500: '#2196F3'
+      50: 'e4f2fe',
+      100: 'bce0fb',
+      200: '90cbf9',
+      300: '64b6f7',
+      400: '42a6f5',
+      500: '2196f3',
+      600: '1d8ef1',
+      700: '1883ef',
+      800: '1479ed',
+      900: '0b68ea',
+      A100: 'ffffff',
+      A200: 'e1ecff',
+      A400: 'aeccff',
+      A700: '95bcff'
     });
 
     $mdThemingProvider.definePalette('openpaas', openpaasPalette);
