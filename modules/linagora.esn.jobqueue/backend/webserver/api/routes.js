0eb2fdd4db0082d9811eaf5c0dfa95970e85d843
Tuan Tuan LE <tlcong@linagora.com>
2019-07-19 16:46:03 +0700

    #1859 only allows platform admin to access to KUE UI router

--- modules/linagora.esn.jobqueue/backend/webserver/api/routes.js
+++ modules/linagora.esn.jobqueue/backend/webserver/api/routes.js
@@ -4,9 +4,10 @@ const express = require('express');
 
 module.exports = (lib, dependencies) => {
   const authorizationMW = dependencies('authorizationMW');
+  const platformAdminsMW = dependencies('platformAdminsMW');
   const router = express.Router();
 
-  router.get('/', authorizationMW.requiresAPILogin, (req, res) => res.redirect('/jobqueue/ui'));
+  router.get('/', authorizationMW.requiresAPILogin, platformAdminsMW.requirePlatformAdmin, (req, res) => res.redirect('/jobqueue/ui'));
 
   return router;
 };
