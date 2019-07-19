aaaa38d47fbaf73fc9411d50d84cf68da65d6521
Tuan Tuan LE <tlcong@linagora.com>
2019-07-19 16:42:19 +0700

    #1859 Configure application to able to load view templates

--- modules/linagora.esn.jobqueue/backend/webserver/config/views.js
+++ modules/linagora.esn.jobqueue/backend/webserver/config/views.js
@@ -1,10 +1,15 @@
-'use strict';
-
 const express = require('express');
-const FRONTEND_PATH = require('../constants').FRONTEND_PATH;
+const {
+  FRONTEND_PATH,
+  CORE_FRONTEND_PATH
+} = require('../constants');
 
 module.exports = (dependencies, application) => {
   application.use(express.static(FRONTEND_PATH));
-  application.set('views', FRONTEND_PATH + '/views');
-  application.get('/views/*', (req, res) => res.render(req.params[0].replace(/\.html$/, '')));
+  application.set('views', `${FRONTEND_PATH}/app`);
+  application.get('/app/*', function(req, res) {
+    const templateName = req.params[0].replace(/\.html$/, '');
+
+    res.render(templateName, { basedir: `${CORE_FRONTEND_PATH}/views` });
+  });
 };
--- modules/linagora.esn.jobqueue/backend/webserver/constants.js
+++ modules/linagora.esn.jobqueue/backend/webserver/constants.js
@@ -1,5 +1,6 @@
-'use strict';
-
 const path = require('path');
 
-module.exports.FRONTEND_PATH = path.normalize(__dirname + '/../../frontend');
+module.exports = {
+  FRONTEND_PATH: path.normalize(`${__dirname}/../../frontend`),
+  CORE_FRONTEND_PATH: path.normalize(`${path.dirname(require.main.filename)}/frontend`)
+};
