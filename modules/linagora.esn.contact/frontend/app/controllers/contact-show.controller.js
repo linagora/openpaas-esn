f5b99dd9521cdb4e834e37b75d82ba162e78bd81
Tuan Tuan LE <tlcong@linagora.com>
2019-09-17 17:21:52 +0200

    linagora/lgs/openpaas/linagora.esn.contact#501 move contact show controller to a dedicated file

---
+++ modules/linagora.esn.contact/frontend/app/controllers/contact-show.controller.js
@@ -0,0 +1,137 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('linagora.esn.contact')
+    .controller('showContactController', showContactController);
+
+  function showContactController(
+    $log,
+    $scope,
+    $state,
+    $timeout,
+    $stateParams,
+    $window,
+    ContactsHelper,
+    contactUpdateDataService,
+    ContactShellDisplayBuilder,
+    deleteContact,
+    sharedContactDataService,
+    contactDisplayError,
+    gracePeriodService,
+    contactService,
+    CONTACT_AVATAR_SIZE,
+    CONTACT_EVENTS
+  ) {
+    $scope.avatarSize = CONTACT_AVATAR_SIZE.bigger;
+    $scope.bookId = $stateParams.bookId;
+    $scope.bookName = $stateParams.bookName;
+    $scope.cardId = $stateParams.cardId;
+    $scope.contact = {};
+    $scope.loaded = false;
+
+    $scope.$on(CONTACT_EVENTS.UPDATED, function(e, data) {
+      if (data.id === $scope.cardId && data.addressbook && data.addressbook.bookName !== $scope.bookName) {
+        $state.go('contact.addressbooks.show', {
+          bookId: $scope.bookId,
+          bookName: data.addressbook.bookName,
+          cardId: data.id
+        }, { location: 'replace' });
+      }
+    });
+
+    function isAddressFilled(type) {
+      if (!$scope.contact.addresses || !$scope.contact.addresses.length) {
+        return false;
+      }
+
+      return $scope.contact.addresses.filter(function(address) {
+        return address.type.toLowerCase() === type.toLowerCase();
+      }).length;
+    }
+
+    $scope.fillContactData = function(contact) {
+      ContactsHelper.fillScopeContactData($scope, contact);
+      $scope.displayShell = ContactShellDisplayBuilder.build(contact);
+    };
+
+    $scope.getAddress = function(type) {
+      return $scope.contact.addresses.filter(function(address) {
+        return address.type.toLowerCase() === type.toLowerCase();
+      })[0];
+    };
+
+    $scope.edit = function() {
+      $state.go('contact.addressbooks.edit', {
+        bookId: $scope.bookId,
+        bookName: $scope.bookName,
+        cardId: $scope.cardId
+      }, { location: 'replace' });
+    };
+
+    $scope.deleteContact = function() {
+      $timeout(function() {
+        deleteContact($scope.bookId, $scope.bookName, $scope.contact);
+      }, 200);
+    };
+
+    $scope.shouldDisplayWork = function() {
+      return !!($scope.contact.orgName || $scope.contact.orgRole || isAddressFilled('work'));
+    };
+
+    $scope.shouldDisplayHome = function() {
+      return !!(isAddressFilled('home') || $scope.formattedBirthday || $scope.contact.nickname);
+    };
+
+    $scope.shouldDisplayOthers = function() {
+      return !!(isAddressFilled('other') || ($scope.contact.tags && $scope.contact.tags.length) || $scope.contact.notes || ($scope.contact.urls && $scope.contact.urls.length));
+    };
+
+    $scope.openAddressbook = function() {
+      $state.go('contact.addressbooks', { bookName: $scope.contact.addressbook.bookName });
+    };
+
+    if (contactUpdateDataService.contact) {
+
+      $scope.fillContactData(contactUpdateDataService.contact);
+
+      $scope.$on('$stateChangeStart', function(evt, next, nextParams) {
+        gracePeriodService.flush(contactUpdateDataService.taskId);
+        // check if the user edit the contact again
+        if (next && next.name && nextParams &&
+            next.name === 'contact.addressbooks.edit' &&
+            nextParams.bookId === $scope.bookId &&
+            nextParams.bookName === $scope.bookName &&
+            nextParams.cardId === $scope.cardId) {
+          // cache the contact to show in editContactController
+          contactUpdateDataService.contact = $scope.contact;
+        } else {
+          contactUpdateDataService.contact = null;
+        }
+      });
+
+      $scope.$on(CONTACT_EVENTS.CANCEL_UPDATE, function(evt, data) {
+        if (data.id === $scope.cardId) {
+          $scope.contact = data;
+        }
+      });
+
+      $window.addEventListener('beforeunload', function() {
+        gracePeriodService.flush(contactUpdateDataService.taskId);
+      });
+
+      $scope.loaded = true;
+    } else {
+      contactService.getContact({ bookId: $scope.bookId, bookName: $scope.bookName }, $scope.cardId)
+        .then($scope.fillContactData, function(err) {
+          $log.debug('Error while loading contact', err);
+          $scope.error = true;
+          contactDisplayError('Cannot get contact details');
+        })
+        .finally(function() {
+          $scope.loaded = true;
+        });
+    }
+
+    sharedContactDataService.contact = {};
+  }
+})(angular);
---
+++ modules/linagora.esn.contact/frontend/app/controllers/contact-show.controller.spec.js
@@ -0,0 +1,481 @@
+'use strict';
+
+/* global sinon, chai: false */
+
+var expect = chai.expect;
+
+describe('The showContactController', function() {
+  var $window, $controller, $state, $rootScope, $alert, $timeout, $stateParams, $location;
+  var scope, contactUpdateDataService, ContactShellDisplayBuilder, gracePeriodService, ContactAPIClient, contactAddressbookDisplayService;
+  var ContactShell, notificationFactory, selectionService, VcardBuilder, ContactLocationHelper;
+  var CONTACT_AVATAR_SIZE, CONTACT_EVENTS;
+  var bookId = '123456789', bookName = 'bookName', cardId = '987654321';
+
+  beforeEach(function() {
+    module('esn.core');
+    module('linagora.esn.contact');
+
+    ContactShellDisplayBuilder = {
+      build: function(shell) {return shell;}
+    };
+
+    ContactShell = function() {};
+    notificationFactory = {
+      weakError: sinon.spy(),
+      weakInfo: sinon.spy(),
+      strongError: sinon.spy()
+    };
+    $location = {
+      path: function() {},
+      url: function() {},
+      search: function() {
+        return {
+          q: {
+            replace: function() {}
+          }
+        };
+      }
+    };
+    $stateParams = {
+      bookId: bookId,
+      bookName: bookName
+    };
+    selectionService = {
+      clear: function() {}
+    };
+    $alert = {
+      alert: function() {}
+    };
+    gracePeriodService = {
+      askUserForCancel: function() {
+        return {promise: $q.when({})};
+      },
+      grace: function() {
+        return {
+          then: function() {}
+        };
+      },
+      cancel: function() {},
+      flush: function() {}
+    };
+
+    contactUpdateDataService = { contact: null, taskId: null, contactUpdatedIds: [] };
+
+    ContactAPIClient = {
+      addressbookHome: function() {
+        return {
+          addressbook: function() {
+            return {
+              list: function() { return $q.when([]); },
+              get: function() { return $q.when({ name: 'My Contacts' }); },
+              vcard: function() {
+                return {
+                  get: function() { return $q.when(); },
+                  list: function() { return $q.when({}); },
+                  search: function() { return $q.when({ data: [] }); },
+                  create: function() { return $q.when(); },
+                  update: function() { return $q.when(); },
+                  remove: function() { return $q.when(); }
+                };
+              }
+            };
+          }
+        };
+      }
+    };
+
+    VcardBuilder = {
+      toVcard: function() {
+        return 'vcard';
+      }
+    };
+
+    ContactLocationHelper = {
+      home: function() {},
+      contact: {
+        new: function() {},
+        show: function() {},
+        edit: function() {}
+      }
+    };
+
+    module(function($provide) {
+      $provide.value('notificationFactory', notificationFactory);
+      $provide.value('$location', $location);
+      $provide.value('selectionService', selectionService);
+      $provide.value('$stateParams', $stateParams);
+      $provide.value('$alert', function(options) { $alert.alert(options); });
+      $provide.value('gracePeriodService', gracePeriodService);
+      $provide.value('contactUpdateDataService', contactUpdateDataService);
+      $provide.decorator('$window', function($delegate) {
+        $delegate.addEventListener = angular.noop;
+
+        return $delegate;
+      });
+      $provide.value('ContactShell', ContactShell);
+      $provide.value('ContactAPIClient', ContactAPIClient);
+      $provide.value('VcardBuilder', VcardBuilder);
+      $provide.value('ContactLocationHelper', ContactLocationHelper);
+      $provide.value('ContactShellDisplayBuilder', ContactShellDisplayBuilder);
+    });
+  });
+
+  beforeEach(function() {
+    inject(function(
+      _$window_,
+      _$controller_,
+      _$rootScope_,
+      _$timeout_,
+      _$state_,
+      _contactAddressbookDisplayService_,
+      _CONTACT_AVATAR_SIZE_,
+      _CONTACT_EVENTS_
+    ) {
+      $window = _$window_;
+      $controller = _$controller_;
+      $rootScope = _$rootScope_;
+      $timeout = _$timeout_;
+      $state = _$state_;
+      contactAddressbookDisplayService = _contactAddressbookDisplayService_;
+      CONTACT_AVATAR_SIZE = _CONTACT_AVATAR_SIZE_;
+      CONTACT_EVENTS = _CONTACT_EVENTS_;
+    });
+
+    scope = $rootScope.$new();
+    contactAddressbookDisplayService.convertShellToDisplayShell = angular.noop;
+
+    createVcardMock(function() {
+      return {
+        get: function() { return $q.when({}); }
+      };
+    });
+  });
+
+  function createVcardMock(vcardFn, bookId, bookName) {
+    ContactAPIClient.addressbookHome = function(id) {
+      if (bookId) {
+        expect(id).to.equal(bookId);
+      }
+
+      return {
+        addressbook: function(name) {
+          if (bookName) {
+            expect(name).to.equal(bookName);
+          }
+
+          return {
+            vcard: vcardFn,
+            get: function() { return $q.when({ bookId: bookId, bookName: bookName, name: 'My Contacts' }); }
+          };
+        }
+      };
+    };
+  }
+
+  function initController() {
+    $controller('showContactController', { $scope: scope });
+    scope.$digest();
+  }
+
+  it('should change the state to the new contact destination after contact is moved', function(done) {
+    $state.go = sinon.spy();
+
+    initController();
+
+    var contact = {
+      id: scope.cardId,
+      addressbook: {
+        bookName: 'new-addressbook'
+      }
+    };
+
+    $rootScope.$broadcast(CONTACT_EVENTS.UPDATED, contact);
+
+    expect($state.go).to.have.been.calledWith('contact.addressbooks.show', {
+      bookId: scope.bookId,
+      bookName: 'new-addressbook',
+      cardId: scope.cardId
+    }, { location: 'replace' });
+
+    done();
+  });
+
+  it('should have bigger size for contact avatar', function() {
+    initController();
+    expect(scope.avatarSize).to.equal(CONTACT_AVATAR_SIZE.bigger);
+  });
+
+  it('should display an error if the contact cannot be loaded initially', function(done) {
+    createVcardMock(function() {
+      return {
+        get: function() { return $q.reject('WTF'); }
+      };
+    });
+    $alert.alert = function() { done(); };
+
+    initController();
+  });
+
+  describe('The fillContactData function', function() {
+    it('should fill the scope with the contact', function() {
+      contactUpdateDataService.contact = {};
+      initController();
+      var contact = { emails: [{ type: 'work', value: 'me@work.com' }, {type: 'home', value: 'me@home.com' }] };
+
+      scope.fillContactData(contact);
+      contactUpdateDataService.contact = contact;
+      expect(scope.contact).to.deep.equal(contact);
+    });
+
+    it('should fill the scope with the contact emails', function() {
+      contactUpdateDataService.contact = {};
+      initController();
+      var contact = { emails: [{ type: 'work', value: 'me@work.com' }, {type: 'home', value: 'me@home.com' }] };
+
+      scope.fillContactData(contact);
+      expect(scope.emails.length).to.equal(2);
+    });
+
+    it('should fill the scope with the contact phones', function() {
+      contactUpdateDataService.contact = {};
+      initController();
+      var contact = { tel: [{ type: 'work', value: '+33333333' }, { type: 'home', value: '+33444444' }] };
+
+      scope.fillContactData(contact);
+      expect(scope.phones.length).to.equal(2);
+    });
+
+    it('should fill the scope with the contact formattedBirthday', function() {
+      contactUpdateDataService.contact = {};
+      initController();
+      var contact = { birthday: '123', tel: [{ type: 'work', value: '+33333333' }, { type: 'home', value: '+33444444' }] };
+
+      scope.fillContactData(contact);
+      expect(scope.formattedBirthday).to.be.defined;
+    });
+
+    it('should build the display shell', function() {
+      var display = {foo: 'bar'};
+
+      ContactShellDisplayBuilder.build = function() {
+        return display;
+      };
+      contactUpdateDataService.contact = {};
+      initController();
+      var contact = { birthday: '123', tel: [{ type: 'work', value: '+33333333' }, { type: 'home', value: '+33444444' }] };
+
+      scope.fillContactData(contact);
+      expect(scope.displayShell).to.deep.equal(display);
+    });
+
+  });
+
+  describe('The $scope.shouldDisplayWork function', function() {
+
+    it('should return false when nothing defined', function() {
+      contactUpdateDataService.contact = {};
+      initController();
+      expect(scope.shouldDisplayWork()).to.be.false;
+    });
+
+    it('should return true when orgName is defined', function() {
+      contactUpdateDataService.contact = { orgName: 'linagora' };
+      initController();
+      expect(scope.shouldDisplayWork()).to.be.true;
+    });
+
+    it('should return true when orgRole is defined', function() {
+      contactUpdateDataService.contact = { orgRole: 'CTO' };
+      initController();
+      expect(scope.shouldDisplayWork()).to.be.true;
+    });
+
+    it('should return true when work address is filled', function() {
+      contactUpdateDataService.contact = { addresses: [{ type: 'work', value: 'Paris' }] };
+      initController();
+      expect(scope.shouldDisplayWork()).to.be.true;
+    });
+  });
+
+  describe('The $scope.shouldDisplayHome function', function() {
+
+    it('should return false when nothing defined', function() {
+      contactUpdateDataService.contact = {};
+      initController();
+      expect(scope.shouldDisplayHome()).to.be.false;
+    });
+
+    it('should return true when home address is filled', function() {
+      contactUpdateDataService.contact = { addresses: [{ type: 'home', value: 'Montpellier' }] };
+      initController();
+      expect(scope.shouldDisplayHome()).to.be.true;
+    });
+
+    it('should return true when birthday is filled', function() {
+      contactUpdateDataService.contact = { birthday: '15/12/1978' };
+      initController();
+      expect(scope.shouldDisplayHome()).to.be.true;
+    });
+
+    it('should return true when nickname is filled', function() {
+      contactUpdateDataService.contact = { nickname: 'yolo' };
+      initController();
+      expect(scope.shouldDisplayHome()).to.be.true;
+    });
+  });
+
+  describe('The $scope.shouldDisplayOthers function', function() {
+
+    it('should return false when nothing defined', function() {
+      contactUpdateDataService.contact = {};
+      initController();
+      expect(scope.shouldDisplayOthers()).to.be.false;
+    });
+
+    it('should return true when other address is defined', function() {
+      contactUpdateDataService.contact = { addresses: [{ type: 'other', value: 'Toulouse' }] };
+      initController();
+      expect(scope.shouldDisplayOthers()).to.be.true;
+    });
+
+    it('should return false when other tags are defined but empty', function() {
+      contactUpdateDataService.contact = { tags: [] };
+      initController();
+      expect(scope.shouldDisplayOthers()).to.be.false;
+    });
+
+    it('should return true when other tags are defined and not empty', function() {
+      contactUpdateDataService.contact = { tags: ['js', 'node'] };
+      initController();
+      expect(scope.shouldDisplayOthers()).to.be.true;
+    });
+
+    it('should return true when notes are defined', function() {
+      contactUpdateDataService.contact = { notes: 'This guy is so funky' };
+      initController();
+      expect(scope.shouldDisplayOthers()).to.be.true;
+    });
+
+    it('should return false when other urls are defined but empty', function() {
+      contactUpdateDataService.contact = { urls: [] };
+      initController();
+      expect(scope.shouldDisplayOthers()).to.be.false;
+    });
+
+    it('should return true when other tags are defined and not empty', function() {
+      contactUpdateDataService.contact = { urls: ['foo', 'bar'] };
+      initController();
+      expect(scope.shouldDisplayOthers()).to.be.true;
+    });
+  });
+
+  describe('Tests show contact during graceperiod task', function() {
+
+    it('should show the contact taken from contactUpdateDataService', function() {
+      contactUpdateDataService.contact = { id: 'myId' };
+      initController();
+      expect(scope.contact).to.eql(contactUpdateDataService.contact);
+    });
+
+    it('should clear contactUpdateDataService.contact when switch to other path', function() {
+      contactUpdateDataService.contact = { id: 'myId' };
+      initController();
+
+      scope.$emit('$stateChangeStart', {
+        name: '/some/path/other/than/contact/edit'
+      });
+
+      scope.$digest();
+      expect(contactUpdateDataService.contact).to.be.null;
+    });
+
+    it('should update contactUpdateDataService.contact when ther user edits contact again', function() {
+      contactUpdateDataService.contact = { id: 'myId' };
+      initController();
+
+      scope.contact = { id: 'myOtherId' };
+      scope.bookId = bookId;
+      scope.bookName = bookName;
+      scope.cardId = cardId;
+
+      scope.$emit('$stateChangeStart', {
+        name: 'contact.addressbooks.edit'
+      },
+      {
+        bookId: bookId,
+        bookName: bookName,
+        cardId: cardId
+      });
+
+      scope.$digest();
+      expect(contactUpdateDataService.contact).to.eql(scope.contact);
+    });
+
+    it('should flush the task when switch to other path', function(done) {
+      contactUpdateDataService.contact = { id: 'myId' };
+      contactUpdateDataService.taskId = 'a taskId';
+
+      gracePeriodService.flush = function(taskId) {
+        expect(taskId).to.equal('a taskId');
+        done();
+      };
+
+      initController();
+
+      scope.$emit('$stateChangeStart', {
+        name: '/some/path/other/than/contact/edit'
+      });
+
+      scope.$digest();
+    });
+
+    it('should update contact on CONTACT_EVENTS.CANCEL_UPDATE event', function() {
+      contactUpdateDataService.contact = { id: 'myId', firstName: 'Bob' };
+      contactUpdateDataService.taskId = 'a taskId';
+
+      initController();
+      scope.cardId = 'myId';
+      var newContact = { id: 'myId', firstName: 'Alice' };
+
+      scope.$emit(CONTACT_EVENTS.CANCEL_UPDATE, newContact);
+
+      scope.$digest();
+
+      expect(scope.contact).to.eql(newContact);
+    });
+
+    it('should flush the task on beforeunload event', function(done) {
+      contactUpdateDataService.contact = { id: 'myId' };
+      contactUpdateDataService.taskId = 'a taskId';
+
+      gracePeriodService.flush = sinon.spy();
+      $window.addEventListener = function(evt, handler) {
+        expect(evt).to.equal('beforeunload');
+        handler();
+        expect(gracePeriodService.flush.calledWithExactly('a taskId')).to.be.true;
+        done();
+      };
+
+      initController();
+
+    });
+
+  });
+
+  describe('The deleteContact function', function() {
+
+    it('should call deleteContact service with the right bookId, bookName and cardId', function() {
+      scope.bookName = 'bookName';
+      scope.contact = { id: 1, firstName: 'Foo', lastName: 'Bar', addressbook: {} };
+      var spy = sinon.spy();
+
+      $controller.bind(null, 'showContactController', {
+        $scope: scope,
+        deleteContact: spy
+      })();
+      scope.deleteContact();
+      $timeout.flush();
+      expect(spy).to.have.been.calledWith(bookId, scope.bookName, scope.contact);
+    });
+  });
+});
