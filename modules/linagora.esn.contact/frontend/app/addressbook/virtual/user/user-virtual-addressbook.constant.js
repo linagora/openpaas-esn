34f79ed1ece2b1bc9bf742f4e7810e10ddb260b6
Thang NGUYEN MANH <tnmanh@linagora.com>
2019-09-18 14:34:15 +0700

    linagora.esn.contact#506: Delete virtual address book for domain member users

--- modules/linagora.esn.contact/frontend/app/addressbook/virtual/user/user-virtual-addressbook.constant.js
+++
@@ -1,7 +0,0 @@
-(function(angular) {
-  'use strict';
-
-  angular.module('linagora.esn.contact')
-    .constant('CONTACT_USER_VIRTUAL_ADDRESSBOOK_ID', 'linagora.esn.contact.virtual.members');
-
-})(angular);
