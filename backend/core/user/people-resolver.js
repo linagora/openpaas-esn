9d9241ea52db1bfc597298cae51119f4809e519f
Christophe HAMERLING <chamerling@linagora.com>
2019-10-10 14:41:20 +0200

    #2799 Add phone support in User search response

--- backend/core/user/people-resolver.js
+++ backend/core/user/people-resolver.js
@@ -34,12 +34,14 @@ function denormalizer({ source }) {
   const email = new Model.EmailAddress({ value: denormalized.preferredEmail, type: 'default' });
   const name = new Model.Name({ displayName: getDisplayName(source) });
   const photo = new Model.Photo({ url: getPath(source) });
+  const phone = source.main_phone ? new Model.PhoneNumber({ value: source.main_phone }) : undefined;
 
   return Promise.resolve(
     new Model.Person({
       id: denormalized._id,
       objectType: OBJECT_TYPE,
       emailAddresses: [email],
+      phoneNumbers: phone ? [phone] : [],
       names: [name],
       photos: [photo]
     })
