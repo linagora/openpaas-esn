dfa80665505121ba9b91f28eadb37b5c7c49148f
Bao Khanh LE <bkle@linagora.com>
2019-12-12 16:03:46 +0700

    #2823: Technical-user CRUD methods implementation and unit tests

--- backend/core/technical-user/index.js
+++ backend/core/technical-user/index.js
@@ -1,33 +1,79 @@
-'use strict';
+const mongoose = require('mongoose');
+const TechnicalUser = mongoose.model('TechnicalUser');
+const authToken = require('../auth/token');
 
-var mongoose = require('mongoose');
-var TechnicalUser = mongoose.model('TechnicalUser');
-var authToken = require('../auth/token');
+const TYPE = 'technical';
 
-var TYPE = 'technical';
-module.exports.TYPE = TYPE;
+module.exports = {
+  TYPE,
+  add,
+  deleteById,
+  findByType,
+  findByTypeAndDomain,
+  get,
+  getNewToken,
+  list,
+  update
+};
 
 function findByType(type, callback) {
   TechnicalUser.find({type: type}, callback);
 }
-module.exports.findByType = findByType;
 
 function findByTypeAndDomain(type, domain, callback) {
   TechnicalUser.find({type: type, domain: domain}, callback);
 }
-module.exports.findByTypeAndDomain = findByTypeAndDomain;
+
+function add(technicalUser, callback) {
+  if (!(technicalUser instanceof TechnicalUser)) {
+    technicalUser = new TechnicalUser(technicalUser);
+  }
+
+  return technicalUser.save(callback);
+}
+
+function deleteById(id, callback) {
+  TechnicalUser.findByIdAndDelete(id, callback);
+}
 
 function get(id, callback) {
   TechnicalUser.findOne({_id: id}, callback);
 }
-module.exports.get = get;
 
-function list(callback) {
-  TechnicalUser.find(callback);
+function update(id, payload, callback) {
+  const { name, description, type, domain, data } = payload;
+
+  TechnicalUser.findByIdAndUpdate(id,
+    {
+      name,
+      description,
+      type,
+      domain,
+      data
+    }, callback);
 }
-module.exports.list = list;
+
+function list(options, callback) {
+  options = options || {};
+  const findOptions = {};
+
+  if (options.domainId) {
+    findOptions.domain = options.domainId;
+  }
+
+  let query = TechnicalUser.find(findOptions);
+
+  if (options.offset > 0) {
+    query = query.skip(+options.offset);
+  }
+
+  if (options.limit > 0) {
+    query = query.limit(+options.limit);
+  }
+
+  return query.exec(callback);
+}
 
 function getNewToken(technicalUser, ttl, callback) {
   authToken.getNewToken({ttl: ttl, user: technicalUser._id, user_type: TYPE}, callback);
 }
-module.exports.getNewToken = getNewToken;
