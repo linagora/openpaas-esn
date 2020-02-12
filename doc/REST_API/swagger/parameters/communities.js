9f6b90ea8982c378ff23e60c5bc1db1668f2213b
Tuan Tuan LE <tlcong@linagora.com>
2020-02-12 10:37:39 +0700

    linagora/lgs/openpaas/linagora.esn.community#11 fix swagger documentation
    When moving out swagger doc for community module, there are swagger definations for param and response that are required in core
    
    So move back them to core to fix the issue

---
+++ doc/REST_API/swagger/parameters/communities.js
@@ -0,0 +1,11 @@
+/**
+  * @swagger
+  * parameter:
+  *   ct_raw_data:
+  *     name: raw_data
+  *     in: body
+  *     description: the raw file data.
+  *     required: true
+  *     schema:
+  *       format: binary
+  */
