d4ba1a258270f1bd128b70d3aaec847d104a1c6a
Tuan Tuan LE <tlcong@linagora.com>
2020-02-12 10:37:39 +0700

    linagora/lgs/openpaas/linagora.esn.community#11 fix swagger documentation
    When moving out swagger doc for community module, there are swagger definations for param and response that are required in core
    
    So move back them to core to fix the issue

---
+++ doc/REST_API/swagger/responses/communities.js
@@ -0,0 +1,17 @@
+/**
+  * @swagger
+  * response:
+  *   ct_avatar:
+  *     description: OK. With the recorded image ID
+  *     schema:
+  *       type: object
+  *       properties:
+  *         "_id":
+  *           type: string
+  *           format: uuid
+  *     examples:
+  *       application/json:
+  *         {
+  *           "_id": '9f888058-e9e6-4915-814b-935d5b88389d'
+  *         }
+  */
