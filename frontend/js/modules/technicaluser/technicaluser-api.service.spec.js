ab920cb709c66b764852de4fb67ca5d3cf0237a1
Bao Khanh LE <bkle@linagora.com>
2020-02-26 17:59:36 +0700

    #2825: Frontend service to call technical user update and remove  API + tests

--- frontend/js/modules/technicaluser/technicaluser-api.service.spec.js
+++ frontend/js/modules/technicaluser/technicaluser-api.service.spec.js
@@ -3,17 +3,20 @@
 describe('The esnTechnicalUserAPIClient service', function() {
     var $httpBackend;
     var esnTechnicalUserAPIClient;
+    var domainId, technicalUser;
 
     beforeEach(module('esn.technicaluser'));
 
     beforeEach(inject(function(_$httpBackend_, _esnTechnicalUserAPIClient_) {
       $httpBackend = _$httpBackend_;
       esnTechnicalUserAPIClient = _esnTechnicalUserAPIClient_;
+
+      domainId = 1;
+      technicalUser = { _id: 123 };
     }));
 
-    describe('The list fn', function() {
+    describe('The list function', function() {
       it('should send a request to /api/domains/:domainId/technicalusers', function() {
-        var domainId = 1;
         var options = {
           offset: 5,
           limit: 30
@@ -26,14 +29,29 @@ describe('The esnTechnicalUserAPIClient service', function() {
       });
     });
 
-    describe('The add fn', function() {
+    describe('The add function', function() {
       it('should send a request to /api/domains/:domainId/technicalusers', function() {
-        var domainId = 1;
-        var technicalUser = {};
-
         $httpBackend.expectPOST('/api/domains/' + domainId + '/technicalusers').respond(201, []);
 
         esnTechnicalUserAPIClient.add(domainId, technicalUser);
+        $httpBackend.flush();
+      });
+    });
+
+    describe('The update function', function() {
+      it('should send a request to /api/domains/:domainId/technicalusers/:technicalUserId', function() {
+        $httpBackend.expectPUT('/api/domains/' + domainId + '/technicalusers/' + technicalUser._id).respond(204, []);
+
+        esnTechnicalUserAPIClient.update(domainId, technicalUser);
+        $httpBackend.flush();
+      });
+    });
+
+    describe('The remove function', function() {
+      it('should send a request to /api/domains/:domainId/technicalusers/:technicalUserId', function() {
+        $httpBackend.expectDELETE('/api/domains/' + domainId + '/technicalusers/' + technicalUser._id).respond(204, []);
+
+        esnTechnicalUserAPIClient.remove(domainId, technicalUser);
         $httpBackend.flush();
       });
     });
