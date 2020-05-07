8daefafb7c623946e933bb503eda26d383f1eacd
Bao Ngoc PHAM <bnpham@linagora.com>
2020-05-07 15:03:43 +0700

    #2942 Implement Health Check for service ElasticSearch

--- backend/core/init.js
+++ backend/core/init.js
@@ -2,11 +2,13 @@ const db = require('./db');
 const pubsub = require('./pubsub');
 const platformAdmin = require('./platformadmin/init');
 const amqp = require('./amqp');
+const elasticsearch = require('./elasticsearch');
 
 module.exports = callback => {
   db.mongo.init();
   pubsub.init();
   amqp.init();
+  elasticsearch.init();
   platformAdmin();
 
   callback && callback();
