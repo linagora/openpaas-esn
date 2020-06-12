39b1968720b2ac992d396982f571fcdcc7b4efa5
Bao Ngoc PHAM <bnpham@linagora.com>
2020-06-12 10:01:37 +0700

    linagora/lgs/openpaas/linagora.esn.calendar#1789 Implement binding ngModel in esn-date-picker

--- frontend/js/modules/datetime/date-picker/date-picker.controller.js
+++ frontend/js/modules/datetime/date-picker/date-picker.controller.js
@@ -4,18 +4,30 @@
   angular.module('esn.datetime')
     .controller('esnDatetimeDatePickerController', esnDatetimeDatePickerController);
 
-  function esnDatetimeDatePickerController($timeout, esnDatetimeService) {
+  function esnDatetimeDatePickerController($scope, $timeout, esnDatetimeService) {
     var self = this;
 
     self.$onInit = $onInit;
     self.onChange = onChange;
 
     function $onInit() {
+      // Watch the change to ngModel & update uiValue accordingly
+      var unsubscribeFromWatchNgModel = $scope.$watch(function() {
+        return self.ngModel;
+      }, function(newValue, oldValue) {
+        if (newValue && oldValue && newValue.valueOf() === oldValue.valueOf()) return;
+        self.uiValue = esnDatetimeService.updateObjectToBrowserTimeZone(newValue);
+      });
+
+      $scope.$on('$destroy', function() {
+        unsubscribeFromWatchNgModel();
+      });
+
       self.uiValue = self.ngModel && esnDatetimeService.updateObjectToBrowserTimeZone(self.ngModel);
     }
 
     function onChange() {
-      self.ngModel = esnDatetimeService.updateObjectToUserTimeZone(self.uiValue);
+      self.ngModel = esnDatetimeService.updateObjectToUserTimeZone(self.uiValue.local());
       $timeout(function() {
         self.ngChange();
       }, 0);
