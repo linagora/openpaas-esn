586e4a73a93335caa8bf7cb677c01e9e060089fe
Ba Son PHAM <bspham@linagora.com>
2020-07-21 10:44:47 +0700

    Update the documentation of API Update the documentation of API PUT /themes/:uuid
    Update the documentation of API PUT /themes/:uuid
    
    Delete tm_400
    
    Update the documentation of API Update the documentation of API PUT /themes/:uuid
    
    Use reference in payload schema
    
    Use status code 200 instead of 204

---
+++ doc/REST_API/swagger/parameters/themes.js
@@ -0,0 +1,11 @@
+/**
+ * @swagger
+ * parameter:
+ *   tm_themes:
+ *     name: tm_themes
+ *     in: body
+ *     required: true
+ *     description: The payload which is used to set the theme for a given domain.
+ *     schema:
+ *       $ref: "#/definitions/tm_themes"
+ */
