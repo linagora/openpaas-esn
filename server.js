a6ca522577e48769558fe5cac1bb4726fa751362
Michael BAILLY <mbailly@linagora.com>
2020-12-10 13:26:45 +0100

    Revert " fix"
    This reverts commit 6012b064eaf1312e76e67e2022958dd29b5aa98a.

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 'use strict';
 
+<<<<<<< HEAD
+const config = require('./backend/core/config')('default');
+
+if (config.apm && config.apm.enabled) {
+  require('elastic-apm-node').start(config.apm.configuration);
+=======
 if (process.env.APM_SERVER_URL) {
   const opts = {
     serverUrl: process.env.APM_SERVER_URL,
@@ -9,6 +15,7 @@ if (process.env.APM_SERVER_URL) {
   console.log('Starting APM', opts);
 
   require('elastic-apm-node').start(opts);
+>>>>>>> faf5c5f7e...  #3055 enable and use Elastic APM
 }
 
 var async = require('async');
